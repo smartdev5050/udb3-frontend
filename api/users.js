5512ac15e44415aa100743b6ee0f14662aeb439d
fulldev1023 <simbadevlop@gmail.com>
2020-06-26 14:50:26 +0200

    Add users.js for fetching user information

---
+++ api/users.js
@@ -0,0 +1,10 @@
+export const getMe = (token) => async (id) => {
+  const res = await fetch(`http://io.uitdatabank.dev/users/${id}`, {
+    headers: {
+      'Content-Type': 'application/json',
+      Authorization: `Bearer ${token}`,
+      'X-Api-Key': process.env.apiKey,
+    },
+  })
+  return await res.json()
+}
