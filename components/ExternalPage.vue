242ba5c1160c4acaae5db29caad4e903da38985d
fulldev1023 <simbadevlop@gmail.com>
2020-06-26 14:31:59 +0200

    Push the route of udb3-angular-app to the history

--- components/ExternalPage.vue
+++ components/ExternalPage.vue
@@ -3,28 +3,30 @@
 </template>
 
 <script>
-export default {
-  name: 'ExternalPage',
-  props: {
-    path: {
-      type: String,
-      default: '',
+  export default {
+    name: 'ExternalPage',
+    props: {
+      path: {
+        type: String,
+        default: '',
+      },
     },
-  },
-  mounted() {
-    setTimeout(() => {
-      const iframe = this.$refs.iframe.contentWindow
-      console.log('iframe', iframe)
-      iframe.postMessage('hello', '*')
-    }, 2000)
-  },
-}
+    mounted() {
+      // const iframe = this.$refs.iframe.contentWindow
+      window.addEventListener('message', (event) => {
+        if (event.data.type === 'UDB') {
+          const location = event.data.message
+          history.pushState('test', 'test', location)
+        }
+      })
+    },
+  }
 </script>
 
 <style scoped>
-.external_app {
-  height: 100vh;
-  width: 100%;
-  border: 0;
-}
+  .external_app {
+    height: 100vh;
+    width: 100%;
+    border: 0;
+  }
 </style>
