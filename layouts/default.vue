350ddba0c5e142d93ba7fef02110b76beb9e97f7
fulldev1023 <simbadevlop@gmail.com>
2020-06-26 14:33:39 +0200

    Removed test message eventListener

--- layouts/default.vue
+++ layouts/default.vue
@@ -8,44 +8,39 @@
 </template>
 
 <script>
-import Sidebar from '../components/Sidebar'
-export default {
-  components: {
-    Sidebar,
-  },
-  mounted() {
-    window.addEventListener('message', (event) => {
-      const message = event.data
-      console.log('message', message)
-    })
-  },
-}
+  import Sidebar from '../components/Sidebar'
+  export default {
+    middleware: ['auth'],
+    components: {
+      Sidebar,
+    },
+  }
 </script>
 
 <style>
-.main-container {
-  display: flex;
-  flex-direction: row;
-}
-.content {
-  flex: 1;
-}
-html {
-  font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont, 'Segoe UI',
-    Roboto, 'Helvetica Neue', Arial, sans-serif;
-  font-size: 16px;
-  word-spacing: 1px;
-  -ms-text-size-adjust: 100%;
-  -webkit-text-size-adjust: 100%;
-  -moz-osx-font-smoothing: grayscale;
-  -webkit-font-smoothing: antialiased;
-  box-sizing: border-box;
-}
+  .main-container {
+    display: flex;
+    flex-direction: row;
+  }
+  .content {
+    flex: 1;
+  }
+  html {
+    font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
+      'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
+    font-size: 16px;
+    word-spacing: 1px;
+    -ms-text-size-adjust: 100%;
+    -webkit-text-size-adjust: 100%;
+    -moz-osx-font-smoothing: grayscale;
+    -webkit-font-smoothing: antialiased;
+    box-sizing: border-box;
+  }
 
-*,
-*::before,
-*::after {
-  box-sizing: border-box;
-  margin: 0;
-}
+  *,
+  *::before,
+  *::after {
+    box-sizing: border-box;
+    margin: 0;
+  }
 </style>
