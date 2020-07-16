e262fb94527cb0e3b43fb692eb8bd1563f2819fe
fulldev1023 <simbadevlop@gmail.com>
2020-07-16 16:01:46 +0200

    Add beter favicon

--- nuxt.config.js
+++ nuxt.config.js
@@ -19,7 +19,7 @@ export default {
         content: process.env.npm_package_description || '',
       },
     ],
-    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    link: [{ rel: 'icon', type: 'image/x-icon', href: '/favicon.png' }],
   },
   /*
    ** Global CSS
