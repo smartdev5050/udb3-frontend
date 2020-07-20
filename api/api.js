8d7887a973af2c3f77a353b7b5c1793be78f63fa
fulldev1023 <simbadevlop@gmail.com>
2020-07-20 11:47:01 +0200

    Logout on invalid token

--- api/api.js
+++ api/api.js
@@ -1,3 +1,5 @@
+import jwtDecode from 'jwt-decode';
+import { logout } from '../services/auth';
 import { getMe, getPermissions, getRoles } from './user';
 
 export const getHeaders = (token) => ({
@@ -5,10 +7,23 @@ export const getHeaders = (token) => ({
   'X-Api-Key': process.env.apiKey,
 });
 
+const isTokenValid = (token) => {
+  const { exp } = jwtDecode(token);
+  const now = Math.round(Date.now() / 1000);
+  return exp - now > 0;
+};
+
 export default (tokenCallback) => {
   const apiUrl = process.env.apiUrl;
 
-  const headersCallback = () => getHeaders(tokenCallback());
+  const headersCallback = () => {
+    const token = tokenCallback();
+    if (!isTokenValid(token)) {
+      logout();
+      return;
+    }
+    return getHeaders(token);
+  };
 
   return {
     user: {
