10b4aa07c60aa479ee4285dce394b85344715f59
fulldev1023 <simbadevlop@gmail.com>
2020-07-27 17:10:56 +0200

    III-3368 changed font

--- layouts/default.vue
+++ layouts/default.vue
@@ -32,9 +32,8 @@
     flex: 1;
   }
   html {
-    font-family: 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-      'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-    font-size: 16px;
+    font-family: 'Open Sans', Helvetica, Arial, sans-serif;
+    font-size: 15px;
     word-spacing: 1px;
     -ms-text-size-adjust: 100%;
     -webkit-text-size-adjust: 100%;
