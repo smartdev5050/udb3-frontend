a6f79fe144b0612fd178b620dfa5c11055520b08
fulldev1023 <simbadevlop@gmail.com>
2020-07-30 09:19:16 +0200

    add spaces in style of default component

--- layouts/default.vue
+++ layouts/default.vue
@@ -23,15 +23,18 @@
 
 <style>
   @import url('//fonts.googleapis.com/css?family=Open+Sans:400italic,600italic,700italic,300,400,700,600');
+
   .main-container {
     display: flex;
     flex-direction: row;
     position: relative;
     z-index: 0;
   }
+
   .content {
     flex: 1;
   }
+
   html {
     -ms-text-size-adjust: 100%;
     -webkit-text-size-adjust: 100%;
@@ -39,6 +42,7 @@
     -webkit-font-smoothing: antialiased;
     box-sizing: border-box;
   }
+
   body {
     font-family: 'Open Sans', Helvetica, Arial, sans-serif;
     font-size: 15px;
