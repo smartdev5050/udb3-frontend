6f02a3cd4ce953043899af0ddafb0e9ddd3c3612
fulldev1023 <simbadevlop@gmail.com>
2020-08-03 16:19:56 +0200

    add pagination component

---
+++ components/pagination.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="pagination">
+    <b-pagination
+      v-model="currentPage"
+      :total-rows="rows"
+      :per-page="perPage"
+      :first-text="First"
+      :prev-text="$t('actions.previous')"
+      :next-text="$t('actions.next')"
+    />
+  </div>
+</template>
+
+<script>
+  export default {
+    props: {
+      currentPage: { type: Number, default: 1 },
+      rows: { type: Number, default: 1 },
+      perPage: { type: Number, default: 1 },
+    },
+  };
+</script>
