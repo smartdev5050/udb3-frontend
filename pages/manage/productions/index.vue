95bbfa49316c710dd53894e49751c48ac511344a
fulldev1023 <simbadevlop@gmail.com>
2020-08-04 14:54:25 +0200

    change translation path name from add to create

--- pages/manage/productions/index.vue
+++ pages/manage/productions/index.vue
@@ -5,7 +5,7 @@
         {{ $t('menu.productions') }}
         <small>
           <nuxt-link to="productions/create">
-            {{ $t('actions.add') }}
+            {{ $t('actions.create') }}
           </nuxt-link>
         </small>
       </h1>
