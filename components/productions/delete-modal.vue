2136e32b72f3d42987faf2b3cb82d4e410e79415
fulldev1023 <simbadevlop@gmail.com>
2020-08-14 15:03:09 +0200

    add translations to modal

--- components/productions/delete-modal.vue
+++ components/productions/delete-modal.vue
@@ -1,9 +1,31 @@
 <template>
-  <b-modal id="deleteModal" ok-title="Okéé" cancel-title="Nein" hide-header>
-    <p>Ben je zeker?</p>
+  <b-modal
+    id="deleteModal"
+    :ok-title="$t('productions.delete')"
+    :cancel-title="$t('productions.cancel')"
+    hide-header
+  >
+    <div class="content-container">
+      {{
+        $t('productions.delete_question', {
+          eventName: 'hello',
+          productionTitle: 'jello',
+        })
+      }}
+    </div>
   </b-modal>
 </template>
 
 <script>
   export default {};
 </script>
+
+<style lang="scss">
+  .modal-content button {
+    text-transform: capitalize !important;
+  }
+
+  .content-container {
+    padding: 1rem;
+  }
+</style>
