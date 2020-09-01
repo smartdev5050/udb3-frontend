b11d2cc65f75282ff212a0ddc808fb63f097c637
fulldev1023 <simbadevlop@gmail.com>
2020-09-01 14:39:07 +0200

    Add udb3-button component

---
+++ udb3-ui/udb3-button.vue
@@ -0,0 +1,24 @@
+<template>
+  <b-button v-bind="$attrs" :variant="variant"><slot /></b-button>
+</template>
+
+<style lang="scss" scoped></style>
+
+<script>
+  export const ButtonVariant = {
+    PRIMARY: 'primary',
+    SECONDARY: 'outline-secondary',
+    SUCCESS: 'success',
+    DANGER: 'danger',
+  };
+
+  export default {
+    name: 'Udb3Button',
+    props: {
+      variant: {
+        type: String,
+        default: ButtonVariant.PRIMARY,
+      },
+    },
+  };
+</script>
