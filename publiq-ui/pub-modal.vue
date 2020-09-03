c51a2d210dec640ac20893671e7777f7d3bb0d75
fulldev1023 <simbadevlop@gmail.com>
2020-09-03 10:34:55 +0200

    add modal stories

--- publiq-ui/pub-modal.vue
+++ publiq-ui/pub-modal.vue
@@ -1,9 +1,25 @@
 <template>
   <b-modal
+    v-if="variant === modalVariant.WITH_BUTTONS"
+    :title="title"
+    :ok-title="confirmTitle"
+    :cancel-title="cancelTitle"
+    cancel-variant="outline-secondary"
+    :visible="isVisible"
+    :scrollable="false"
+    :hide-header="hideHeader"
+    @ok.prevent="handleConfirm"
+    @abort="handleCancel"
+  >
+    <slot />
+  </b-modal>
+  <b-modal
+    v-else
     size="xl"
     :title="title"
     :scrollable="false"
-    hide-footer
+    :hide-header="hideHeader"
+    :hide-footer="hideFooter"
     :visible="isVisible"
   >
     <slot />
@@ -11,6 +27,11 @@
 </template>
 
 <script>
+  export const ModalVariant = {
+    DEFAULT: 'default',
+    WITH_BUTTONS: 'with-buttons',
+  };
+
   export default {
     props: {
       title: {
@@ -20,6 +41,39 @@
       isVisible: {
         type: Boolean,
         default: false,
+      },
+      variant: {
+        type: String,
+        default: 'default',
+      },
+      confirmTitle: {
+        type: String,
+        default: 'Confirm',
+      },
+      cancelTitle: {
+        type: String,
+        default: 'Cancel',
+      },
+      hideHeader: {
+        type: Boolean,
+        default: false,
+      },
+      hideFooter: {
+        type: Boolean,
+        default: false,
+      },
+    },
+    computed: {
+      modalVariant() {
+        return ModalVariant;
+      },
+    },
+    methods: {
+      handleConfirm() {
+        this.$emit('confirm');
+      },
+      handleCancel() {
+        this.$emit('cancel');
       },
     },
   };
