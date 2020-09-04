56ce6be7bf1dd60317a201d3b20749c872cc940b
fulldev1023 <simbadevlop@gmail.com>
2020-09-04 16:52:13 +0200

    delete duplicate styling

--- components/giftbox.vue
+++ components/giftbox.vue
@@ -170,19 +170,6 @@
 </script>
 
 <style lang="scss" scoped>
-  ::v-deep .modal {
-    overflow-y: hidden;
-  }
-
-  ::v-deep .modal-content {
-    max-height: 95vh;
-    box-shadow: 0 5px 15px rgba(0, 0, 0, 0.5);
-  }
-
-  ::v-deep .modal-body {
-    padding: 0;
-  }
-
   .features-wrapper {
     width: 100%;
     height: 100%;
