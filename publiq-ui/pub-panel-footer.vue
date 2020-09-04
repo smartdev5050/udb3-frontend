fccada475ebee61bea84dffa3cd5f9219dfffcfb
fulldev1023 <simbadevlop@gmail.com>
2020-09-04 11:29:11 +0200

    add panel and panel-footer components

---
+++ publiq-ui/pub-panel-footer.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="panel-footer">
+    <slot />
+  </div>
+</template>
+
+<script>
+  export default {};
+</script>
+
+<style lang="scss" scoped>
+  .panel-footer {
+    padding: 0.75rem 1rem;
+    background-color: #f5f5f5;
+    border-top: 1px solid #ddd;
+  }
+</style>
---
+++ publiq-ui/pub-panel.vue
@@ -0,0 +1,17 @@
+<template>
+  <div class="panel">
+    <slot />
+  </div>
+</template>
+
+<script>
+  export default {};
+</script>
+
+<style lang="scss" scoped>
+  .panel {
+    border: 1px solid #ddd;
+    margin-bottom: 1rem;
+    box-shadow: 0 1px 1px rgba(0, 0, 0, 0.05);
+  }
+</style>
