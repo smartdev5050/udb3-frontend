39d785dc13972f06af5cb63eaacc89450d7d726a
fulldev1023 <simbadevlop@gmail.com>
2020-09-07 10:54:47 +0200

    add pub-loading-spinner component

---
+++ publiq-ui/pub-loading-spinner.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="text-center d-flex justify-content-center spinner-container">
+    <b-spinner variant="primary"></b-spinner>
+  </div>
+</template>
+
+<script>
+  export default {};
+</script>
+
+<style lang="scss" scoped>
+  .spinner-container {
+    margin: 1rem;
+    width: 100%;
+  }
+</style>
