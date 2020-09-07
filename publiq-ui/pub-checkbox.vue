da19ea3de00853d946f363c5ff7b5c3b93067b62
fulldev1023 <simbadevlop@gmail.com>
2020-09-07 10:40:51 +0200

    add pub-checkbox component

---
+++ publiq-ui/pub-checkbox.vue
@@ -0,0 +1,29 @@
+<template>
+  <b-form-checkbox
+    :checked="isChecked"
+    :disabled="isDisabled"
+    @input="handleInput"
+  >
+    <slot />
+  </b-form-checkbox>
+</template>
+
+<script>
+  export default {
+    props: {
+      isChecked: {
+        type: Boolean,
+        default: false,
+      },
+      isDisabled: {
+        type: Boolean,
+        default: false,
+      },
+    },
+    methods: {
+      handleInput() {
+        this.$emit('input');
+      },
+    },
+  };
+</script>
