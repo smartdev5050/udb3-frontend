7a42374640599f7a2d1550c9d52427423780134e
fulldev1023 <simbadevlop@gmail.com>
2020-09-08 16:59:23 +0200

    add pub-alert component

---
+++ publiq-ui/pub-alert.vue
@@ -0,0 +1,26 @@
+<template>
+  <b-alert :variant="variant" :show="show" dismissible>
+    <slot />
+  </b-alert>
+</template>
+
+<script>
+  export const AlertVariant = {
+    DANGER: 'danger',
+    INFO: 'info',
+    SUCCESS: 'success',
+  };
+
+  export default {
+    props: {
+      variant: {
+        type: String,
+        default: AlertVariant.INFO,
+      },
+      show: {
+        type: Boolean,
+        default: false,
+      },
+    },
+  };
+</script>
