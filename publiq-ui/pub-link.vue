0046f8c9e62d199c23cf0f85a736db9e7b701a03
fulldev1023 <simbadevlop@gmail.com>
2020-09-10 17:12:02 +0200

    add pub-link component

---
+++ publiq-ui/pub-link.vue
@@ -0,0 +1,25 @@
+<template>
+  <a :href="href"><slot /></a>
+</template>
+
+<script>
+  export default {
+    props: {
+      href: {
+        type: String,
+        default: '',
+      },
+    },
+  };
+</script>
+
+<style lang="scss" scoped>
+  a {
+    font-weight: 400;
+    color: $udb-blue;
+
+    &:hover {
+      text-decoration: underline;
+    }
+  }
+</style>
