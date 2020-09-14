d97570b269591c6222c362ca5af114c2df3f0dab
fulldev1023 <simbadevlop@gmail.com>
2020-09-14 10:13:33 +0200

    add pub-radio-group

---
+++ publiq-ui/pub-radio-group.vue
@@ -0,0 +1,54 @@
+<template>
+  <b-form-group :label="groupLabel">
+    <b-form-radio
+      v-for="(value, index) in values"
+      :key="value"
+      v-model="selectedModel"
+      :value="value"
+      :name="name"
+      >{{ labels[index] }}</b-form-radio
+    >
+  </b-form-group>
+</template>
+
+<script>
+  export default {
+    props: {
+      name: {
+        type: String,
+        default: '',
+      },
+      groupLabel: {
+        type: String,
+        default: '',
+      },
+      labels: {
+        type: Array,
+        default: () => [],
+      },
+      values: {
+        type: Array,
+        default: () => [],
+      },
+      selected: {
+        type: String,
+        default: '',
+      },
+    },
+    computed: {
+      selectedModel: {
+        get() {
+          return this.selected;
+        },
+        set(newSelected) {
+          this.$emit('input', newSelected);
+        },
+      },
+    },
+    watch: {
+      selected() {
+        console.log({ selected: this.selected });
+      },
+    },
+  };
+</script>
