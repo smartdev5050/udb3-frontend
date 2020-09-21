33f93dccfb52db01362bb34f9aca996c08b72590
fulldev1023 <simbadevlop@gmail.com>
2020-09-21 16:35:51 +0200

    add pub-title

---
+++ publiq-ui/pub-title.vue
@@ -0,0 +1,36 @@
+<template>
+  <component :is="heading"><slot /></component>
+</template>
+
+<script>
+  export default {
+    props: {
+      size: {
+        type: Number,
+        default: 2,
+        required: true,
+        validator(val) {
+          [1, 2, 3, 4].includes(val);
+        },
+      },
+    },
+    computed: {
+      heading() {
+        return `h${this.size}`;
+      },
+    },
+  };
+</script>
+
+<style lang="scss" scoped>
+  // nearly the same as pub-page-title
+  h1 {
+    font-weight: 300;
+    font-size: 1.6rem;
+  }
+
+  h2 {
+    font-weight: 700;
+    font-size: 1.2rem;
+  }
+</style>
