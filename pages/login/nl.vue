20a5b61ff30d9896f2f455e6318757d0ffa9bd24
fulldev1023 <simbadevlop@gmail.com>
2020-10-06 16:00:01 +0200

    add new pages to accommodate different language paths

---
+++ pages/login/nl.vue
@@ -0,0 +1,20 @@
+<template>
+  <login />
+</template>
+
+<script>
+  import Login from '@/components/login';
+
+  export default {
+    layout: 'login',
+    components: {
+      Login,
+    },
+    mounted() {
+      this.$i18n.locale = 'nl';
+      this.$cookies.set('udb-language', 'nl');
+    },
+  };
+</script>
+
+<style lang="scss" scoped></style>
