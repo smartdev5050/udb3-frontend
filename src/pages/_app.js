f2c1c1412fd03d0339de4933d21f7d3b335eb298
fulldev1023 <simbadevlop@gmail.com>
2020-10-26 11:21:54 +0100

    Ignore prop validation on _app

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,6 @@
 import '../styles/global/globals.scss';
 
+// eslint-disable-next-line react/prop-types
 const App = ({ Component, pageProps }) => <Component {...pageProps} />;
 
 export default App;
