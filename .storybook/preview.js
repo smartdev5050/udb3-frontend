16176768e6a9cb5d1ecf16b40d494db94b220a2d
fulldev1023 <simbadevlop@gmail.com>
2020-10-26 11:44:31 +0100

    Set up storybook

---
+++ .storybook/preview.js
@@ -0,0 +1,5 @@
+import '../src/styles/global/globals.scss';
+
+export const parameters = {
+  actions: { argTypesRegex: '^on[A-Z].*' },
+};
---
+++ next.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+  stories: ['../src/components/publiq-ui/**/*.stories.mdx'],
+  addons: [
+    '@storybook/addon-links',
+    '@storybook/addon-essentials',
+    '@storybook/addon-a11y',
+  ],
+};
