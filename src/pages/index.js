ce0354eb68c5f616c65b405be8d61595fcf94f24
fulldev1023 <simbadevlop@gmail.com>
2020-10-26 11:29:18 +0100

    Load Alert component on home page

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,7 +1,12 @@
+import Alert from '../components/publiq-ui/Alert';
 import styles from './index.module.scss';
 
 const Home = () => {
-  return <div className={styles.container}>Hello</div>;
+  return (
+    <div className={styles.container}>
+      <Alert visible>Welcome to Udb3 React</Alert>
+    </div>
+  );
 };
 
 export default Home;
