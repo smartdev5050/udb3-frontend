6a07742d6ee56b780332d6dd3814fc7ec94bd794
fulldev1023 <simbadevlop@gmail.com>
2020-10-29 15:21:16 +0100

    Fix ModalContent layout

--- src/components/publiq-ui/ModalContent.js
+++ src/components/publiq-ui/ModalContent.js
@@ -1,42 +1,57 @@
 import PropTypes from 'prop-types';
 import { Modal as BootstrapModal } from 'react-bootstrap';
-import { Button, ButtonVariants } from './Button';
 import styled from 'styled-components';
 
-const StyledBootstrapModal = styled(BootstrapModal)``;
+const StyledBootstrapModal = styled(BootstrapModal)`
+  .modal-title {
+    font-size: 1.067rem;
+    font-weight: 700;
+  }
 
-const ModalContent = ({ visible, onClose }) => {
+  .modal {
+    overflow-y: hidden;
+  }
+
+  .modal-content {
+    border-radius: 0;
+    max-height: 95vh;
+    box-shadow: 0 5px 15px rgba(0, 0, 0, 0.5);
+  }
+
+  .modal-body {
+    padding: 0;
+  }
+`;
+
+const ModalContent = ({ visible, title, onClose, children }) => {
   return (
     <StyledBootstrapModal
       show={visible}
       onHide={onClose}
       backdrop="static"
       keyboard={false}
+      size="xl"
     >
       <StyledBootstrapModal.Header closeButton>
-        <StyledBootstrapModal.Title>Modal title</StyledBootstrapModal.Title>
+        <StyledBootstrapModal.Title as="h5" hidden={!title}>
+          {title}
+        </StyledBootstrapModal.Title>
       </StyledBootstrapModal.Header>
-      <StyledBootstrapModal.Body>
-        I will not close if you click outside me. Don't even try to press escape
-        key.
-      </StyledBootstrapModal.Body>
-      <StyledBootstrapModal.Footer>
-        <Button variant={ButtonVariants.SECONDARY} onClick={onClose}>
-          Close
-        </Button>
-        <Button variant={ButtonVariants.PRIMARY}>Understood</Button>
-      </StyledBootstrapModal.Footer>
+      <StyledBootstrapModal.Body>{children}</StyledBootstrapModal.Body>
     </StyledBootstrapModal>
   );
 };
 
 ModalContent.propTypes = {
   visible: PropTypes.bool,
+  title: PropTypes.string,
   onClose: PropTypes.func,
+  children: PropTypes.node,
 };
 
 ModalContent.defaultProps = {
   visible: false,
+  title: '',
   onClose: () => {},
 };
 
