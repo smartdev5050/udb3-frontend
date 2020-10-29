e121df5501dbe1b14060706519632b46792e02d1
fulldev1023 <simbadevlop@gmail.com>
2020-10-29 16:35:31 +0100

    Add Checkbox component

---
+++ src/components/publiq-ui/Checkbox.js
@@ -0,0 +1,58 @@
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+import { Label } from './Label';
+
+const StyledCheckbox = styled.div`
+  display: flex;
+  align-items: center;
+
+  input {
+    margin-right: 0.5rem;
+  }
+
+  input:hover {
+    cursor: pointer;
+  }
+
+  label {
+    font-weight: normal;
+  }
+
+  label:hover {
+    cursor: pointer;
+  }
+`;
+
+const Checkbox = ({ id, name, checked, disabled, onToggle, children }) => {
+  return (
+    <StyledCheckbox>
+      <input
+        id={id}
+        type="checkbox"
+        name={name}
+        checked={checked}
+        disabled={disabled}
+        onChange={onToggle}
+      />
+      <Label id={id}>{children}</Label>
+    </StyledCheckbox>
+  );
+};
+
+Checkbox.propTypes = {
+  id: PropTypes.string.isRequired,
+  name: PropTypes.string,
+  checked: PropTypes.bool,
+  disabled: PropTypes.bool,
+  onToggle: PropTypes.func,
+  children: PropTypes.node,
+};
+
+Checkbox.defaultprops = {
+  name: '',
+  checked: false,
+  disabled: false,
+  onInput: () => {},
+};
+
+export { Checkbox };
