95deb8fc94398bba9589e082dc8416e066ac3dfa
fulldev1023 <simbadevlop@gmail.com>
2020-10-29 16:10:29 +0100

    Add Label component

---
+++ src/components/publiq-ui/Label.js
@@ -0,0 +1,23 @@
+import PropTypes from 'prop-types';
+import styled from 'styled-components';
+
+const StyledLabel = styled.label`
+  font-size: 1rem;
+  font-weight: 700;
+  margin-bottom: 0;
+`;
+
+const Label = ({ id, children }) => {
+  return <StyledLabel htmlFor={id}>{children}</StyledLabel>;
+};
+
+Label.propTypes = {
+  id: PropTypes.string,
+  children: PropTypes.node,
+};
+
+Label.defaultTypes = {
+  id: '',
+};
+
+export { Label };
