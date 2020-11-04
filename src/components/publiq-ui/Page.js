7fa4844cf125651718108e2bb2cfecadb860258b
fulldev1023 <simbadevlop@gmail.com>
2020-11-04 13:26:25 +0100

    Add page component

--- src/components/publiq-ui/Page.js
+++ src/components/publiq-ui/Page.js
@@ -8,6 +8,7 @@ const StyledSection = styled.section`
   width: 100%;
   min-height: 100vh;
   display: flex;
+  flex-direction: column;
   justify-content: left;
   align-items: flex-start;
   text-align: left;
