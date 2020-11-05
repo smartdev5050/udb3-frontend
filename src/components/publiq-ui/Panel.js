f78aacb6fc6c9e0fe93d38d6f22662f906465a0c
fulldev1023 <simbadevlop@gmail.com>
2020-11-05 16:42:33 +0100

    Use getValueFromTheme

--- src/components/publiq-ui/Panel.js
+++ src/components/publiq-ui/Panel.js
@@ -1,8 +1,11 @@
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import { getValueFromTheme } from './theme';
+
+const getValue = getValueFromTheme('panel');
 
 const StyledSection = styled.section`
-  border: 1px solid #ddd;
+  border: 1px solid ${getValue('borderColor')};
   margin-bottom: 1rem;
   box-shadow: 0 1px 1px rgba(0, 0, 0, 0.05);
 `;
--- src/components/publiq-ui/PanelFooter.js
+++ src/components/publiq-ui/PanelFooter.js
@@ -1,10 +1,13 @@
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import { getValueFromTheme } from './theme';
+
+const getValue = getValueFromTheme('panelFooter');
 
 const StyledSection = styled.section`
   padding: 0.75rem 1rem;
-  background-color: #f5f5f5;
-  border-top: 1px solid #ddd;
+  background-color: ${getValue('backgroundColor')};
+  border-top: 1px solid ${getValue('borderColor')};
 `;
 
 const PanelFooter = ({ children, className }) => (
