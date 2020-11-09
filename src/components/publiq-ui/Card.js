4c3c1320a2c8f21950219d4751c5fe429376959e
fulldev1023 <simbadevlop@gmail.com>
2020-11-09 16:57:35 +0100

    Add spacing to Card

--- src/components/publiq-ui/Card.js
+++ src/components/publiq-ui/Card.js
@@ -1,20 +1,28 @@
 import { Card as BootstrapCard } from 'react-bootstrap';
 import styled from 'styled-components';
 import PropTypes from 'prop-types';
+import { pick } from 'lodash';
+import { spacingProps, spacingPropTypes } from './Box';
 
 const StyledBootstrapCard = styled(BootstrapCard)`
   &.card {
     border: none;
   }
+
+  ${spacingProps}
 `;
 
-const Card = ({ children, className }) => (
-  <StyledBootstrapCard className={className}>
-    <StyledBootstrapCard.Body>{children}</StyledBootstrapCard.Body>
-  </StyledBootstrapCard>
-);
+const Card = ({ children, className, ...props }) => {
+  const layoutProps = pick(props, Object.keys(spacingPropTypes));
+  return (
+    <StyledBootstrapCard className={className} {...layoutProps}>
+      <StyledBootstrapCard.Body>{children}</StyledBootstrapCard.Body>
+    </StyledBootstrapCard>
+  );
+};
 
 Card.propTypes = {
+  ...spacingPropTypes,
   className: PropTypes.string,
   children: PropTypes.node,
 };
