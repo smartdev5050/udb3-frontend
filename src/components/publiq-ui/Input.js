5ac37002aaa9e322655ee91609531feb6b50a99d
fulldev1023 <simbadevlop@gmail.com>
2020-11-09 17:05:40 +0100

    Add spacing to Input

--- src/components/publiq-ui/Input.js
+++ src/components/publiq-ui/Input.js
@@ -1,16 +1,28 @@
 import { Form } from 'react-bootstrap';
 import PropTypes from 'prop-types';
+import { spacingProps, spacingPropTypes } from './Box';
+import { pick } from 'lodash';
+import styled from 'styled-components';
 
-const Input = ({ type, id, placeholder, className }) => (
-  <Form.Control
-    id={id}
-    type={type}
-    placeholder={placeholder}
-    className={className}
-  />
-);
+const StyledFormControl = styled(Form.Control)`
+  ${spacingProps};
+`;
+
+const Input = ({ type, id, placeholder, className, ...props }) => {
+  const layoutProps = pick(props, Object.keys(spacingPropTypes));
+  return (
+    <StyledFormControl
+      id={id}
+      type={type}
+      placeholder={placeholder}
+      className={className}
+      {...layoutProps}
+    />
+  );
+};
 
 Input.propTypes = {
+  ...spacingPropTypes,
   className: PropTypes.string,
   type: PropTypes.string,
   id: PropTypes.string.isRequired,
