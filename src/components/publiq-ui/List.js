f02fd27d702ddf9e05cb1464e31f2a75cee85c49
fulldev1023 <simbadevlop@gmail.com>
2020-11-09 17:35:32 +0100

    Add spacing to List

--- src/components/publiq-ui/List.js
+++ src/components/publiq-ui/List.js
@@ -1,11 +1,15 @@
 import PropTypes from 'prop-types';
+import { getLayoutProps } from './Box';
 import { Stack, stackProps } from './Stack';
 
-const List = ({ children, spacing, className }) => (
-  <Stack className={className} spacing={spacing} as="ul">
-    {children}
-  </Stack>
-);
+const List = ({ children, spacing, className, ...props }) => {
+  const layoutProps = getLayoutProps(props);
+  return (
+    <Stack className={className} spacing={spacing} as="ul" {...layoutProps}>
+      {children}
+    </Stack>
+  );
+};
 
 List.propTypes = {
   ...stackProps,
