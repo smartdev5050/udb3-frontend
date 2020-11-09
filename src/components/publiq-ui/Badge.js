a69dc00c6db1af69fa67e711aa1f734e070f9c44
fulldev1023 <simbadevlop@gmail.com>
2020-11-09 16:54:45 +0100

    Add spacing to badge

--- src/components/publiq-ui/Badge.js
+++ src/components/publiq-ui/Badge.js
@@ -1,17 +1,32 @@
 import { Badge as BootstrapBadge } from 'react-bootstrap';
 import PropTypes from 'prop-types';
+import { pick } from 'lodash';
+import { spacingProps, spacingPropTypes } from './Box';
+import styled from 'styled-components';
 
 const BadgeVariants = {
   DANGER: 'danger',
 };
 
-const Badge = ({ children, className, variant }) => (
-  <BootstrapBadge className={className} variant={variant}>
-    {children}
-  </BootstrapBadge>
-);
+const StyledBootstrapBadge = styled(BootstrapBadge)`
+  ${spacingProps};
+`;
+
+const Badge = ({ children, className, variant, ...props }) => {
+  const layoutProps = pick(props, Object.keys(spacingPropTypes));
+  return (
+    <StyledBootstrapBadge
+      className={className}
+      variant={variant}
+      {...layoutProps}
+    >
+      {children}
+    </StyledBootstrapBadge>
+  );
+};
 
 Badge.propTypes = {
+  ...spacingPropTypes,
   className: PropTypes.string,
   children: PropTypes.node,
   variant: PropTypes.string,
