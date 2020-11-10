55f582638fea8a445101cbc6915cb11326da36e1
fulldev1023 <simbadevlop@gmail.com>
2020-11-10 11:26:47 +0100

    Add boxProps to Panel

--- src/components/publiq-ui/Panel.js
+++ src/components/publiq-ui/Panel.js
@@ -1,20 +1,25 @@
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
 import { getValueFromTheme } from './theme';
+import { boxProps, boxPropTypes, getBoxProps } from './Box';
 
 const getValue = getValueFromTheme('panel');
 
 const StyledSection = styled.section`
   border: 1px solid ${getValue('borderColor')};
-  margin-bottom: 1rem;
   box-shadow: 0 1px 1px rgba(0, 0, 0, 0.05);
+
+  ${boxProps}
 `;
 
-const Panel = ({ children, className }) => (
-  <StyledSection className={className}>{children}</StyledSection>
+const Panel = ({ children, className, ...props }) => (
+  <StyledSection className={className} {...getBoxProps(props)} marginBottom={4}>
+    {children}
+  </StyledSection>
 );
 
 Panel.propTypes = {
+  ...boxPropTypes,
   className: PropTypes.string,
   children: PropTypes.node,
 };
