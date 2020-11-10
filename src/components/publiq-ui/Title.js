c842792055728b348cce51bf68d94deed5c560de
fulldev1023 <simbadevlop@gmail.com>
2020-11-10 11:22:13 +0100

    Add boxProps to Title

--- src/components/publiq-ui/Title.js
+++ src/components/publiq-ui/Title.js
@@ -1,28 +1,34 @@
 import PropTypes from 'prop-types';
 import { Component } from 'react';
 import styled from 'styled-components';
+import { boxProps, boxPropTypes, getBoxProps } from './Box';
 
 const StyledComponent = styled(Component)`
   &.h1 {
     font-weight: 300;
     font-size: 1.6rem;
-    margin: 0;
   }
 
   &.h2 {
     font-weight: 700;
     font-size: 1.2rem;
-    margin: 0;
   }
+
+  ${boxProps};
 `;
 
-const Title = ({ size, children, className }) => (
-  <StyledComponent className={`h${size} ${className || ''}`} as={`h${size}`}>
+const Title = ({ size, children, className, ...props }) => (
+  <StyledComponent
+    className={`h${size} ${className || ''}`}
+    as={`h${size}`}
+    {...getBoxProps(props)}
+  >
     {children}
   </StyledComponent>
 );
 
 Title.propTypes = {
+  ...boxPropTypes,
   size: PropTypes.oneOf([1, 2]),
   className: PropTypes.string,
   children: PropTypes.node,
