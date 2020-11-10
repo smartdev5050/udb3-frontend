4228f0d0fcb488620a9498f001ecd1b7bafff0e2
fulldev1023 <simbadevlop@gmail.com>
2020-11-10 10:54:06 +0100

    Add boxProps to Small

--- src/components/publiq-ui/Small.js
+++ src/components/publiq-ui/Small.js
@@ -1,15 +1,21 @@
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import { boxProps, boxPropTypes, getBoxProps } from './Box';
 
 const StyledSmall = styled.small`
   font-size: 0.65rem;
+
+  ${boxProps};
 `;
 
-const Small = ({ children, className }) => (
-  <StyledSmall className={className}>{children}</StyledSmall>
+const Small = ({ children, className, ...props }) => (
+  <StyledSmall className={className} {...getBoxProps(props)}>
+    {children}
+  </StyledSmall>
 );
 
 Small.propTypes = {
+  ...boxPropTypes,
   className: PropTypes.string,
   children: PropTypes.node,
 };
