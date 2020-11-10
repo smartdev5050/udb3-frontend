4a4b1af3a1c48d73036c210181e817e4c2912d7e
fulldev1023 <simbadevlop@gmail.com>
2020-11-10 10:43:33 +0100

    Remove double border on ListItems

--- src/components/publiq-ui/List.js
+++ src/components/publiq-ui/List.js
@@ -1,17 +1,24 @@
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 import { getBoxProps } from './Box';
 import { Stack, stackProps } from './Stack';
 
+const StyledStack = styled(Stack)`
+  li:not(:last-child) {
+    border-bottom: none;
+  }
+`;
+
 const List = ({ children, spacing, className, ...props }) => {
   return (
-    <Stack
+    <StyledStack
       className={className}
       spacing={spacing}
       as="ul"
       {...getBoxProps(props)}
     >
       {children}
-    </Stack>
+    </StyledStack>
   );
 };
 
