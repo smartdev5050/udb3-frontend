8abe64ad08727358cfa9bfe04cfe79fadb317654
fulldev1023 <simbadevlop@gmail.com>
2020-11-10 11:22:41 +0100

    Add marginBottom to PageTitle

--- src/components/publiq-ui/PageTitle.js
+++ src/components/publiq-ui/PageTitle.js
@@ -1,19 +1,21 @@
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import { getValueFromTheme } from './theme';
 import { Title } from './Title';
+
+const getValue = getValueFromTheme('pageTitle');
 
 const StyledTitle = styled(Title)`
   &.h1 {
     width: 100%;
     line-height: 3.74rem;
-    color: #222;
-    border-bottom: 1px solid #ccc;
-    margin-bottom: 2rem;
+    color: ${getValue('color')};
+    border-bottom: 1px solid ${getValue('borderColor')};
   }
 `;
 
 const PageTitle = ({ children, className }) => (
-  <StyledTitle size={1} className={className}>
+  <StyledTitle size={1} className={className} marginBottom={5}>
     {children}
   </StyledTitle>
 );
