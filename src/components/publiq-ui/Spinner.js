7bea6037348aadc46c35bad51586362125025079
fulldev1023 <simbadevlop@gmail.com>
2020-11-10 10:55:46 +0100

    Add boxProps to Spinner

--- src/components/publiq-ui/Spinner.js
+++ src/components/publiq-ui/Spinner.js
@@ -1,6 +1,7 @@
 import PropTypes from 'prop-types';
 import { Spinner as BootstrapSpinner } from 'react-bootstrap';
 import styled from 'styled-components';
+import { boxProps, boxPropTypes, getBoxProps } from './Box';
 
 const SpinnerVariants = {
   PRIMARY: 'primary',
@@ -14,17 +15,20 @@ const SpinnerSizes = {
 const StyledDiv = styled.div`
   width: 100%;
   align-items: center;
+
+  ${boxProps};
 `;
 
-const Spinner = ({ variant, size, className }) => {
+const Spinner = ({ variant, size, className, ...props }) => {
   return (
-    <StyledDiv className={className}>
+    <StyledDiv className={className} {...getBoxProps(props)}>
       <BootstrapSpinner animation="border" variant={variant} size={size} />
     </StyledDiv>
   );
 };
 
 Spinner.propTypes = {
+  ...boxPropTypes,
   variant: PropTypes.oneOf(Object.values(SpinnerVariants)),
   size: PropTypes.oneOf(Object.values(SpinnerSizes)),
   className: PropTypes.string,
