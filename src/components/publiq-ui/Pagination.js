50d5b8f2ce09c2a03ce3e0c2d767b741733e4249
fulldev1023 <simbadevlop@gmail.com>
2020-11-12 10:12:59 +0100

    Use Inline for Pagination

--- src/components/publiq-ui/Pagination.js
+++ src/components/publiq-ui/Pagination.js
@@ -1,41 +1,9 @@
 import { Pagination as BootstrapPagination } from 'react-bootstrap';
-import styled from 'styled-components';
 import PropTypes from 'prop-types';
 import { getValueFromTheme } from './theme';
-import { boxProps, boxPropTypes, getBoxProps } from './Box';
+import { getInlineProps, Inline, inlineProptypes } from './Inline';
 
 const getValue = getValueFromTheme(`pagination`);
-
-const StyledPagination = styled(BootstrapPagination)`
-  .page-link {
-    color: ${getValue('color')};
-    border-color: ${getValue('borderColor')};
-    padding: ${getValue('paddingY')} ${getValue('paddingX')};
-    &:hover {
-      background-color: ${getValue('hoverBackgroundColor')};
-      color: ${getValue('hoverColor')};
-    }
-    &:focus {
-      box-shadow: ${getValue('focusBoxShadow')};
-    }
-  }
-
-  & > .page-item.active > .page-link {
-    background-color: ${getValue('activeBackgroundColor')};
-    color: ${getValue('activeColor')};
-    border-color: ${getValue('activeBorderColor')};
-  }
-
-  .prev-btn {
-    margin-right: 0.2rem;
-  }
-
-  .next-btn {
-    margin-left: 0.2rem;
-  }
-
-  ${boxProps};
-`;
 
 const Pagination = ({
   className,
@@ -53,8 +21,42 @@ const Pagination = ({
   }
 
   return (
-    <StyledPagination className={className} {...getBoxProps(props)}>
-      <StyledPagination.Prev
+    <Inline
+      forwardedAs="ul"
+      css={`
+        .page-link {
+          color: ${getValue('color')};
+          border-color: ${getValue('borderColor')};
+          padding: ${getValue('paddingY')} ${getValue('paddingX')};
+
+          &:hover {
+            background-color: ${getValue('hoverBackgroundColor')};
+            color: ${getValue('hoverColor')};
+          }
+
+          &:focus {
+            box-shadow: ${getValue('focusBoxShadow')};
+          }
+        }
+
+        & > .page-item.active > .page-link {
+          background-color: ${getValue('activeBackgroundColor')};
+          color: ${getValue('activeColor')};
+          border-color: ${getValue('activeBorderColor')};
+        }
+
+        .prev-btn {
+          margin-right: 0.2rem;
+        }
+
+        .next-btn {
+          margin-left: 0.2rem;
+        }
+      `}
+      {...getInlineProps(props)}
+      className={className}
+    >
+      <BootstrapPagination.Prev
         className="prev-btn"
         disabled={currentPage === 1}
         onClick={() => {
@@ -64,9 +66,9 @@ const Pagination = ({
         }}
       >
         {prevText}
-      </StyledPagination.Prev>
+      </BootstrapPagination.Prev>
       {pages.map((page) => (
-        <StyledPagination.Item
+        <BootstrapPagination.Item
           key={page}
           active={page === currentPage}
           onClick={() => {
@@ -74,9 +76,9 @@ const Pagination = ({
           }}
         >
           {page}
-        </StyledPagination.Item>
+        </BootstrapPagination.Item>
       ))}
-      <StyledPagination.Next
+      <BootstrapPagination.Next
         className="next-btn"
         disabled={currentPage === pages.length}
         onClick={() => {
@@ -86,13 +88,13 @@ const Pagination = ({
         }}
       >
         {nextText}
-      </StyledPagination.Next>
-    </StyledPagination>
+      </BootstrapPagination.Next>
+    </Inline>
   );
 };
 
 Pagination.propTypes = {
-  ...boxPropTypes,
+  ...inlineProptypes,
   className: PropTypes.string,
   currentPage: PropTypes.number.isRequired,
   totalItems: PropTypes.number.isRequired,
