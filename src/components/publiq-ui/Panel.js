4f2ad015e81305f04749d74f0a662e4aeaf42b87
fulldev1023 <simbadevlop@gmail.com>
2020-11-12 10:17:38 +0100

    Use Box for Panel

--- src/components/publiq-ui/Panel.js
+++ src/components/publiq-ui/Panel.js
@@ -1,21 +1,21 @@
 import PropTypes from 'prop-types';
-import styled from 'styled-components';
 import { getValueFromTheme } from './theme';
-import { boxProps, boxPropTypes, getBoxProps } from './Box';
+import { Box, boxPropTypes, getBoxProps } from './Box';
 
 const getValue = getValueFromTheme('panel');
 
-const StyledSection = styled.section`
-  border: 1px solid ${getValue('borderColor')};
-  box-shadow: 0 1px 1px rgba(0, 0, 0, 0.05);
-
-  ${boxProps}
-`;
-
 const Panel = ({ children, className, ...props }) => (
-  <StyledSection className={className} {...getBoxProps(props)} marginBottom={4}>
+  <Box
+    css={`
+      border: 1px solid ${getValue('borderColor')};
+      box-shadow: 0 1px 1px rgba(0, 0, 0, 0.05);
+    `}
+    className={className}
+    {...getBoxProps(props)}
+    marginBottom={4}
+  >
     {children}
-  </StyledSection>
+  </Box>
 );
 
 Panel.propTypes = {
