596d2149c3ca60fd53caca3e11b6106af9d251ce
fulldev1023 <simbadevlop@gmail.com>
2020-11-13 15:20:50 +0100

    Use Styled link

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -2,17 +2,42 @@ import { Stack } from './publiq-ui/Stack';
 import { Link } from './publiq-ui/Link';
 import { useTranslation } from 'react-i18next';
 import { Icon, IconVariants } from './publiq-ui/Icon';
+import { getValueFromTheme } from './publiq-ui/theme';
 
 const SideBar = () => {
   const { t } = useTranslation();
+
+  const getValueForLink = getValueFromTheme('sideBarLink');
+  const getValueForSideBar = getValueFromTheme('sideBar');
+
+  const StyledLink = (props) => (
+    <Link
+      {...props}
+      padding={2}
+      css={`
+        width: 100%;
+        display: inline-block;
+        align-items: center;
+        text-decoration: none;
+        color: ${getValueForLink('color')};
+
+        &:hover {
+          color: ${getValueForLink('hover.color')};
+          background-color: ${getValueForLink('hover.backgroundColor')};
+          text-decoration: none;
+        }
+      `}
+    />
+  );
+
   return (
     <Stack
       css={`
         position: relative;
         width: 230px;
-        background-color: #c0120c;
+        background-color: ${getValueForSideBar('backgroundColor')};
         height: 100vh;
-        color: #fff;
+        color: ${getValueForSideBar('color')};
         text-align: left;
         z-index: 2000;
         padding: 5px;
@@ -28,65 +53,71 @@ const SideBar = () => {
           src="/assets/udb-logo-mobile.svg"
           alt="Uitdatabank"
           className="udb-logo-mobile"
+          css="display: none;"
         />
       </Link>
-      <Stack as="ul">
+      <Stack as="ul" margin={3} marginBottom={6}>
         <li>
-          <Link href="/dashboard">
+          <StyledLink href="/dashboard">
             <Icon variant={IconVariants.HOME} marginRight={3} />
             <span>{t('menu.home')}</span>
-          </Link>
+          </StyledLink>
         </li>
         <li>
-          <Link href="/event">
+          <StyledLink href="/event">
             <Icon variant={IconVariants.PLUS_CIRCLE} marginRight={3} />
             <span>{t('menu.add')}</span>
-          </Link>
+          </StyledLink>
         </li>
         <li>
-          <Link href="/search">
+          <StyledLink href="/search">
             <Icon variant={IconVariants.SEARCH} marginRight={3} />
             <span>{t('menu.search')}</span>
-          </Link>
+          </StyledLink>
         </li>
       </Stack>
-      <p>{t('menu.management')}</p>
-      <Stack as="ul">
+      <Stack
+        as="ul"
+        css={`
+          border-top: 1px solid #900d09;
+        `}
+      >
+        <p>{t('menu.management')}</p>
         <li>
-          <Link href="/manage/moderation/overview">
+          <StyledLink href="/manage/moderation/overview">
             <Icon variant={IconVariants.FLAG} marginRight={3} />
             <span>{t('menu.validate')}</span>
-          </Link>
+          </StyledLink>
         </li>
         <li>
-          <Link href="/manage/users/overview">
+          <StyledLink href="/manage/users/overview">
             <Icon variant={IconVariants.USER} marginRight={3} />
             <span>{t('menu.users')}</span>
-          </Link>
+          </StyledLink>
         </li>
         <li>
-          <Link href="/manage/roles/overview">
+          <StyledLink href="/manage/roles/overview">
             <Icon variant={IconVariants.USERS} marginRight={3} />
             <span>{t('menu.roles')}</span>
-          </Link>
+          </StyledLink>
         </li>
         <li>
-          <Link href="/manage/labels/overview">
+          <StyledLink href="/manage/labels/overview">
             <Icon variant={IconVariants.TAG} marginRight={3} />
             <span>{t('menu.labels')}</span>
-          </Link>
+          </StyledLink>
         </li>
         <li>
-          <Link href="/manage/organizations">
+          <StyledLink href="/manage/organizations">
             <Icon variant={IconVariants.SLIDE_SHARE} marginRight={3} />
             <span>{t('menu.organizations')}</span>
-          </Link>
+          </StyledLink>
         </li>
         <li>
-          <Link href="/manage/productions">
+          <StyledLink href="/manage/productions">
             <Icon variant={IconVariants.LAYER_GROUP} marginRight={3} />
             <span>{t('menu.productions')}</span>
-          </Link>
+          </StyledLink>
         </li>
       </Stack>
     </Stack>
