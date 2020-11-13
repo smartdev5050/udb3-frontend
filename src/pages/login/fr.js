7dd06feb441925d1168999a2d5016535269eadc6
fulldev1023 <simbadevlop@gmail.com>
2020-11-13 11:16:31 +0100

    Use cookies for udb-language

--- src/pages/login/fr.js
+++ src/pages/login/fr.js
@@ -1,9 +1,17 @@
+import { useCookies } from 'react-cookie';
 import { LoginButton } from '../../components/LoginButton';
 
 const Fr = () => {
+  const cookieOptions = {
+    path: '/',
+    maxAge: 60 * 60 * 24 * 30,
+  };
+  const [, setCookie] = useCookies(['udb-language']);
+  setCookie('udb-language', 'fr', cookieOptions);
+
   return (
     <div>
-      <LoginButton language="nl" />
+      <LoginButton language="fr" />
     </div>
   );
 };
--- src/pages/login/nl.js
+++ src/pages/login/nl.js
@@ -1,6 +1,14 @@
+import { useCookies } from 'react-cookie';
 import { LoginButton } from '../../components/LoginButton';
 
 const Nl = () => {
+  const cookieOptions = {
+    path: '/',
+    maxAge: 60 * 60 * 24 * 30,
+  };
+  const [, setCookie] = useCookies(['udb-language']);
+  setCookie('udb-language', 'nl', cookieOptions);
+
   return (
     <div>
       <LoginButton language="nl" />
