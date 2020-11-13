6cd3c4dd8bf9d04ff3a94397538e401dbb70d06b
fulldev1023 <simbadevlop@gmail.com>
2020-11-13 10:54:56 +0100

    Add env variables to next config

--- next.config.js
+++ next.config.js
@@ -5,4 +5,13 @@ module.exports = {
     '@storybook/addon-essentials',
     '@storybook/addon-a11y',
   ],
+  env: {
+    API_KEY: process.env.API_KEY,
+    API_URL: process.env.API_URL,
+    LEGACY_APP_URL: process.env.LEGACY_APP_URL,
+    AUTH_URL: process.env.AUTH_URL,
+    SOCKET_URL: process.env.SOCKET_URL,
+    NEW_FEATURES_URL: process.env.NEW_FEATURES_URL,
+    ENV_NAME: process.env.ENV_NAME,
+  },
 };
