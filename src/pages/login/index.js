b2fa2b9c59a53f0820709591fe5c93aacc9c35dc
fulldev1023 <simbadevlop@gmail.com>
2020-11-13 11:16:19 +0100

    Use useEffect to push router

--- src/pages/login/index.js
+++ src/pages/login/index.js
@@ -1,11 +1,15 @@
 import i18next from 'i18next';
 import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 
 const Index = () => {
   const router = useRouter();
 
-  const locale = i18next.language.split('-')[0] || 'nl';
-  router.push(`/login/${locale}`);
+  useEffect(() => {
+    const locale = i18next.language.split('-')[0];
+    router.push(`/login/${locale}`);
+  }, []);
+
   return <div />;
 };
 
