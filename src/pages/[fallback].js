20589baccfe9c39c23929e0f3f9d283d02a32219
fulldev1023 <simbadevlop@gmail.com>
2020-11-13 13:03:07 +0100

    Fix error useRouter

--- src/pages/[fallback].js
+++ src/pages/[fallback].js
@@ -2,38 +2,44 @@ import Default from '../layouts/Default';
 import { useRouter } from 'next/router';
 import { useCookies } from 'react-cookie';
 import i18next from 'i18next';
+import { useEffect } from 'react';
 
 const Fallback = () => {
+  let iframe;
   const router = useRouter();
   const [cookies] = useCookies(['token']);
 
-  if (!cookies.token) {
-    router.push('/login');
-  }
+  useEffect(() => {
+    if (!cookies.token) {
+      router.push('/login');
+    }
 
-  const queryString = new URLSearchParams({
-    ...router.query,
-    jwt: cookies.token,
-    lang: i18next.language,
-  }).toString();
+    const queryString = new URLSearchParams({
+      ...router.query,
+      jwt: cookies.token,
+      lang: i18next.language,
+    }).toString();
 
-  const path = router.asPath ? router.asPath : '';
-  const parsedQueryString = queryString ? `?${queryString}` : '';
-  const generatedPath = `${process.env.LEGACY_APP_URL}${path}${parsedQueryString}`;
-  const iframe = `<iframe src="${generatedPath}" />`;
+    const path = router.asPath ? router.asPath : '';
+    const parsedQueryString = queryString ? `?${queryString}` : '';
+    const generatedPath = `${process.env.LEGACY_APP_URL}${path}${parsedQueryString}`;
+    iframe = `<iframe src="${generatedPath}" />`;
+  }, []);
 
   return (
     <Default>
-      <div
-        css={`
-          iframe {
-            height: 100vh;
-            width: 100%;
-            border: 0;
-          }
-        `}
-        dangerouslySetInnerHTML={{ __html: iframe }}
-      />
+      {iframe && (
+        <div
+          css={`
+            iframe {
+              height: 100vh;
+              width: 100%;
+              border: 0;
+            }
+          `}
+          dangerouslySetInnerHTML={{ __html: iframe }}
+        />
+      )}
     </Default>
   );
 };
