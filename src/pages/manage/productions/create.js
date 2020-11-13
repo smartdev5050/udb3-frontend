6aa5612bf4b82921ca3b05aa11a6c5280482767f
fulldev1023 <simbadevlop@gmail.com>
2020-11-13 10:56:48 +0100

    Add production pages

---
+++ src/pages/manage/productions/create.js
@@ -0,0 +1,12 @@
+import Default from '../../../layouts/Default';
+import { Title } from '../../../components/publiq-ui/Title';
+
+const Create = () => {
+  return (
+    <Default>
+      <Title>Productions - create</Title>
+    </Default>
+  );
+};
+
+export default Create;
---
+++ src/pages/manage/productions/index.js
@@ -0,0 +1,12 @@
+import Default from '../../../layouts/Default';
+import { Title } from '../../../components/publiq-ui/Title';
+
+const Index = () => {
+  return (
+    <Default>
+      <Title>Productions</Title>
+    </Default>
+  );
+};
+
+export default Index;
