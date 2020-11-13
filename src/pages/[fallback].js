2653bd2782f9d109ccfbe36061c5036ac0fab199
fulldev1023 <simbadevlop@gmail.com>
2020-11-13 10:57:03 +0100

    Add fallback page

---
+++ src/pages/[fallback].js
@@ -0,0 +1,41 @@
+import Default from '../layouts/Default';
+import { useRouter } from 'next/router';
+import { useCookies } from 'react-cookie';
+import i18next from 'i18next';
+
+const Fallback = () => {
+  const router = useRouter();
+  const [cookies] = useCookies(['token']);
+
+  if (!cookies.token) {
+    router.push('/login');
+  }
+
+  const queryString = new URLSearchParams({
+    ...router.query,
+    jwt: cookies.token,
+    lang: i18next.language,
+  }).toString();
+
+  const path = router.asPath ? router.asPath : '';
+  const parsedQueryString = queryString ? `?${queryString}` : '';
+  const generatedPath = `${process.env.LEGACY_APP_URL}${path}${parsedQueryString}`;
+  const iframe = `<iframe src="${generatedPath}" />`;
+
+  return (
+    <Default>
+      <div
+        css={`
+          iframe {
+            height: 100vh;
+            width: 100%;
+            border: 0;
+          }
+        `}
+        dangerouslySetInnerHTML={{ __html: iframe }}
+      />
+    </Default>
+  );
+};
+
+export default Fallback;
