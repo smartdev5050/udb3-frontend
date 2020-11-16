85c01451699e175665ac5c7a12ad242beacf0e98
fulldev1023 <simbadevlop@gmail.com>
2020-11-16 11:31:24 +0100

    Refactor Sidebar

--- src/components/SideBar.js
+++ src/components/SideBar.js
@@ -1,47 +1,150 @@
+import PropTypes from 'prop-types';
 import { Stack } from './publiq-ui/Stack';
 import { Link } from './publiq-ui/Link';
+import { List } from './publiq-ui/List';
 import { useTranslation } from 'react-i18next';
-import { Icon, IconVariants } from './publiq-ui/Icon';
+import { Icon, Icons } from './publiq-ui/Icon';
 import { getValueFromTheme } from './publiq-ui/theme';
+import { ListItem } from './publiq-ui/ListItem';
+import { Box } from './publiq-ui/Box';
+import { Title } from './publiq-ui/Title';
+
+const getValueForMenuItemLink = getValueFromTheme('menuItemLink');
+const getValueForSideBar = getValueFromTheme('sideBar');
+const getValueForMenu = getValueFromTheme('menu');
+
+const MenuItem = ({ href, iconName, children }) => {
+  return (
+    <ListItem>
+      <Link
+        href={href}
+        css={`
+          display: flex;
+          width: 100%;
+          text-decoration: none;
+          color: ${getValueForMenuItemLink('color')};
+          padding: 4px;
+
+          &:hover {
+            color: ${getValueForMenuItemLink('hover.color')};
+            background-color: ${getValueForMenuItemLink(
+              'hover.backgroundColor',
+            )};
+            text-decoration: none;
+          }
+        `}
+      >
+        <Icon name={iconName} marginRight={3} />
+        <Box as="span">{children}</Box>
+      </Link>
+    </ListItem>
+  );
+};
+
+MenuItem.propTypes = {
+  href: PropTypes.string,
+  iconName: PropTypes.string,
+  children: PropTypes.node,
+};
+
+const Menu = ({ items = [], title, ...props }) => {
+  const Content = (contentProps) => (
+    <List {...contentProps}>
+      {items.map((menuItem) => (
+        <MenuItem key={menuItem.href} {...menuItem} />
+      ))}
+    </List>
+  );
+
+  if (!title) return <Content {...props} />;
+
+  return (
+    <Stack {...props}>
+      <Title
+        size={2}
+        css={`
+          font-size: 13px;
+          font-weight: 400;
+          text-transform: uppercase;
+          opacity: 0.5;
+        `}
+      >
+        {title}
+      </Title>
+      <Content />
+    </Stack>
+  );
+};
+
+Menu.propTypes = {
+  items: PropTypes.array,
+  title: PropTypes.string,
+};
 
 const SideBar = () => {
   const { t } = useTranslation();
 
-  const getValueForLink = getValueFromTheme('sideBarLink');
-  const getValueForSideBar = getValueFromTheme('sideBar');
+  const userMenu = [
+    {
+      href: '/dashboard',
+      iconName: Icons.HOME,
+      children: t('menu.home'),
+    },
+    {
+      href: '/event',
+      iconName: Icons.PLUS_CIRCLE,
+      children: t('menu.add'),
+    },
+    {
+      href: '/search',
+      iconName: Icons.SEARCH,
+      children: t('menu.search'),
+    },
+  ];
 
-  const StyledLink = (props) => (
-    <Link
-      {...props}
-      padding={2}
-      css={`
-        width: 100%;
-        display: inline-block;
-        align-items: center;
-        text-decoration: none;
-        color: ${getValueForLink('color')};
-
-        &:hover {
-          color: ${getValueForLink('hover.color')};
-          background-color: ${getValueForLink('hover.backgroundColor')};
-          text-decoration: none;
-        }
-      `}
-    />
-  );
+  const manageMenu = [
+    {
+      href: '/manage/moderation/overview',
+      iconName: Icons.FLAG,
+      children: t('menu.validate'),
+    },
+    {
+      href: '/manage/users/overview',
+      iconName: Icons.USER,
+      children: t('menu.users'),
+    },
+    {
+      href: '/manage/roles/overview',
+      iconName: Icons.USERS,
+      children: t('menu.roles'),
+    },
+    {
+      href: '/manage/labels/overview',
+      iconName: Icons.TAG,
+      children: t('menu.labels'),
+    },
+    {
+      href: '/manage/organizations',
+      iconName: Icons.SLIDE_SHARE,
+      children: t('menu.organizations'),
+    },
+    {
+      href: '/manage/productions',
+      iconName: Icons.LAYER_GROUP,
+      children: t('menu.productions'),
+    },
+  ];
 
   return (
     <Stack
       css={`
-        position: relative;
         width: 230px;
         background-color: ${getValueForSideBar('backgroundColor')};
         height: 100vh;
         color: ${getValueForSideBar('color')};
-        text-align: left;
         z-index: 2000;
-        padding: 5px;
       `}
+      padding={2}
     >
       <Link href="/dashboard">
         <img
@@ -56,69 +159,16 @@ const SideBar = () => {
           css="display: none;"
         />
       </Link>
-      <Stack as="ul" margin={3} marginBottom={6}>
-        <li>
-          <StyledLink href="/dashboard">
-            <Icon variant={IconVariants.HOME} marginRight={3} />
-            <span>{t('menu.home')}</span>
-          </StyledLink>
-        </li>
-        <li>
-          <StyledLink href="/event">
-            <Icon variant={IconVariants.PLUS_CIRCLE} marginRight={3} />
-            <span>{t('menu.add')}</span>
-          </StyledLink>
-        </li>
-        <li>
-          <StyledLink href="/search">
-            <Icon variant={IconVariants.SEARCH} marginRight={3} />
-            <span>{t('menu.search')}</span>
-          </StyledLink>
-        </li>
-      </Stack>
       <Stack
-        as="ul"
+        spacing={4}
         css={`
-          border-top: 1px solid #900d09;
+          > :not(:first-child) {
+            border-top: 1px solid ${getValueForMenu('borderColor')};
+          }
         `}
       >
-        <p>{t('menu.management')}</p>
-        <li>
-          <StyledLink href="/manage/moderation/overview">
-            <Icon variant={IconVariants.FLAG} marginRight={3} />
-            <span>{t('menu.validate')}</span>
-          </StyledLink>
-        </li>
-        <li>
-          <StyledLink href="/manage/users/overview">
-            <Icon variant={IconVariants.USER} marginRight={3} />
-            <span>{t('menu.users')}</span>
-          </StyledLink>
-        </li>
-        <li>
-          <StyledLink href="/manage/roles/overview">
-            <Icon variant={IconVariants.USERS} marginRight={3} />
-            <span>{t('menu.roles')}</span>
-          </StyledLink>
-        </li>
-        <li>
-          <StyledLink href="/manage/labels/overview">
-            <Icon variant={IconVariants.TAG} marginRight={3} />
-            <span>{t('menu.labels')}</span>
-          </StyledLink>
-        </li>
-        <li>
-          <StyledLink href="/manage/organizations">
-            <Icon variant={IconVariants.SLIDE_SHARE} marginRight={3} />
-            <span>{t('menu.organizations')}</span>
-          </StyledLink>
-        </li>
-        <li>
-          <StyledLink href="/manage/productions">
-            <Icon variant={IconVariants.LAYER_GROUP} marginRight={3} />
-            <span>{t('menu.productions')}</span>
-          </StyledLink>
-        </li>
+        <Menu items={userMenu} />
+        <Menu items={manageMenu} title={t('menu.management')} />
       </Stack>
     </Stack>
   );
