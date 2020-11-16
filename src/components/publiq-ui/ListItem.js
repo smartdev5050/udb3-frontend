896c4f2bafaf6ca263c7b45674397ad948c47be9
fulldev1023 <simbadevlop@gmail.com>
2020-11-16 11:30:47 +0100

    Use Inline in ListItem

--- src/components/publiq-ui/ListItem.js
+++ src/components/publiq-ui/ListItem.js
@@ -1,30 +1,20 @@
 import PropTypes from 'prop-types';
-import { Box, boxPropTypes, getBoxProps } from './Box';
-import { getValueFromTheme } from './theme';
-
-const getValue = getValueFromTheme('listItem');
+import { Inline, getInlineProps, inlinePropTypes } from './Inline';
 
 const ListItem = ({ children, className, onClick, ...props }) => (
-  <Box
-    forwardedAs="li"
+  <Inline
+    as="li"
     tabIndex={0}
     className={className}
     onClick={onClick}
-    css={`
-      position: relative;
-      display: block;
-      padding: 0.75rem 1.25rem;
-      background-color: ${getValue('backgroundColor')};
-      border: 1px solid rgba(0, 0, 0, 0.125);
-    `}
-    {...getBoxProps(props)}
+    {...getInlineProps(props)}
   >
     {children}
-  </Box>
+  </Inline>
 );
 
 ListItem.propTypes = {
-  ...boxPropTypes,
+  ...inlinePropTypes,
   className: PropTypes.string,
   children: PropTypes.node,
   onClick: PropTypes.func,
