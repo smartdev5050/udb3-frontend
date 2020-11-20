a921c0d155599b57ce653480ef81f4b5a409ada3
fulldev1023 <simbadevlop@gmail.com>
2020-11-20 14:35:26 +0100

    Use correct queryCache

--- src/provider/ContextProvider.js
+++ src/provider/ContextProvider.js
@@ -4,7 +4,9 @@ import { I18nextProvider } from 'react-i18next';
 import i18n from '../i18n';
 import { ThemeProvider } from '../components/publiq-ui/ThemeProvider';
 import { CookiesProvider } from 'react-cookie';
-import { queryCache, ReactQueryCacheProvider } from 'react-query';
+import { QueryCache, ReactQueryCacheProvider } from 'react-query';
+
+const queryCache = new QueryCache();
 
 const providers = [
   ({ children }) => I18nextProvider({ children, i18n }),
