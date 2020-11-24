e89622d2fa1649ebfed076ef29ea511c7cf22b3a
fulldev1023 <simbadevlop@gmail.com>
2020-11-24 17:41:06 +0100

    Fix Page component

--- src/components/publiq-ui/Page.js
+++ src/components/publiq-ui/Page.js
@@ -1,24 +1,28 @@
 import PropTypes from 'prop-types';
-import { Stack } from './Stack';
+import { getStackProps, Stack, stackPropTypes } from './Stack';
 import { getValueFromTheme } from './theme';
 
 const getValue = getValueFromTheme('page');
 
-const Page = ({ children, className }) => (
+const Page = ({ children, className, ...props }) => (
   <Stack
-    as="main"
+    forwardedAs="main"
     className={className}
     css={`
       background-color: ${getValue('backgroundColor')};
       width: 100%;
       min-height: 100vh;
     `}
+    paddingLeft={4}
+    paddingRight={4}
+    {...getStackProps(props)}
   >
     {children}
   </Stack>
 );
 
 Page.propTypes = {
+  ...stackPropTypes,
   children: PropTypes.node,
   className: PropTypes.string,
 };
