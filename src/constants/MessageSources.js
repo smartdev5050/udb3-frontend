5d51da5dce6a53d4451c7302802d2ae7467ca290
fulldev1023 <simbadevlop@gmail.com>
2020-11-27 17:32:19 +0100

    Add constants directory and constants for messages

---
+++ src/constants/MessageSources.js
@@ -0,0 +1,5 @@
+const MessageSources = {
+  UDB: 'UDB',
+};
+
+export { MessageSources };
---
+++ src/constants/MessageTypes.js
@@ -0,0 +1,6 @@
+const MessageTypes = {
+  URL_CHANGED: 'URL_CHANGED',
+  JOB_ADDED: 'JOB_ADDED',
+};
+
+export { MessageTypes };
