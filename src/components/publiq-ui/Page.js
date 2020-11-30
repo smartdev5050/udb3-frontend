b20dc529dac965093493b3b3d3f45fa1afbc458c
fulldev1023 <simbadevlop@gmail.com>
2020-11-30 16:30:09 +0100

    Refactor PageTitle

--- src/components/publiq-ui/Page.js
+++ src/components/publiq-ui/Page.js
@@ -2,21 +2,52 @@ import PropTypes from 'prop-types';
 import { Stack } from './Stack';
 import { getValueFromTheme } from './theme';
 
-const getValue = getValueFromTheme('page');
+import { boxPropTypes, getBoxProps } from './Box';
+import { Title } from './Title';
+
+const getValueForPage = getValueFromTheme('page');
 
 const Page = ({ children, className }) => (
   <Stack
-    as="main"
+    forwardedAs="main"
     className={className}
+    flex={1}
+    backgroundColor={getValueForPage('backgroundColor')}
+    minHeight="100vh"
+    padding={3}
     css={`
-      background-color: ${getValue('backgroundColor')};
-      width: 100%;
-      min-height: 100vh;
+      overflow-x: hidden;
+      overflow-y: auto;
     `}
   >
     {children}
   </Stack>
 );
+
+const getValueForTitle = getValueFromTheme('pageTitle');
+
+const PageTitle = ({ children, className, ...props }) => (
+  <Title
+    size={1}
+    className={className}
+    color={getValueForTitle('color')}
+    css={`
+      line-height: 220%;
+      border-bottom: 1px solid ${getValueForTitle('borderColor')};
+    `}
+    {...getBoxProps(props)}
+  >
+    {children}
+  </Title>
+);
+
+PageTitle.propTypes = {
+  ...boxPropTypes,
+  className: PropTypes.string,
+  children: PropTypes.node,
+};
+
+Page.Title = PageTitle;
 
 Page.propTypes = {
   children: PropTypes.node,
--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,10 +1,9 @@
 import { Page } from '../components/publiq-ui/Page';
-import { PageTitle } from '../components/publiq-ui/PageTitle';
 
 const Dashboard = () => {
   return (
     <Page>
-      <PageTitle>Dashboard</PageTitle>
+      <Page.Title>Dashboard</Page.Title>
     </Page>
   );
 };
