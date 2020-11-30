e52cc071f2a7e49fb35776ea8e5fd4665a500a8c
fulldev1023 <simbadevlop@gmail.com>
2020-11-30 16:34:09 +0100

    Fix Image component

--- src/components/publiq-ui/Image.js
+++ src/components/publiq-ui/Image.js
@@ -1,33 +1,26 @@
 import PropTypes from 'prop-types';
 import { Box, boxPropTypes, getBoxProps } from './Box';
 
-const Image = ({ src, alt, width, height, className, ...props }) => {
-  return (
-    <Box
-      forwardedAs="img"
-      src={src}
-      alt={alt}
-      css={`
-        width: ${width}px;
-        height: ${height ? height + 'px' : 'auto'};
-      `}
-      className={className}
-      {...getBoxProps(props)}
-    />
-  );
-};
+const Image = ({ src, height, alt, className, ...props }) => (
+  <Box
+    as="img"
+    src={src}
+    alt={alt}
+    className={className}
+    {...getBoxProps(props)}
+  />
+);
 
 Image.propTypes = {
   ...boxPropTypes,
   src: PropTypes.string,
   alt: PropTypes.string,
-  width: PropTypes.number,
-  height: PropTypes.number,
   className: PropTypes.string,
 };
 
 Image.defaultProps = {
   width: 600,
+  height: 'auto',
 };
 
 export { Image };
