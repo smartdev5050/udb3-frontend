f2471e94cd3cfc58908597765c1bcc38948b1113
fulldev1023 <simbadevlop@gmail.com>
2020-12-07 12:08:32 +0100

    Add productions api call

---
+++ src/hooks/api/productions.js
@@ -0,0 +1,26 @@
+import { useAuthenticatedQuery } from './useAuthenticatedQuery';
+
+const getProductions = async (key, { headers, ...queryData }) => {
+  const url = new URL(`${process.env.NEXT_PUBLIC_API_URL}/productions/`);
+  url.search = new URLSearchParams({
+    ...queryData,
+  }).toString();
+  const res = await fetch(url, {
+    headers,
+  });
+  return await res.json();
+};
+
+const useGetProductions = (perPage, config = {}) =>
+  useAuthenticatedQuery(
+    [
+      'productions',
+      {
+        limit: perPage,
+      },
+    ],
+    getProductions,
+    config,
+  );
+
+export { useGetProductions };
