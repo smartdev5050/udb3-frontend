2d1b3dd9874528ddbb110824b2ba2003d2d8307a
fulldev1023 <simbadevlop@gmail.com>
2020-12-07 10:59:25 +0100

    Add input to productions index

--- src/pages/manage/productions/index.js
+++ src/pages/manage/productions/index.js
@@ -1,9 +1,23 @@
+import { useTranslation } from 'react-i18next';
 import { Page } from '../../../components/publiq-ui/Page';
+import { InputWithLabel } from '../../../components/publiq-ui/InputWithLabel';
 
 const Index = () => {
+  const { t } = useTranslation();
   return (
     <Page>
-      <Page.Title>Productions</Page.Title>
+      <Page.Title
+        actionTitle={t('productions.overview.create')}
+        actionHref="/manage/productions/create"
+      >
+        {t('menu.productions')}
+      </Page.Title>
+      <InputWithLabel
+        id="productions-overview-search"
+        placeholder={t('productions.overview.search.placeholder')}
+      >
+        {t('productions.overview.search.label')}
+      </InputWithLabel>
     </Page>
   );
 };
