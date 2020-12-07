14196c695f39c31f8543261b52c564e87f82e7aa
fulldev1023 <simbadevlop@gmail.com>
2020-12-07 12:08:58 +0100

    Add Productions and Events into index with basic styling

--- src/pages/manage/productions/index.js
+++ src/pages/manage/productions/index.js
@@ -1,9 +1,36 @@
 import { useTranslation } from 'react-i18next';
 import { Page } from '../../../components/publiq-ui/Page';
 import { InputWithLabel } from '../../../components/publiq-ui/InputWithLabel';
+import { Inline } from '../../../components/publiq-ui/Inline';
+import { Productions } from '../../../components/productions/index/Productions';
+import { useGetProductions } from '../../../hooks/api/productions';
+import { Events } from '../../../components/productions/index/Events';
+import { parseSpacing } from '../../../components/publiq-ui/Box';
+import { useEffect, useState } from 'react';
 
 const Index = () => {
   const { t } = useTranslation();
+  const { data: productionsData } = useGetProductions(15);
+  const rawProductions = productionsData?.member ?? [];
+  const events = [];
+
+  const [productions, setProductions] = useState([]);
+
+  useEffect(() => {
+    if (rawProductions.length === 0) {
+      return;
+    }
+
+    setProductions(
+      rawProductions.map((production, index) => {
+        if (index === 0) {
+          return { ...production, active: true };
+        }
+        return { ...production, active: false };
+      }),
+    );
+  }, [rawProductions]);
+
   return (
     <Page>
       <Page.Title
@@ -15,9 +42,17 @@ const Index = () => {
       <InputWithLabel
         id="productions-overview-search"
         placeholder={t('productions.overview.search.placeholder')}
+        marginBottom={4}
       >
         {t('productions.overview.search.label')}
       </InputWithLabel>
+      <Inline spacing={4}>
+        <Productions
+          width={`calc(40% - ${parseSpacing(4)()})`}
+          productions={productions}
+        />
+        <Events width="60%" productions={events} />
+      </Inline>
     </Page>
   );
 };
