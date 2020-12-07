7907e43ccac7291459f5533cdccf20da1305308c
fulldev1023 <simbadevlop@gmail.com>
2020-12-07 10:03:40 +0100

    Optimize rerendering

--- src/pages/[...params].js
+++ src/pages/[...params].js
@@ -1,8 +1,20 @@
 import { useRouter } from 'next/router';
 import i18next from 'i18next';
-import { useEffect, useState } from 'react';
 import { Box } from '../components/publiq-ui/Box';
 import { useCookiesWithOptions } from '../hooks/useCookiesWithOptions';
+import { memo, useMemo } from 'react';
+import PropTypes from 'prop-types';
+
+const IFrame = memo(({ url }) => (
+  <Box as="iframe" src={url} width="100%" height="100vh" />
+));
+
+IFrame.propTypes = {
+  url: PropTypes.string,
+};
+
+const prefixWhenNotEmpty = (value, prefix) =>
+  value ? `${prefix}${value}` : value;
 
 const Fallback = () => {
   const {
@@ -12,26 +24,22 @@ const Fallback = () => {
 
   const [cookies] = useCookiesWithOptions(['token']);
 
-  if (typeof window === 'undefined' || asPath === '/[...params]') {
-    return null;
-  }
+  const legacyPath = useMemo(() => {
+    const path = new URL(`http://localhost/${asPath}`).pathname;
 
-  const queryString = new URLSearchParams({
-    ...queryWithoutParams,
-    jwt: cookies.token,
-    lang: i18next.language,
-  }).toString();
+    const queryString = prefixWhenNotEmpty(
+      new URLSearchParams({
+        ...queryWithoutParams,
+        jwt: cookies.token,
+        lang: i18next.language,
+      }),
+      '?',
+    );
 
-  const path = asPath
-    ? new URL(`${window.location.protocol}//${window.location.host}${asPath}`)
-        .pathname
-    : '';
+    return `${process.env.NEXT_PUBLIC_LEGACY_APP_URL}${path}${queryString}`;
+  }, [asPath, cookies.token, i18next.language]);
 
-  const parsedQueryString = queryString ? `?${queryString}` : '';
-
-  const legacyPath = `${process.env.NEXT_PUBLIC_LEGACY_APP_URL}${path}${parsedQueryString}`;
-
-  return <Box as="iframe" src={legacyPath} width="100%" height="100vh" />;
+  return <IFrame url={legacyPath} />;
 };
 
 export default Fallback;
