0bc94940f128d40f438f7ae8917b6ce47fc6e48a
fulldev1023 <simbadevlop@gmail.com>
2020-12-08 17:26:27 +0100

    Add loading props and spinners to Events and Productions

--- src/components/productions/index/Events.js
+++ src/components/productions/index/Events.js
@@ -3,18 +3,34 @@ import { useTranslation } from 'react-i18next';
 import { List } from '../../publiq-ui/List';
 import { getStackProps, Stack, stackPropTypes } from '../../publiq-ui/Stack';
 import { Title } from '../../publiq-ui/Title';
-import { Spinner } from '../../publiq-ui/Spinner';
 import { CheckboxWithLabel } from '../../publiq-ui/CheckboxWithLabel';
 import { Button, ButtonVariants } from '../../publiq-ui/Button';
 import { Icon, Icons } from '../../publiq-ui/Icon';
 import { useState } from 'react';
 import { getValueFromTheme } from '../../publiq-ui/theme';
 import { Panel } from '../../publiq-ui/Panel';
+import { Inline } from '../../publiq-ui/Inline';
+import { Text } from '../../publiq-ui/Text';
+import { useGetCalendarSummary } from '../../../hooks/api/events';
+import { Spinner } from '../../publiq-ui/Spinner';
 
 const getValue = getValueFromTheme('eventItem');
 
-const Event = ({ id, name, type, date, place, className }) => {
+const getEventType = (terms) => {
+  const foundTerm = terms.find((term) => term.domain === 'eventtype') || {};
+  return foundTerm.label ? foundTerm.label : '';
+};
+
+const Event = ({ id, name, type, location, className }) => {
+  const { i18n } = useTranslation();
+  const { data: period } = useGetCalendarSummary({
+    id,
+    locale: i18n?.language ?? '',
+    format: 'sm',
+  });
+
   const [isExpanded, setIsExpanded] = useState(false);
+
   const handleClickToggleExpand = () => {
     setIsExpanded((prevValue) => !prevValue);
   };
@@ -26,21 +42,41 @@ const Event = ({ id, name, type, date, place, className }) => {
       paddingRight={4}
       paddingBottom={3}
       paddingTop={3}
-      justifyContent="space-between"
       backgroundColor="white"
       className={className}
     >
-      <CheckboxWithLabel id={id} name={name}>
-        {name}
-      </CheckboxWithLabel>
-      <Button
-        onClick={handleClickToggleExpand}
-        variant={ButtonVariants.UNSTYLED}
-      >
-        <Icon name={isExpanded ? Icons.CHEVRON_DOWN : Icons.CHEVRON_RIGHT} />
-      </Button>
+      <Stack flex={1}>
+        <Inline justifyContent="space-between">
+          <CheckboxWithLabel id={id} name={name}>
+            {name}
+          </CheckboxWithLabel>
+          <Button
+            onClick={handleClickToggleExpand}
+            variant={ButtonVariants.UNSTYLED}
+          >
+            <Icon
+              name={isExpanded ? Icons.CHEVRON_DOWN : Icons.CHEVRON_RIGHT}
+            />
+          </Button>
+        </Inline>
+        {isExpanded && (
+          <Stack>
+            <Text>{`type: ${type}`}</Text>
+            <Text>{`period: ${period}`}</Text>
+            <Text>{`location: ${location}`}</Text>
+          </Stack>
+        )}
+      </Stack>
     </List.Item>
   );
+};
+
+Event.propTypes = {
+  id: PropTypes.string.isRequired,
+  name: PropTypes.string.isRequired,
+  type: PropTypes.string,
+  location: PropTypes.string,
+  className: PropTypes.string,
 };
 
 const Events = ({
@@ -50,37 +86,43 @@ const Events = ({
   className,
   ...props
 }) => {
-  const { t } = useTranslation();
-
-  if (loading) return <Spinner />;
+  const { t, i18n } = useTranslation();
 
   return (
     <Stack spacing={4} {...getStackProps(props)}>
-      <Title>
-        {t('productions.overview.events_in_production', {
-          productionName: activeProductionName,
-        })}
-      </Title>
-      <Panel>
-        <List>
-          {events.map((event, index) => (
-            <Event
-              key={event['@id']}
-              id={event['@id']}
-              name={event.name.nl}
-              css={
-                index !== events.length - 1
-                  ? (props) => {
-                      return `border-bottom: 1px solid ${getValue(
-                        'borderColor',
-                      )(props)};`;
-                    }
-                  : undefined
-              }
-            />
-          ))}
-        </List>
-      </Panel>
+      {loading ? (
+        <Spinner />
+      ) : (
+        <>
+          <Title>
+            {t('productions.overview.events_in_production', {
+              productionName: activeProductionName,
+            })}
+          </Title>
+          <Panel>
+            <List>
+              {events.map((event, index) => (
+                <Event
+                  key={event.id}
+                  id={event.id}
+                  name={event.name[i18n.language]}
+                  type={getEventType(event.terms)}
+                  location={event.location.name[i18n.language]}
+                  css={
+                    index !== events.length - 1
+                      ? (props) => {
+                          return `border-bottom: 1px solid ${getValue(
+                            'borderColor',
+                          )(props)};`;
+                        }
+                      : undefined
+                  }
+                />
+              ))}
+            </List>
+          </Panel>
+        </>
+      )}
     </Stack>
   );
 };
@@ -90,10 +132,12 @@ Events.propTypes = {
   events: PropTypes.array,
   activeProductionName: PropTypes.string,
   className: PropTypes.string,
+  loading: PropTypes.bool,
 };
 
 Events.defaultProps = {
   events: [],
+  loading: false,
 };
 
 export { Events };
--- src/components/productions/index/Productions.js
+++ src/components/productions/index/Productions.js
@@ -5,6 +5,7 @@ import { List } from '../../publiq-ui/List';
 import { useTranslation } from 'react-i18next';
 import { getValueFromTheme } from '../../publiq-ui/theme';
 import { Panel } from '../../publiq-ui/Panel';
+import { Spinner } from '../../publiq-ui/Spinner';
 
 const getValue = getValueFromTheme('productionItem');
 
@@ -12,44 +13,51 @@ const Productions = ({
   productions,
   onClickProduction,
   className,
+  loading,
   ...props
 }) => {
   const { t } = useTranslation();
   return (
     <Stack className={className} spacing={4} {...getStackProps(props)}>
-      <Title>{t('productions.overview.production')}</Title>
-      <Panel>
-        <List>
-          {productions.map((production, index) => (
-            <List.Item
-              paddingLeft={4}
-              paddingRight={4}
-              paddingBottom={3}
-              paddingTop={3}
-              color={production.active && getValue('activeColor')}
-              backgroundColor={
-                production.active
-                  ? getValue('activeBackgroundColor')
-                  : getValue('backgroundColor')
-              }
-              cursor="pointer"
-              css={
-                index !== productions.length - 1
-                  ? (props) => {
-                      return `border-bottom: 1px solid ${getValue(
-                        'borderColor',
-                      )(props)};`;
-                    }
-                  : undefined
-              }
-              key={production.production_id}
-              onClick={() => onClickProduction(production.production_id)}
-            >
-              {production.name}
-            </List.Item>
-          ))}
-        </List>
-      </Panel>
+      {loading ? (
+        <Spinner />
+      ) : (
+        <>
+          <Title>{t('productions.overview.production')}</Title>
+          <Panel>
+            <List>
+              {productions.map((production, index) => (
+                <List.Item
+                  paddingLeft={4}
+                  paddingRight={4}
+                  paddingBottom={3}
+                  paddingTop={3}
+                  color={production.active && getValue('activeColor')}
+                  backgroundColor={
+                    production.active
+                      ? getValue('activeBackgroundColor')
+                      : getValue('backgroundColor')
+                  }
+                  cursor="pointer"
+                  css={
+                    index !== productions.length - 1
+                      ? (props) => {
+                          return `border-bottom: 1px solid ${getValue(
+                            'borderColor',
+                          )(props)};`;
+                        }
+                      : undefined
+                  }
+                  key={production.production_id}
+                  onClick={() => onClickProduction(production.production_id)}
+                >
+                  {production.name}
+                </List.Item>
+              ))}
+            </List>
+          </Panel>
+        </>
+      )}
     </Stack>
   );
 };
@@ -59,10 +67,12 @@ Productions.propTypes = {
   productions: PropTypes.array,
   onClickProduction: PropTypes.func,
   className: PropTypes.string,
+  loading: PropTypes.bool,
 };
 
 Productions.defaultProps = {
   productions: [],
+  loading: false,
 };
 
 export { Productions };
