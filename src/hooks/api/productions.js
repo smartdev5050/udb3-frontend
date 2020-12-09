768987950f5257f99b9a5b73d743b3f2f7405172
fulldev1023 <simbadevlop@gmail.com>
2020-12-09 14:33:20 +0100

    Add useDeleteEventById and useDeleteEventsByIds to api calls hooks

--- src/hooks/api/productions.js
+++ src/hooks/api/productions.js
@@ -1,3 +1,5 @@
+import { fetchWithRedirect } from '../../utils/fetchWithRedirect';
+import { useAuthenticatedMutation } from './useAuthenicatedMutation';
 import { useAuthenticatedQuery } from './useAuthenticatedQuery';
 
 const getProductions = async (key, { headers, ...queryData }) => {
@@ -25,4 +27,36 @@ const useGetProductions = ({ name = '', start = 0, limit = 15 }, config = {}) =>
     config,
   );
 
-export { useGetProductions };
+const deleteEventById = async ({
+  productionId = '',
+  eventId = '',
+  headers,
+}) => {
+  const url = new URL(
+    `${process.env.NEXT_PUBLIC_API_URL}/productions/${productionId}/events/${eventId}`,
+  );
+
+  return await fetchWithRedirect(url, {
+    method: 'DELETE',
+    headers,
+  });
+};
+
+const useDeleteEventById = (config) =>
+  useAuthenticatedMutation(deleteEventById, config);
+
+const deleteEventsByIds = async ({
+  productionId = '',
+  eventIds = [],
+  headers,
+}) => {
+  const mappedEvents = eventIds.map((eventId) => {
+    return deleteEventById({ productionId, eventId, headers });
+  });
+  return await Promise.all(mappedEvents);
+};
+
+const useDeleteEventsByIds = (config) =>
+  useAuthenticatedMutation(deleteEventsByIds, config);
+
+export { useGetProductions, useDeleteEventById, useDeleteEventsByIds };
