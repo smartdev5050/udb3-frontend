accd30fa185a6f4de8a27232630c060cd297015f
fulldev1023 <simbadevlop@gmail.com>
2020-12-11 11:30:29 +0100

    Add tbody to Detailtable to be semantically correct

--- src/components/publiq-ui/DetailTable.js
+++ src/components/publiq-ui/DetailTable.js
@@ -14,27 +14,29 @@ const DetailTable = ({ items, className, ...props }) => {
       className={className}
       {...getStackProps(props)}
     >
-      {items.map(({ header, value }, index) => (
-        <Inline
-          key={index}
-          forwardedAs="tr"
-          padding={3}
-          css={
-            index !== items.length - 1
-              ? (props) => {
-                  return `border-bottom: 1px solid ${getValue('borderColor')(
-                    props,
-                  )};`;
-                }
-              : undefined
-          }
-        >
-          <Box as="th" minWidth={parseSpacing(7)()} fontWeight="bold">
-            {header}
-          </Box>
-          <Box as="td">{value}</Box>
-        </Inline>
-      ))}
+      <Stack as="tbody">
+        {items.map(({ header, value }, index) => (
+          <Inline
+            key={index}
+            forwardedAs="tr"
+            padding={3}
+            css={
+              index !== items.length - 1
+                ? (props) => {
+                    return `border-bottom: 1px solid ${getValue('borderColor')(
+                      props,
+                    )};`;
+                  }
+                : undefined
+            }
+          >
+            <Box as="th" minWidth={parseSpacing(7)()} fontWeight="bold">
+              {header}
+            </Box>
+            <Box as="td">{value}</Box>
+          </Inline>
+        ))}
+      </Stack>
     </Stack>
   );
 };
