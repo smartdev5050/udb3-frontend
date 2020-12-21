f52fe405f6dc1e3fd1d29faf9c2af7c927caf09f
fulldev1023 <simbadevlop@gmail.com>
2020-12-21 17:24:28 +0100

    Create separate layouts and set up hydration

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -12,7 +12,9 @@ import { ContextProvider } from '../provider/ContextProvider';
 import { I18nextProvider, useTranslation } from 'react-i18next';
 import i18n from '../i18n';
 import { CookiesProvider, Cookies } from 'react-cookie';
-import { QueryCache, QueryClient, QueryClientProvider } from 'react-query';
+import { QueryClient, QueryClientProvider } from 'react-query';
+import { Hydrate } from 'react-query/hydration';
+
 import { useEffect } from 'react';
 import { useCookiesWithOptions } from '../hooks/useCookiesWithOptions';
 import { useGetUser } from '../hooks/api/user';
@@ -66,7 +68,16 @@ const useHandleAuthentication = () => {
   }, [asPath]);
 };
 
-const Layout = ({ children }) => {
+const LoginLayout = ({ children }) => {
+  useChangeLanguage();
+  return children;
+};
+
+LoginLayout.propTypes = {
+  children: PropTypes.node,
+};
+
+const ApplicationLayout = ({ children }) => {
   const { asPath, ...router } = useRouter();
   const { cookies } = useCookiesWithOptions(['token']);
 
@@ -76,7 +87,6 @@ const Layout = ({ children }) => {
   });
   useHandleAuthentication();
 
-  if (asPath.startsWith('/login')) return children;
   if (!cookies.token) return null;
 
   return (
@@ -87,50 +97,55 @@ const Layout = ({ children }) => {
   );
 };
 
+ApplicationLayout.propTypes = {
+  children: PropTypes.node,
+};
+
+const Layout = ({ children }) => {
+  const { asPath } = useRouter();
+
+  if (asPath.startsWith('/login')) return <LoginLayout>{children}</LoginLayout>;
+  return <ApplicationLayout>{children}</ApplicationLayout>;
+};
+
 Layout.propTypes = {
   children: PropTypes.node,
 };
 
-const queryCache = new QueryCache();
-const queryClient = new QueryClient({ queryCache });
+const queryClient = new QueryClient();
 
 const isServer = () => typeof window === 'undefined';
 
-// eslint-disable-next-line react/prop-types
-const App = ({ Component, pageProps, cookies }) => (
-  <>
-    <GlobalStyle />
-    <ContextProvider
-      providers={[
-        [I18nextProvider, { i18n }],
-        ThemeProvider,
-        [CookiesProvider, { cookies: isServer() ? cookies : undefined }],
-        [QueryClientProvider, { client: queryClient }],
-      ]}
-    >
-      {/* <ReactQueryDevtools initialIsOpen={false} position="bottom-right" /> */}
-      <Layout>
-        <Component {...pageProps} />
-      </Layout>
-    </ContextProvider>
-  </>
-);
+const App = ({ Component, pageProps }) => {
+  return (
+    <>
+      <GlobalStyle />
+      <ContextProvider
+        providers={[
+          [I18nextProvider, { i18n }],
+          ThemeProvider,
+          [
+            CookiesProvider,
+            {
+              cookies: isServer() ? new Cookies(pageProps.cookies) : undefined,
+            },
+          ],
+          [QueryClientProvider, { client: queryClient }],
+          [Hydrate, { dehydratedState: pageProps.dehydratedState }],
+        ]}
+      >
+        {/* <ReactQueryDevtools initialIsOpen={false} position="bottom-right" /> */}
+        <Layout>
+          <Component {...pageProps} />
+        </Layout>
+      </ContextProvider>
+    </>
+  );
+};
 
-App.getInitialProps = async (props) => {
-  const { asPath, req, query, res } = props.ctx;
-  const cookies = new Cookies(req?.headers?.cookie);
-
-  const isUnAuthorized = !cookies.get('token') && !query?.jwt;
-  const isOnLogin = asPath.startsWith('/login') || asPath === '/[...params]';
-
-  if (!isOnLogin && isUnAuthorized && isServer()) {
-    res.writeHead(302, {
-      Location: '/login',
-    });
-    res.end();
-  }
-
-  return { cookies };
+App.propTypes = {
+  Component: PropTypes.node,
+  pageProps: PropTypes.object,
 };
 
 export default App;
