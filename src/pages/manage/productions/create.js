b7132f3d5754b61ee467e66c038c4a06a64efb95
fulldev1023 <simbadevlop@gmail.com>
2020-12-22 12:54:28 +0100

    Add cookie prefetch on server

--- src/pages/manage/productions/create.js
+++ src/pages/manage/productions/create.js
@@ -1,7 +1,28 @@
+import { Cookies } from 'react-cookie';
 import { Title } from '../../../components/publiq-ui/Title';
 
 const Create = () => {
   return <Title>Productions - create</Title>;
+};
+
+export const getServerSideProps = async ({ req, query }) => {
+  const { cookies } = new Cookies(req?.headers?.cookie);
+  const isUnAuthorized = !cookies.token && !query?.jwt;
+
+  if (isUnAuthorized) {
+    return {
+      redirect: {
+        destination: '/login',
+        permanent: false,
+      },
+    };
+  }
+
+  return {
+    props: {
+      cookies,
+    },
+  };
 };
 
 export default Create;
