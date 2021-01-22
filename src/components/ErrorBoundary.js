9d56887823370dabc15d74abe7adfca283a7f168
fulldev1023 <simbadevlop@gmail.com>
2021-01-22 17:21:39 +0100

    Redirect in ErrorBoundary when a unexpected error occures

--- src/components/ErrorBoundary.js
+++ src/components/ErrorBoundary.js
@@ -1,52 +1,23 @@
 import { Component } from 'react';
 import PropTypes from 'prop-types';
-import { Icon, Icons } from './publiq-ui/Icon';
-import { Title } from './publiq-ui/Title';
-import { Link } from './publiq-ui/Link';
-import { getValueFromTheme } from './publiq-ui/theme';
-import { Stack } from './publiq-ui/Stack';
-import { Translation } from 'react-i18next';
-
-const getValue = getValueFromTheme('pageError');
+import Router from 'next/router';
 
 class ErrorBoundary extends Component {
   constructor(props) {
     super(props);
-    this.state = { hasError: false };
+    this.state = {};
   }
 
-  static getDerivedStateFromError() {
-    return { hasError: true };
+  static getDerivedStateFromError(error) {
+    return { error };
   }
 
   render() {
-    if (this.state.hasError) {
-      return (
-        <Translation>
-          {({ t }) => (
-            <Stack
-              textAlign="center"
-              alignItems="center"
-              marginY={6}
-              spacing={3}
-              flex={1}
-              height="100vh"
-            >
-              <Icon
-                name={Icons.EXCLAMATION_TRIANGLE}
-                width="10rem"
-                height="auto"
-                color={getValue('iconColor')}
-              />
-              <Title size={1}>{t('error.title')}</Title>
-              {/* <Title size={2}>{t('error.sub_title')}</Title> */}
-              <Link href="/dashboard" width="max-content">
-                {t('error.redirect')}
-              </Link>
-            </Stack>
-          )}
-        </Translation>
-      );
+    if (this.state.error) {
+      Router.push({
+        pathname: '/error',
+        query: { errorMessage: this.state.error.message },
+      });
     }
 
     return this.props.children;
