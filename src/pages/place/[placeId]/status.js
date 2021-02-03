98a0d84a0a8a0ae390a1954b389a6bf88a7a90aa
fulldev1023 <simbadevlop@gmail.com>
2021-02-03 11:37:55 +0100

    Add onError to place/{placeid}/status

--- src/pages/place/[placeId]/status.js
+++ src/pages/place/[placeId]/status.js
@@ -2,12 +2,26 @@ import { EventStatus } from '../../../components/EventStatus';
 import { useRouter } from 'next/router';
 import { useGetPlaceById } from '../../../hooks/api/places';
 import { QueryStatus } from '../../../hooks/api/authenticated-query';
+import { useState } from 'react';
 
 const Status = () => {
   const router = useRouter();
   const { placeId } = router.query;
-  const { data: place = {}, status } = useGetPlaceById({ id: placeId });
-  return <EventStatus offer={place} loading={status === QueryStatus.LOADING} />;
+  const [errorMessage, setErrorMessage] = useState();
+  const handleError = (error) => {
+    setErrorMessage(error.message);
+  };
+  const { data: place = {}, status } = useGetPlaceById(
+    { id: placeId },
+    { onError: handleError },
+  );
+  return (
+    <EventStatus
+      offer={place}
+      loading={status === QueryStatus.LOADING}
+      errorMessage={errorMessage}
+    />
+  );
 };
 
 export default Status;
