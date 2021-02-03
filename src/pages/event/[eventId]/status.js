b40fe67ee8090c8670815f5ab277a65c8cfaddfa
fulldev1023 <simbadevlop@gmail.com>
2021-02-03 11:38:18 +0100

    Add onError to event/{eventId}/status

--- src/pages/event/[eventId]/status.js
+++ src/pages/event/[eventId]/status.js
@@ -2,13 +2,27 @@ import { useRouter } from 'next/router';
 import { useGetEventById } from '../../../hooks/api/events';
 import { EventStatus } from '../../../components/EventStatus';
 import { QueryStatus } from '../../../hooks/api/authenticated-query';
+import { useState } from 'react';
 
 const Status = () => {
   const router = useRouter();
   const { eventId } = router.query;
-  const { data: event = {}, status } = useGetEventById({ id: eventId });
+  const [errorMessage, setErrorMessage] = useState();
+  const handleError = (error) => {
+    setErrorMessage(error.message);
+  };
+  const { data: event = {}, status } = useGetEventById(
+    { id: eventId },
+    { onError: handleError },
+  );
 
-  return <EventStatus offer={event} loading={status === QueryStatus.LOADING} />;
+  return (
+    <EventStatus
+      offer={event}
+      loading={status === QueryStatus.LOADING}
+      errorMessage={errorMessage}
+    />
+  );
 };
 
 export default Status;
