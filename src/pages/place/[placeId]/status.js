9863eed3d7fd4a702783f51d3f73098866bddb5e
fulldev1023 <simbadevlop@gmail.com>
2021-02-03 10:39:39 +0100

    Use EventStatus component in /place/{placeId}/status

--- src/pages/place/[placeId]/status.js
+++ src/pages/place/[placeId]/status.js
@@ -1,10 +1,13 @@
-import { Text } from '../../../components/publiq-ui/Text';
+import { EventStatus } from '../../../components/EventStatus';
 import { useRouter } from 'next/router';
+import { useGetPlaceById } from '../../../hooks/api/places';
+import { QueryStatus } from '../../../hooks/api/authenticated-query';
 
 const Status = () => {
   const router = useRouter();
   const { placeId } = router.query;
-  return <Text>EventStatus place {placeId}</Text>;
+  const { data: place = {}, status } = useGetPlaceById({ id: placeId });
+  return <EventStatus offer={place} loading={status === QueryStatus.LOADING} />;
 };
 
 export default Status;
