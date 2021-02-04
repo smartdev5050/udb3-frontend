d8899c2190fddebb8359dc590e5111d6e2cfd1c7
fulldev1023 <simbadevlop@gmail.com>
2021-02-04 15:26:26 +0100

    Remove separate OfferStatus component
    # Conflicts:
    #	src/components/OfferStatus.js

--- src/pages/event/[eventId]/status.js
+++ src/pages/event/[eventId]/status.js
@@ -1,14 +1,25 @@
 import { useRouter } from 'next/router';
 import { useGetEventById } from '../../../hooks/api/events';
-import { OfferStatus } from '../../../components/OfferStatus';
 import { QueryStatus } from '../../../hooks/api/authenticated-query';
-import { useState } from 'react';
+import { useState, useMemo } from 'react';
 import { getApplicationServerSideProps } from '../../../utils/getApplicationServerSideProps';
+import { parseOfferType } from '../../../utils/parseOfferType';
+import { camelCase } from 'lodash';
+import { Alert, AlertVariants } from '../../../components/publiq-ui/Alert';
+import { Button, ButtonVariants } from '../../../components/publiq-ui/Button';
+import { Spinner } from '../../../components/publiq-ui/Spinner';
+import { Page } from '../../../components/publiq-ui/Page';
+import { SelectionTable } from '../../../components/publiq-ui/SelectionTable';
+import { useTranslation } from 'react-i18next';
 
 const Status = () => {
+  const { t, i18n } = useTranslation();
   const router = useRouter();
   const { eventId } = router.query;
+
   const [errorMessage, setErrorMessage] = useState();
+  const [, setSelectedOffers] = useState([]);
+
   const handleError = (error) => {
     setErrorMessage(error.message);
   };
@@ -17,12 +28,78 @@ const Status = () => {
     { onError: handleError },
   );
 
+  const name = event.name?.[i18n.language] ?? event.name?.[event.mainLanguage];
+  const { subEvent: subEvents = [] } = event;
+  const offerType = parseOfferType(event['@context'] ?? '');
+
+  const OfferStatusMap = useMemo(
+    () => ({
+      event: {
+        available: t('offerStatus.status.scheduled'),
+        temporarilyUnavailable: t('offerStatus.status.postponed'),
+        unavailable: t('offerStatus.status.cancelled'),
+      },
+      place: {
+        available: t('offerStatus.status.open'),
+        temporarilyUnavailable: t('offerStatus.status.temporarilyClosed'),
+        unavailable: t('offerStatus.status.permanentlyClosed'),
+      },
+    }),
+    [],
+  );
+
+  const columns = useMemo(
+    () => [
+      {
+        Header: 'Tijdstip',
+        accessor: 'time',
+      },
+      {
+        Header: 'Status',
+        accessor: 'status',
+      },
+    ],
+    [],
+  );
+
+  const data = useMemo(
+    () =>
+      subEvents.map((subEvent) => ({
+        time: 'Dinsdag 13 Februari 2021',
+        status:
+          OfferStatusMap[offerType]?.[camelCase(subEvent.status.type)] ?? '',
+      })),
+    [subEvents],
+  );
+
   return (
-    <OfferStatus
-      offer={event}
-      loading={status === QueryStatus.LOADING}
-      errorMessage={errorMessage}
-    />
+    <Page>
+      <Page.Title>{t('offerStatus.title', { name })}</Page.Title>
+      <Page.Content spacing={5}>
+        {status === QueryStatus.LOADING ? (
+          <Spinner marginTop={4} />
+        ) : errorMessage ? (
+          <Alert variant={AlertVariants.WARNING}>{errorMessage}</Alert>
+        ) : (
+          [
+            <Alert key="alert">{t('offerStatus.info')}</Alert>,
+            <SelectionTable
+              key="table"
+              columns={columns}
+              data={data}
+              onSelectionChanged={setSelectedOffers}
+            />,
+            <Button
+              key="button"
+              variant={ButtonVariants.SUCCESS}
+              width="max-content"
+            >
+              {t('offerStatus.modificationReady')}
+            </Button>,
+          ]
+        )}
+      </Page.Content>
+    </Page>
   );
 };
 
--- src/pages/place/[placeId]/status.js
+++ src/pages/place/[placeId]/status.js
@@ -1,14 +1,19 @@
-import { OfferStatus } from '../../../components/OfferStatus';
 import { useRouter } from 'next/router';
-import { useGetPlaceById } from '../../../hooks/api/places';
 import { QueryStatus } from '../../../hooks/api/authenticated-query';
 import { useState } from 'react';
 import { getApplicationServerSideProps } from '../../../utils/getApplicationServerSideProps';
+import { Alert, AlertVariants } from '../../../components/publiq-ui/Alert';
+import { Button, ButtonVariants } from '../../../components/publiq-ui/Button';
+import { Spinner } from '../../../components/publiq-ui/Spinner';
+import { Page } from '../../../components/publiq-ui/Page';
+import { useTranslation } from 'react-i18next';
+import { useGetPlaceById } from '../../../hooks/api/places';
 
 const Status = () => {
   const router = useRouter();
   const { placeId } = router.query;
   const [errorMessage, setErrorMessage] = useState();
+  const { t, i18n } = useTranslation();
   const handleError = (error) => {
     setErrorMessage(error.message);
   };
@@ -16,12 +21,30 @@ const Status = () => {
     { id: placeId },
     { onError: handleError },
   );
+  const name = place.name?.[i18n.language] ?? place.name?.[place.mainLanguage];
+
   return (
-    <OfferStatus
-      offer={place}
-      loading={status === QueryStatus.LOADING}
-      errorMessage={errorMessage}
-    />
+    <Page>
+      <Page.Title>{t('offerStatus.title', { name })}</Page.Title>
+      <Page.Content spacing={5}>
+        {status === QueryStatus.LOADING ? (
+          <Spinner marginTop={4} />
+        ) : errorMessage ? (
+          <Alert variant={AlertVariants.WARNING}>{errorMessage}</Alert>
+        ) : (
+          [
+            <Alert key="alert">{t('offerStatus.info')}</Alert>,
+            <Button
+              key="button"
+              variant={ButtonVariants.SUCCESS}
+              width="max-content"
+            >
+              {t('offerStatus.modificationReady')}
+            </Button>,
+          ]
+        )}
+      </Page.Content>
+    </Page>
   );
 };
 
