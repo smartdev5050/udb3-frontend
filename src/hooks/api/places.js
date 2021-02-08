6da14133794456e292d9b67a020d425680d0fd92
fulldev1023 <simbadevlop@gmail.com>
2021-02-08 13:30:41 +0100

    Add changeStatus api call to places

--- src/hooks/api/places.js
+++ src/hooks/api/places.js
@@ -1,5 +1,8 @@
 import { fetchFromApi } from '../../utils/fetchFromApi';
-import { useAuthenticatedQuery } from './authenticated-query';
+import {
+  useAuthenticatedMutation,
+  useAuthenticatedQuery,
+} from './authenticated-query';
 
 const getPlaceById = async ({ headers, id }) => {
   const res = await fetchFromApi({
@@ -20,4 +23,19 @@ const useGetPlaceById = ({ id }, configuration = {}) =>
     ...configuration,
   });
 
-export { useGetPlaceById };
+const changeStatus = async ({ headers, id, type, reason }) => {
+  const res = await fetchFromApi({
+    path: `/places/${id.toString()}/status`,
+    options: {
+      method: 'PUT',
+      headers,
+      body: JSON.stringify({ type, reason }),
+    },
+  });
+  return await res.json();
+};
+
+const useChangeStatus = (configuration = {}) =>
+  useAuthenticatedMutation({ mutationFn: changeStatus, ...configuration });
+
+export { useGetPlaceById, useChangeStatus };
