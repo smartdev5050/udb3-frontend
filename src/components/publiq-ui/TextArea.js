954d455c2238f36880dc34812fd89436f1c2f9be
fulldev1023 <simbadevlop@gmail.com>
2021-02-10 10:11:05 +0100

    Add disabled prop to TextArea

--- src/components/publiq-ui/TextArea.js
+++ src/components/publiq-ui/TextArea.js
@@ -4,7 +4,15 @@ import { getValueFromTheme } from './theme';
 
 const getValue = getValueFromTheme('textArea');
 
-const TextArea = ({ id, children, className, onInput, value, ...props }) => {
+const TextArea = ({
+  id,
+  children,
+  className,
+  onInput,
+  value,
+  disabled,
+  ...props
+}) => {
   return (
     <Box
       forwardedAs="textarea"
@@ -14,6 +22,7 @@ const TextArea = ({ id, children, className, onInput, value, ...props }) => {
       minHeight="4rem"
       onInput={onInput}
       value={value}
+      disabled={disabled}
       css={`
         border: 1px solid ${getValue('borderColor')};
       `}
@@ -29,6 +38,11 @@ TextArea.propTypes = {
   className: PropTypes.string,
   value: PropTypes.string,
   onInput: PropTypes.func,
+  disabled: PropTypes.bool,
+};
+
+TextArea.defaultProps = {
+  disabled: false,
 };
 
 export { TextArea };
