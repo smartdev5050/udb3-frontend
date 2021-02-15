0be52c2c63b4fbb11e990fc4ba7660ece9508485
fulldev1023 <simbadevlop@gmail.com>
2021-02-15 15:25:42 +0100

    Remove comments

--- jest.setup.js
+++ jest.setup.js
@@ -2,17 +2,14 @@ import '@testing-library/jest-dom';
 import fetchMock from 'jest-fetch-mock';
 import { setConfig } from 'next/config';
 
-// console.log(process.env);
 fetchMock.enableMocks();
 
-// Make sure you can use "publicRuntimeConfig" within tests.
 setConfig({
   publicRuntimeConfig: { apiUrl: 'http://localhost:3000' },
 });
 
 beforeEach(() => {
   jest.spyOn(console, 'error');
-  // @ts-ignore jest.spyOn adds this functionallity
   // eslint-disable-next-line no-console
   console.error.mockImplementation(() => null);
 });
