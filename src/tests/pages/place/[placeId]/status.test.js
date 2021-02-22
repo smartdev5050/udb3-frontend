5ff6040b80be39638b214b9de3ec17178265dfcc
fulldev1023 <simbadevlop@gmail.com>
2021-02-22 14:55:16 +0100

    Fix failing tests

--- src/tests/pages/place/[placeId]/status.test.js
+++ src/tests/pages/place/[placeId]/status.test.js
@@ -3,66 +3,64 @@ import { parseOfferId } from '../../../../utils/parseOfferId';
 import { setupPage } from '../../../utils/setupPage';
 import { TestApp } from '../../../utils/TestApp';
 import { user } from '../../../data/user';
-import { prettyDOM, render } from '@testing-library/react';
+import { render, waitFor, screen, fireEvent } from '@testing-library/react';
 import Status from '../../../../pages/place/[placeId]/status';
+import { match } from 'path-to-regexp';
+import nl from '../../../../i18n/nl.json';
+import { mockRouterWithParams } from '../../../mocks/mockRouterWithParams';
 
 const placeId = parseOfferId(mockedPlace['@id']);
 
 describe('Status page place', () => {
-  beforeAll(async () => {
-    setupPage({
-      router: {
-        query: {
-          placeId,
-        },
-      },
-    });
-
-    fetch.mockIf(/http:\/\/localhost:3000.*/, (req) => {
-      const { url } = req;
-      if (url.endsWith('/user')) {
-        return JSON.stringify(user);
-      }
-      if (url.endsWith(`/place/${placeId}`)) {
-        return JSON.stringify(place);
-      }
-    });
-
-    const { container, findByText } = render(<Status />, { wrapper: TestApp });
-    await findByText(`Status voor ${place.name.nl}`);
-    // eslint-disable-next-line no-console
-    console.log(prettyDOM(container));
-  });
-
-  // afterAll(() => cleanup());
-
   describe('When the status "Available" is loaded from place object', () => {
-    test('', () => {
-      expect(true).toBe(true);
+    beforeEach(async () => {
+      setupPage({
+        router: {
+          query: {
+            placeId,
+          },
+        },
+      });
+
+      fetch.mockResponse((req) => {
+        const url = req.url.split('http://localhost:3000')[1];
+
+        if (match('/user')(url)) {
+          return Promise.resolve({ body: JSON.stringify(user) });
+        } else if (match('/place/:id')(url)) {
+          return Promise.resolve({ body: JSON.stringify(place) });
+        } else {
+          return undefined;
+        }
+      });
+
+      render(<Status />, { wrapper: TestApp });
+      await waitFor(() => screen.getByText(`Status voor ${place.name.nl}`));
     });
 
-    // it('checks the value "Available"', () => {
-    //   console.log(document.cookie);
-    //   const radioButtonAvailable = screen.getByLabelText(
-    //     nl.offerStatus.status.open,
-    //   );
+    it('checks the value "Available"', () => {
+      const radioButtonAvailable = screen.getByLabelText(
+        nl.offerStatus.status.open,
+      );
+      expect(radioButtonAvailable).toBeChecked();
+    });
 
-    //   expect(radioButtonAvailable).toBeChecked();
-    // });
-
-    // it('disables the textarea for reason', () => {
-    //   const textAreaReason = screen.getByLabelText(nl.offerStatus.reason);
-
-    //   console.log({ textAreaReason });
-    //   expect(textAreaReason).toBeDisabled();
-    // });
+    it('disables the textarea for reason', () => {
+      const textAreaReason = screen.getByLabelText(nl.offerStatus.reason);
+      expect(textAreaReason).toBeDisabled();
+    });
 
     // it('redirects to edit page when button cancel is pressed', () => {
+    //   const router = mockRouterWithParams({
+    //     query: {
+    //       placeId,
+    //     },
+    //   });
+
     //   const buttonCancel = screen.getByRole('button', {
     //     name: nl.offerStatus.actions.cancel,
     //   });
     //   fireEvent.click(buttonCancel);
-    //   const router = useRouter();
 
     //   expect(router.push).toHaveBeenCalledWith(`/place/${placeId}/edit`);
     // });
