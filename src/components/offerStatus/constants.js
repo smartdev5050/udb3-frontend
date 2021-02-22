9320842e851d86910c28b3bba03c581eb510bc95
fulldev1023 <simbadevlop@gmail.com>
2021-02-22 12:54:39 +0100

    Add constants file for offerStatus

---
+++ src/components/offerStatus/constants.js
@@ -0,0 +1,14 @@
+const OfferType = {
+  EVENT: 'event',
+  PLACE: 'place',
+};
+
+const OfferStatus = {
+  AVAILABLE: 'Available',
+  TEMPORARILY_UNAVAILABLE: 'TemporarilyUnavailable',
+  UNAVAILABLE: 'Unavailable',
+};
+
+const MaxLengthReason = 200;
+
+export { OfferType, OfferStatus, MaxLengthReason };
