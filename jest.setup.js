0a06d61306461287945d6c98b19225c8010cebdb
fulldev1023 <simbadevlop@gmail.com>
2021-02-22 14:31:29 +0100

    fix console.error mock

--- jest.setup.js
+++ jest.setup.js
@@ -3,13 +3,8 @@ import fetchMock from 'jest-fetch-mock';
 import { setConfig } from 'next/config';
 
 fetchMock.enableMocks();
+console.error = jest.fn();
 
 setConfig({
   publicRuntimeConfig: { apiUrl: 'http://localhost:3000' },
-});
-
-beforeEach(() => {
-  jest.spyOn(console, 'error');
-  // eslint-disable-next-line no-console
-  console.error.mockImplementation(() => null);
 });
