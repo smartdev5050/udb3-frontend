1d27255fc1551aef5b0f6723d51711f530a6f6c5
fulldev1023 <simbadevlop@gmail.com>
2021-02-23 14:34:22 +0100

    Show StatusFormOnPage if calendarType is not multiple

--- src/pages/event/[eventId]/status.js
+++ src/pages/event/[eventId]/status.js
@@ -1,61 +1,32 @@
 import { useRouter } from 'next/router';
-import { QueryStatus } from '../../../hooks/api/authenticated-query';
-import { useState } from 'react';
 import { getApplicationServerSideProps } from '../../../utils/getApplicationServerSideProps';
-import { Alert, AlertVariants } from '../../../components/publiq-ui/Alert';
-import { Button, ButtonVariants } from '../../../components/publiq-ui/Button';
-import { Spinner } from '../../../components/publiq-ui/Spinner';
-import { Page } from '../../../components/publiq-ui/Page';
-import { useTranslation } from 'react-i18next';
-import { useGetEventById } from '../../../hooks/api/events';
+import { useChangeStatus, useGetEventById } from '../../../hooks/api/events';
 import { dehydrate } from 'react-query/hydration';
-import { StatusModal } from '../../../components/offerStatus/StatusModal';
+import { Spinner } from '../../../components/publiq-ui/Spinner';
+import { QueryStatus } from '../../../hooks/api/authenticated-query';
+import { StatusFormOnPage } from '../../../components/offerStatus/StatusFormOnPage';
 
 const Status = () => {
-  const { t, i18n } = useTranslation();
   const router = useRouter();
   const { eventId } = router.query;
 
-  const [isModalVisible, setIsModalVisible] = useState(true);
-  const [errorMessage, setErrorMessage] = useState();
+  const getEventByIdQuery = useGetEventById({ id: eventId });
 
-  const handleError = (error) => {
-    setErrorMessage(error.message);
-  };
-  const { data: event = {}, status } = useGetEventById(
-    { id: eventId },
-    { onError: handleError },
-  );
+  const event = getEventByIdQuery.data;
 
-  const name = event.name?.[i18n.language] ?? event.name?.[event.mainLanguage];
+  if (getEventByIdQuery.status === QueryStatus.LOADING) {
+    return <Spinner marginTop={4} />;
+  }
+
+  // TODO: replace by multipel view with SelectionTable
+  if (event.calendarType === 'multiple') return <span>multiple</span>;
 
   return (
-    <Page>
-      <Page.Title>{t('offerStatus.title', { name })}</Page.Title>
-      <Page.Content spacing={5}>
-        {status === QueryStatus.LOADING ? (
-          <Spinner marginTop={4} />
-        ) : errorMessage ? (
-          <Alert variant={AlertVariants.WARNING}>{errorMessage}</Alert>
-        ) : (
-          [
-            <Alert key="alert">{t('offerStatus.info')}</Alert>,
-            <StatusModal
-              key="fds"
-              visible={isModalVisible}
-              onClose={() => setIsModalVisible(false)}
-            />,
-            <Button
-              key="button"
-              variant={ButtonVariants.SUCCESS}
-              width="max-content"
-            >
-              {t('offerStatus.modificationReady')}
-            </Button>,
-          ]
-        )}
-      </Page.Content>
-    </Page>
+    <StatusFormOnPage
+      offer={event}
+      error={getEventByIdQuery.error}
+      useChangeStatus={useChangeStatus}
+    />
   );
 };
 
