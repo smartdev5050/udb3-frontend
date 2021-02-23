673ab3c670b2a6ad252c4038d92b903d2e81eda1
fulldev1023 <simbadevlop@gmail.com>
2021-02-23 12:10:35 +0100

    Fix disabled button modal

--- src/components/offerStatus/StatusModal.js
+++ src/components/offerStatus/StatusModal.js
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 import { useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import { MaxLengthReason, OfferStatus, OfferType } from './constants';
+import { MaxLengthReason, OfferType } from './constants';
 import { Modal, ModalVariants } from '../publiq-ui/Modal';
 import { Stack } from '../publiq-ui/Stack';
 import { ReasonAndTypeForm } from './ReasonAndTypeForm';
@@ -23,10 +23,7 @@ const StatusModal = ({ visible, className, onClose }) => {
       cancelTitle={t('offerStatus.actions.close')}
       onConfirm={() => {}}
       onClose={onClose}
-      confirmButtonDisabled={
-        type !== OfferStatus.AVAILABLE &&
-        (reason.length === 0 || reason.length > MaxLengthReason)
-      }
+      confirmButtonDisabled={reason.length > MaxLengthReason}
     >
       <Stack padding={4}>
         <ReasonAndTypeForm
