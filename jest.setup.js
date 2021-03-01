a50eae4534f23b143fbb4f17c37ab157c0c15cc8
fulldev1023 <simbadevlop@gmail.com>
2021-03-01 14:43:51 +0100

    mock useCookiesWithOptions

--- jest.setup.js
+++ jest.setup.js
@@ -1,6 +1,7 @@
 import '@testing-library/jest-dom';
 import fetchMock from 'jest-fetch-mock';
 import { setConfig } from 'next/config';
+import base64 from 'base-64';
 
 fetchMock.enableMocks();
 
@@ -12,6 +13,27 @@ jest.mock('./src/components/Sidebar.js', () => ({
   Sidebar: () => {
     return null;
   },
+}));
+
+const createToken = () => {
+  const header = base64.encode(JSON.stringify({ test: 'ok' }));
+  const payload = base64.encode(
+    // eslint-disable-next-line no-loss-of-precision
+    JSON.stringify({ exp: 99999999999999999999999999999999999999999 }),
+  );
+  return [header, payload, 'SIGNATURE'].join('.');
+};
+
+jest.mock('./src/hooks/useCookiesWithOptions.js', () => ({
+  __esModule: true,
+  useCookiesWithOptions: () => ({
+    cookies: {
+      token: createToken(),
+    },
+    setCookie: jest.fn(),
+    removeCookie: jest.fn(),
+    removeAuthenticationCookies: jest.fn(),
+  }),
 }));
 
 setConfig({
