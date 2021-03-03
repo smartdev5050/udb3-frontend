b8c140d70d4b14a643a91961443fbd72fc37b958
fulldev1023 <simbadevlop@gmail.com>
2021-03-03 14:59:05 +0100

    Change SelectionTable

--- src/ui/SelectionTable.js
+++ src/ui/SelectionTable.js
@@ -6,6 +6,11 @@ import { Checkbox } from './Checkbox';
 import { Box, getBoxProps } from './Box';
 import { uniqueId } from 'lodash';
 import { getValueFromTheme } from './theme';
+import { Inline } from './Inline';
+import { Button, ButtonVariants } from './Button';
+import { Text } from './Text';
+import { useTranslation } from 'react-i18next';
+import { Stack } from './Stack';
 
 const getValue = getValueFromTheme('selectionTable');
 
@@ -41,7 +46,13 @@ CheckBoxCell.propTypes = {
   row: PropTypes.object,
 };
 
-const SelectionTable = ({ columns, data, onSelectionChanged, ...props }) => {
+const SelectionTable = ({
+  columns,
+  data,
+  onSelectionChanged,
+  actions,
+  ...props
+}) => {
   const {
     getTableProps,
     getTableBodyProps,
@@ -61,69 +72,120 @@ const SelectionTable = ({ columns, data, onSelectionChanged, ...props }) => {
     ]);
   });
 
+  const { t } = useTranslation();
+
   useLayoutEffect(() => {
     if (!onSelectionChanged || !selectedFlatRows) return;
-    onSelectionChanged(selectedFlatRows.map((row) => row.values));
+    onSelectionChanged(
+      selectedFlatRows.map((row) => ({
+        id: parseInt(row.id),
+        values: { ...row.values },
+      })),
+    );
   }, [onSelectionChanged, selectedFlatRows]);
 
   return (
-    <Box
-      forwardedAs={BootstrapTable}
-      css={`
-        &.table th,
-        &.table td {
-          padding: 0.75rem;
-          vertical-align: top;
+    <Stack spacing={3}>
+      <Inline
+        forwardedAs="div"
+        width="100%"
+        alignItems="center"
+        spacing={5}
+        paddingTop={3}
+        css={`
           border-top: 1px solid ${getValue('borderColor')};
-        }
-
-        &.table thead th {
-          border-bottom: 1px solid ${getValue('borderColor')};
-        }
-      `}
-      {...getTableProps()}
-      {...getBoxProps(props)}
-    >
-      <thead>
-        {headerGroups.map((headerGroup, indexHeaderGroup) => (
-          <tr key={indexHeaderGroup} {...headerGroup.getHeaderGroupProps()}>
-            {headerGroup.headers.map((column, indexHeader) => (
-              <Box
-                as="th"
-                key={indexHeader}
-                {...column.getHeaderProps()}
-                color={getValue('color')}
-              >
-                {column.render('Header')}
-              </Box>
-            ))}
-          </tr>
-        ))}
-      </thead>
-      <tbody {...getTableBodyProps()}>
-        {rows.map((row, indexRow) => {
-          prepareRow(row);
-          return (
-            <tr key={indexRow} {...row.getRowProps()}>
-              {row.cells.map((cell, indexCell) => {
-                return (
-                  <td key={indexCell} {...cell.getCellProps()}>
-                    {cell.render('Cell')}
-                  </td>
-                );
-              })}
+        `}
+      >
+        <Text
+          minWidth="11rem"
+          css={`
+            flex-shrink: 0;
+          `}
+        >
+          {t('selectionTable.rowsSelectedCount', {
+            count: selectedFlatRows.length,
+          })}
+        </Text>
+        <Inline>
+          {actions.map(({ iconName, title, onClick, disabled }) => (
+            <Button
+              key={title}
+              variant={ButtonVariants.SECONDARY}
+              onClick={onClick}
+              disabled={disabled}
+              iconName={iconName}
+              spacing={3}
+            >
+              {title}
+            </Button>
+          ))}
+        </Inline>
+      </Inline>
+      <Box
+        forwardedAs={BootstrapTable}
+        css={`
+          &.table th,
+          &.table td {
+            padding: 0.75rem;
+            vertical-align: top;
+            border-top: 1px solid ${getValue('borderColor')};
+            :first-child {
+              width: 100px;
+            }
+          }
+          &.table thead th {
+            border-bottom: 1px solid ${getValue('borderColor')};
+          }
+        `}
+        {...getTableProps()}
+        {...getBoxProps(props)}
+      >
+        <thead>
+          {headerGroups.map((headerGroup, indexHeaderGroup) => (
+            <tr key={indexHeaderGroup} {...headerGroup.getHeaderGroupProps()}>
+              {headerGroup.headers.map((column, indexHeader) => (
+                <Box
+                  as="th"
+                  key={indexHeader}
+                  {...column.getHeaderProps()}
+                  color={getValue('color')}
+                >
+                  {column.render('Header')}
+                </Box>
+              ))}
             </tr>
-          );
-        })}
-      </tbody>
-    </Box>
+          ))}
+        </thead>
+        <tbody {...getTableBodyProps()}>
+          {rows.map((row, indexRow) => {
+            prepareRow(row);
+            return (
+              <tr key={indexRow} {...row.getRowProps()}>
+                {row.cells.map((cell, indexCell) => {
+                  return (
+                    <td key={indexCell} {...cell.getCellProps()}>
+                      {cell.render('Cell')}
+                    </td>
+                  );
+                })}
+              </tr>
+            );
+          })}
+        </tbody>
+      </Box>
+    </Stack>
   );
 };
 
 SelectionTable.propTypes = {
   columns: PropTypes.array.isRequired,
   data: PropTypes.array.isRequired,
+  actions: PropTypes.node,
   onSelectionChanged: PropTypes.function,
+};
+
+SelectionTable.defaultProps = {
+  actions: [],
 };
 
 export { SelectionTable };
