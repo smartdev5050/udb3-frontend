62513549e96ed9d83eae97fd1f893b6f641ed37e
fulldev1023 <simbadevlop@gmail.com>
2021-03-03 14:48:37 +0100

    Change Status Modal

--- src/components/StatusModal.js
+++ src/components/StatusModal.js
@@ -1,16 +1,24 @@
 import PropTypes from 'prop-types';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 import { Modal, ModalVariants } from '@/ui/Modal';
 import { Stack } from '@/ui/Stack';
 import { ReasonAndTypeForm } from '@/components/ReasonAndTypeForm';
 import { OfferType } from '@/constants/OfferType';
+import { Spinner } from '@/ui/Spinner';
 
-const StatusModal = ({ visible, className, onClose }) => {
+const StatusModal = ({ visible, loading, className, onClose, onConfirm }) => {
   const { t } = useTranslation();
 
   const [type, setType] = useState('');
   const [reason, setReason] = useState('');
+
+  useEffect(() => {
+    if (!visible) {
+      setType('');
+      setReason('');
+    }
+  }, [visible]);
 
   return (
     <Modal
@@ -21,27 +29,33 @@ const StatusModal = ({ visible, className, onClose }) => {
       className={className}
       confirmTitle={t('offerStatus.actions.save')}
       cancelTitle={t('offerStatus.actions.close')}
-      onConfirm={() => {}}
+      onConfirm={() => onConfirm(type, reason)}
       onClose={onClose}
-      confirmButtonDisabled={reason.length > 200}
+      confirmButtonDisabled={!type || reason.length > 200 || loading}
     >
-      <Stack padding={4}>
-        <ReasonAndTypeForm
-          offerType={OfferType.EVENT}
-          statusType={type}
-          statusReason={reason}
-          onChangeStatusType={(e) => setType(e.target.value)}
-          onInputStatusReason={(e) => setReason(e.target.value)}
-        />
-      </Stack>
+      {loading ? (
+        <Spinner marginY={4} />
+      ) : (
+        <Stack padding={4}>
+          <ReasonAndTypeForm
+            offerType={OfferType.EVENT}
+            statusType={type}
+            statusReason={reason}
+            onChangeStatusType={(e) => setType(e.target.value)}
+            onInputStatusReason={(e) => setReason(e.target.value)}
+          />
+        </Stack>
+      )}
     </Modal>
   );
 };
 
 StatusModal.propTypes = {
   visible: PropTypes.bool,
+  loading: PropTypes.bool,
   className: PropTypes.string,
   onClose: PropTypes.func,
+  onConfirm: PropTypes.func,
 };
 
 export { StatusModal };
