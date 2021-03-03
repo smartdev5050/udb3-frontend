004e87bb9953f686a336229efa9c313a98063222
fulldev1023 <simbadevlop@gmail.com>
2021-03-03 15:03:44 +0100

    Add formatPeriod

---
+++ src/utils/formatPeriod.js
@@ -0,0 +1,45 @@
+import { differenceInDays, format } from 'date-fns';
+import nl from 'date-fns/locale/nl-BE';
+import fr from 'date-fns/locale/fr';
+import { capitalize } from 'lodash';
+
+const locales = {
+  nl,
+  fr,
+};
+
+const formatPeriod = (startDate, endDate, locale, t) => {
+  const start = new Date(startDate);
+  const end = new Date(endDate);
+
+  const formatTypeDate = 'EEEE d MMMM yyyy';
+  const formatTypeTime = 'HH:mm';
+
+  const formattedDateStart = format(start, formatTypeDate, {
+    locale: locales[locale],
+  });
+  const formattedTimeStart = format(start, formatTypeTime);
+
+  const formattedDateEnd = format(end, formatTypeDate, {
+    locale: locales[locale],
+  });
+  const formattedTimeEnd = format(end, formatTypeTime);
+
+  if (differenceInDays(start, end) === 0) {
+    return capitalize(
+      `${formattedDateStart} ${t(
+        'calendarSummary.at',
+      )} ${formattedTimeStart} ${t(
+        'calendarSummary.till',
+      )} ${formattedTimeEnd}`,
+    );
+  }
+
+  return `${t('calendarSummary.from')} ${formattedDateStart} ${t(
+    'calendarSummary.at',
+  )} ${formattedTimeStart} ${t('calendarSummary.till')} ${formattedDateEnd} ${t(
+    'calendarSummary.at',
+  )} ${formattedTimeEnd}`;
+};
+
+export { formatPeriod };
