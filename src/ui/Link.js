f4ea6cf41b17a40825f856d05590f1715c28beb8
fulldev1023 <simbadevlop@gmail.com>
2021-03-08 16:43:53 +0100

    Don't add prefetch prop in Next.js env

--- src/ui/Link.js
+++ src/ui/Link.js
@@ -82,7 +82,11 @@ const Link = ({
 
   if (isInternalLink) {
     return (
-      <NextLink href={href} passHref prefetch={!process.env.STORYBOOK}>
+      <NextLink
+        href={href}
+        passHref
+        {...(process.env.STORYBOOK ? { prefetch: false } : {})}
+      >
         <BaseLink
           className={className}
           variant={variant}
