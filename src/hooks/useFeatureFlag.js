f450da0a9bb47099a12fd98e5b18196388015232
fulldev1023 <simbadevlop@gmail.com>
2021-04-12 11:03:50 +0200

    Expose helpers on window

--- src/hooks/useFeatureFlag.js
+++ src/hooks/useFeatureFlag.js
@@ -1,16 +1,29 @@
+import { FeatureFlags } from '@/constants/FeatureFlags';
+import Cookies from 'universal-cookie';
 import { useCookiesWithOptions } from './useCookiesWithOptions';
+
+const cookies = new Cookies();
+
+const createFullIdentifier = (identifier) => `ff_${identifier}`;
 
 const useFeatureFlag = (identifier) => {
   if (!identifier) return [false, () => {}];
 
-  const fullIdentifier = `ff_${identifier}`;
-
   const { cookies, setCookie } = useCookiesWithOptions();
+
+  const fullIdentifier = createFullIdentifier(identifier);
 
   const set = (value) => setCookie(fullIdentifier, value);
   const value = cookies?.[fullIdentifier] ?? false;
 
   return [value, set];
 };
+
+if (typeof window !== 'undefined') {
+  window.FeatureFlags = FeatureFlags;
+  window.setFeatureFlag = (identifier, value) => {
+    cookies.set(createFullIdentifier(identifier), value);
+  };
+}
 
 export { useFeatureFlag };
