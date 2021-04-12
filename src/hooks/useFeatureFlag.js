c85961f56ad4b63fa0b87647bb24dbff449b53af
fulldev1023 <simbadevlop@gmail.com>
2021-04-12 10:53:07 +0200

    Add useFeatureFlag hook

---
+++ src/hooks/useFeatureFlag.js
@@ -0,0 +1,14 @@
+import { useCookiesWithOptions } from './useCookiesWithOptions';
+
+export const useFeatureFlag = (identifier) => {
+  if (!identifier) return [false, () => {}];
+
+  const fullIdentifier = `ff_${identifier}`;
+
+  const { cookies, setCookie } = useCookiesWithOptions();
+
+  const set = (value) => setCookie(fullIdentifier, value);
+  const value = cookies?.[fullIdentifier] ?? false;
+
+  return [value, set];
+};
