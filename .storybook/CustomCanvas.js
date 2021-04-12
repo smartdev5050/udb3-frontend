eac2ed89bb7090eb6108e6f5203fe6f154c91f07
fulldev1023 <simbadevlop@gmail.com>
2021-04-12 16:12:21 +0200

    Improve CustomCanvas

--- .storybook/CustomCanvas.js
+++ .storybook/CustomCanvas.js
@@ -2,9 +2,11 @@ import { Canvas } from '@storybook/addon-docs/blocks';
 import { styled } from '@storybook/theming';
 
 const SyledCanvas = styled(Canvas)`
-  .plain-text {
+pre {
+  span, .plain-text {
     color: #ffffff;
   }
+}  
 `;
 
 export { SyledCanvas as CustomCanvas };
