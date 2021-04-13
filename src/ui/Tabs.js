90854a023f4da2e36361d8deffdaadb61df97058
fulldev1023 <simbadevlop@gmail.com>
2021-04-13 16:28:18 +0200

    Create a new Tabs UI component

---
+++ src/ui/Tabs.js
@@ -0,0 +1,34 @@
+import { Tab as BootstrapTab, Tabs as BootstrapTabs } from 'react-bootstrap';
+import * as PropTypes from 'prop-types';
+
+const Tabs = ({ activeKey, onSelect, children }) => {
+  return (
+    <BootstrapTabs activeKey={activeKey} onSelect={onSelect}>
+      {children}
+    </BootstrapTabs>
+  );
+};
+
+Tabs.propTypes = {
+  activeKey: PropTypes.string.isRequired,
+  onSelect: PropTypes.func.isRequired,
+  children: PropTypes.node,
+};
+
+const Tab = ({ eventKey, title, children }) => {
+  return (
+    <BootstrapTab eventKey={eventKey} title={title}>
+      {children}
+    </BootstrapTab>
+  );
+};
+
+Tab.propTypes = {
+  eventKey: PropTypes.string.isRequired,
+  title: PropTypes.func.isRequired,
+  children: PropTypes.node,
+};
+
+Tabs.Tab = Tab;
+
+export { Tabs };
