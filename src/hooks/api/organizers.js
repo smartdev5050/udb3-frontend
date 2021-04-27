2e91422f006206f288e9106f9f74c258ac9a5e0c
fulldev1023 <simbadevlop@gmail.com>
2021-04-27 13:39:23 +0200

    Add methods to get organizer by Id and by creator

---
+++ src/hooks/api/organizers.js
@@ -0,0 +1,56 @@
+import { fetchFromApi } from '@/utils/fetchFromApi';
+import { useAuthenticatedQuery } from '@/hooks/api/authenticated-query';
+
+const getOrganizerById = async ({ headers, id }) => {
+  const res = await fetchFromApi({
+    path: `/organizers/${id.toString()}`,
+    options: {
+      headers,
+    },
+  });
+  return await res.json();
+};
+
+const useGetOrganizerById = ({ req, queryClient, id }, configuration = {}) =>
+  useAuthenticatedQuery({
+    req,
+    queryClient,
+    queryKey: ['organizers'],
+    queryFn: getOrganizerById,
+    queryArguments: { id },
+    enabled: !!id,
+    ...configuration,
+  });
+
+const getOrganizersByCreatorId = async ({ headers, ...queryData }) => {
+  const res = await fetchFromApi({
+    path: '/organizers/',
+    searchParams: {
+      ...queryData,
+    },
+    options: {
+      headers,
+    },
+  });
+  return await res.json();
+};
+
+const useGetOrganizersByCreatorId = (
+  { req, queryClient, creatorId },
+  configuration = {},
+) =>
+  useAuthenticatedQuery({
+    req,
+    queryClient,
+    queryKey: ['organizers'],
+    queryFn: getOrganizersByCreatorId,
+    queryArguments: {
+      creator: creatorId,
+      limit: 50,
+      start: 0,
+      embed: true,
+    },
+    ...configuration,
+  });
+
+export { useGetOrganizerById, useGetOrganizersByCreatorId };
