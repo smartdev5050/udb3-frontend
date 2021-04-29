1b3cb64501a37151addd31fc9a955d56094ef8da
fulldev1023 <simbadevlop@gmail.com>
2021-04-29 10:37:37 +0200

    Add types for themeprovider

--- src/ui/ThemeProvider.tsx
+++ src/ui/ThemeProvider.tsx
@@ -1,7 +1,8 @@
 import { ThemeProvider as SCThemeProvider } from 'styled-components';
 import { theme } from './theme';
+import type { Theme } from './theme';
 
-const ThemeProvider = (props) => {
+const ThemeProvider = (props: { theme: Theme }) => {
   return <SCThemeProvider theme={theme} {...props} />;
 };
 
