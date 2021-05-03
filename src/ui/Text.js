1433cbb256cc52c5fb60d00ce0a030fd84714b0a
fulldev1023 <simbadevlop@gmail.com>
2021-05-03 14:43:06 +0200

    Use color prop instead of css prop

--- src/ui/Text.js
+++ src/ui/Text.js
@@ -1,5 +1,4 @@
 import PropTypes from 'prop-types';
-import { css } from 'styled-components';
 import { Box, boxPropTypes, getBoxProps } from './Box';
 import { getValueFromTheme } from './theme';
 
@@ -10,14 +9,14 @@ const TextVariants = {
   MUTED: 'muted',
 };
 
-const mutedCSS = css`
-  color: ${getValue('muted.color')};
-`;
-
 const Text = ({ as, children, className, variant, ...props }) => {
-  const css = variant === TextVariants.MUTED ? mutedCSS : ``;
   return (
-    <Box as={as} className={className} css={css} {...getBoxProps(props)}>
+    <Box
+      as={as}
+      className={className}
+      color={variant === TextVariants.MUTED && getValue('muted.color')}
+      {...getBoxProps(props)}
+    >
       {children}
     </Box>
   );
