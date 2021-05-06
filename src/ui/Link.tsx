6af4680b84e8852fa0a9bc3079a724a56fbe4d1d
fulldev1023 <simbadevlop@gmail.com>
2021-05-06 18:20:13 +0200

    Type Link

--- src/ui/Link.tsx
+++ src/ui/Link.tsx
@@ -1,11 +1,14 @@
 import NextLink from 'next/link';
-import PropTypes from 'prop-types';
+import type { ReactNode } from 'react';
 import { cloneElement, forwardRef } from 'react';
 
+import type { Values } from '@/types/Values';
 import { Button, ButtonVariants } from '@/ui/Button';
 
+import type { Icons } from './Icon';
 import { Icon } from './Icon';
-import { getInlineProps, Inline, inlinePropTypes } from './Inline';
+import type { InlineProps } from './Inline';
+import { getInlineProps, Inline } from './Inline';
 import { Text } from './Text';
 import { getValueFromTheme } from './theme';
 
@@ -16,67 +19,83 @@ const LinkButtonVariants = {
   BUTTON_SECONDARY: ButtonVariants.SECONDARY,
   BUTTON_DANGER: ButtonVariants.DANGER,
   BUTTON_SUCCESS: ButtonVariants.SUCCESS,
-};
+} as const;
 
 const LinkVariants = {
-  UNSTYLED: 'unstyled',
   ...LinkButtonVariants,
+  UNSTYLED: 'unstyled',
+} as const;
+
+type BaseLinkProps = InlineProps & {
+  variant?: Values<typeof LinkVariants>;
 };
 
-const BaseLink = forwardRef(({ variant, children, ...props }, ref) => {
-  if (variant === LinkVariants.UNSTYLED) {
+const BaseLink = forwardRef<HTMLDivElement, BaseLinkProps>(
+  ({ href, variant, title, children, className, ...props }, ref) => {
+    if (variant === LinkVariants.UNSTYLED) {
+      return (
+        <Inline
+          className={className}
+          href={href}
+          ref={ref}
+          as="a"
+          title={title}
+          display="inline-flex"
+          color={{ default: 'inherit', hover: 'inherit' }}
+          alignItems="center"
+          {...getInlineProps(props)}
+        >
+          {children}
+        </Inline>
+      );
+    }
+
+    if (Object.values(LinkButtonVariants).includes(variant)) {
+      return (
+        <Inline
+          className={className}
+          href={href}
+          ref={ref}
+          as="a"
+          display="inline-flex"
+          alignItems="center"
+          {...getInlineProps(props)}
+        >
+          <Button forwardedAs="span" variant={variant}>
+            {children}
+          </Button>
+        </Inline>
+      );
+    }
+
     return (
       <Inline
+        className={className}
+        href={href}
         ref={ref}
         forwardedAs="a"
+        color={{ default: getValue('color'), hover: getValue('color') }}
         display="inline-flex"
-        color={{ default: 'inherit', hover: 'inherit' }}
-        alignItems="center"
-        {...props}
+        fontWeight={400}
+        css={`
+          &:hover {
+            text-decoration: underline;
+          }
+        `}
+        {...getInlineProps(props)}
       >
         {children}
       </Inline>
     );
-  }
+  },
+);
 
-  if (Object.values(LinkButtonVariants).includes(variant)) {
-    return (
-      <Inline
-        ref={ref}
-        forwardedAs="a"
-        display="inline-flex"
-        alignItems="center"
-        {...props}
-      >
-        <Button forwardedAs="span" variant={variant}>
-          {children}
-        </Button>
-      </Inline>
-    );
-  }
-
-  return (
-    <Inline
-      ref={ref}
-      forwardedAs="a"
-      color={{ default: getValue('color'), hover: getValue('color') }}
-      display="inline-flex"
-      css={`
-        font-weight: 400;
-        &:hover {
-          text-decoration: underline;
-        }
-      `}
-      {...props}
-    >
-      {children}
-    </Inline>
-  );
-});
-
-BaseLink.propTypes = {
-  variant: PropTypes.string,
-  children: PropTypes.node,
+type LinkProps = BaseLinkProps & {
+  href: string;
+  iconName?: Values<typeof Icons>;
+  suffix?: ReactNode;
+  customChildren?: boolean;
+  shouldHideText?: boolean;
 };
 
 const Link = ({
@@ -90,11 +109,13 @@ const Link = ({
   variant,
   title,
   ...props
-}) => {
+}: LinkProps) => {
   const isInternalLink = href.startsWith('/');
 
   const clonedSuffix = suffix
-    ? cloneElement(suffix, {
+    ? // @ts-expect-error
+      cloneElement(suffix, {
+        // @ts-expect-error
         ...suffix.props,
         key: 'suffix',
         css: `align-self: flex-end`,
@@ -145,19 +166,6 @@ const Link = ({
       {inner}
     </BaseLink>
   );
-};
-
-Link.propTypes = {
-  ...inlinePropTypes,
-  href: PropTypes.string,
-  title: PropTypes.string,
-  iconName: PropTypes.string,
-  suffix: PropTypes.node,
-  className: PropTypes.string,
-  children: PropTypes.node,
-  customChildren: PropTypes.bool,
-  shouldHideText: PropTypes.bool,
-  as: PropTypes.node,
 };
 
 Link.defaultProps = {
