c830b28aa2f5d79f07af2701a7be9549ecb876f9
fulldev1023 <simbadevlop@gmail.com>
2021-05-06 14:55:02 +0200

    Server side render events places and organizers

--- src/components/pages/events/index.tsx
+++ src/components/pages/events/index.tsx
@@ -1,8 +1,45 @@
+import { Cookies } from 'react-cookie';
+import { dehydrate } from 'react-query/hydration';
+import type { User } from 'types/User';
+
 import { DashboardPage } from '@/components/DashboardPage';
+import { useGetEventsByCreator } from '@/hooks/api/events';
+import { useGetOrganizersByCreator } from '@/hooks/api/organizers';
+import { useGetPlacesByCreator } from '@/hooks/api/places';
 import { getApplicationServerSideProps } from '@/utils/getApplicationServerSideProps';
 
 const Index = () => <DashboardPage activeTab="events" />;
 
-export const getServerSideProps = getApplicationServerSideProps();
+export const getServerSideProps = getApplicationServerSideProps(
+  async ({ req, cookies: rawCookies, queryClient }) => {
+    const cookies = new Cookies(rawCookies);
+    const user: User = cookies.get('user');
+
+    await useGetEventsByCreator({
+      req,
+      queryClient,
+      creator: { id: user?.id, email: user.email },
+    });
+
+    await useGetPlacesByCreator({
+      req,
+      queryClient,
+      creator: { id: user?.id, email: user.email },
+    });
+
+    await useGetOrganizersByCreator({
+      req,
+      queryClient,
+      creator: { id: user?.id, email: user.email },
+    });
+
+    return {
+      props: {
+        dehydratedState: dehydrate(queryClient),
+        cookies: rawCookies,
+      },
+    };
+  },
+);
 
 export default Index;
