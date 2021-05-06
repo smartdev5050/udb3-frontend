98fcc182e0dbb22557eb1316b9795ac913aebf3a
fulldev1023 <simbadevlop@gmail.com>
2021-05-06 11:21:06 +0200

    Add tabs to DashboardPage

--- src/components/DashboardPage.tsx
+++ src/components/DashboardPage.tsx
@@ -1,4 +1,4 @@
-import { useMemo } from 'react';
+import { useMemo, useState } from 'react';
 import type { UseQueryResult } from 'react-query';
 
 import { useGetEventsByCreator } from '@/hooks/api/events';
@@ -6,9 +6,10 @@ import { useGetOrganizersByCreator } from '@/hooks/api/organizers';
 import { useGetPlacesByCreator } from '@/hooks/api/places';
 import { useCookiesWithOptions } from '@/hooks/useCookiesWithOptions';
 import type { User } from '@/types/User';
-import { List } from '@/ui/List';
+// import { List } from '@/ui/List';
 import { Page } from '@/ui/Page';
 import { Stack } from '@/ui/Stack';
+import { Tabs } from '@/ui/Tabs';
 import { Text } from '@/ui/Text';
 
 type DashboardOptions = 'events' | 'places' | 'organizers';
@@ -21,13 +22,24 @@ const GetEventsByCreatorMap = {
   organizers: useGetOrganizersByCreator,
 };
 
-const DashboardPage = ({ activeTab }: Props) => {
+const DashboardPage = ({ activeTab: initialActiveTab }: Props) => {
   const { cookies } = useCookiesWithOptions(['user']);
+  const [activeTab, setActiveTab] = useState(initialActiveTab);
 
   const useGetEventsByCreator = useMemo(
     () => GetEventsByCreatorMap[activeTab],
     [activeTab],
   );
+
+  const handleSelectTab = (eventKey: DashboardOptions) => {
+    setActiveTab(eventKey);
+    // change the url to match the tab, but don't trigger a refresh
+    window.history.pushState(
+      undefined,
+      '',
+      `${window.location.protocol}//${window.location.host}/${eventKey}`,
+    );
+  };
 
   const user: User = cookies.user;
 
@@ -42,12 +54,18 @@ const DashboardPage = ({ activeTab }: Props) => {
       <Page.Title>Welkom {user?.username}</Page.Title>
       <Page.Content spacing={5}>
         <Stack spacing={3}>
-          <Text>Mijn activiteiten en locaties</Text>
-          <List>
-            <List.Item>
-              <Text>Test</Text>
-            </List.Item>
-          </List>
+          <Text>Mijn activiteiten, locaties en organisaties</Text>
+          <Tabs activeKey={activeTab} onSelect={handleSelectTab}>
+            <Tabs.Tab eventKey="events" title="Events">
+              <Text>EVENTS</Text>
+            </Tabs.Tab>
+            <Tabs.Tab eventKey="places" title="Places">
+              <Text>PLACES</Text>
+            </Tabs.Tab>
+            <Tabs.Tab eventKey="organizers" title="Organizers">
+              <Text>ORGANIZERS</Text>
+            </Tabs.Tab>
+          </Tabs>
         </Stack>
       </Page.Content>
     </Page>
