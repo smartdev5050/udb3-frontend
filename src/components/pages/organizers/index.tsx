66fbce3ef1b8891d904c3c718428423964fae6b6
fulldev1023 <simbadevlop@gmail.com>
2021-05-07 11:34:15 +0200

    Add optional chaining for email

--- src/components/pages/organizers/index.tsx
+++ src/components/pages/organizers/index.tsx
@@ -18,19 +18,19 @@ export const getServerSideProps = getApplicationServerSideProps(
     await useGetEventsByCreator({
       req,
       queryClient,
-      creator: { id: user?.id, email: user.email },
+      creator: { id: user?.id, email: user?.email },
     });
 
     await useGetPlacesByCreator({
       req,
       queryClient,
-      creator: { id: user?.id, email: user.email },
+      creator: { id: user?.id, email: user?.email },
     });
 
     await useGetOrganizersByCreator({
       req,
       queryClient,
-      creator: { id: user?.id, email: user.email },
+      creator: { id: user?.id, email: user?.email },
     });
 
     return {
