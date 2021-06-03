eae84c1cf00509ff9699e6c99d22b441d92d5a31
fulldev1023 <simbadevlop@gmail.com>
2021-06-03 11:33:50 +0200

    Use publicRuntimeConfig

--- sentry.client.config.js
+++ sentry.client.config.js
@@ -3,10 +3,13 @@
 // https://docs.sentry.io/platforms/javascript/guides/nextjs/
 
 import * as Sentry from '@sentry/nextjs';
+import getConfig from 'next/config';
+
+const { publicRuntimeConfig } = getConfig();
 
 Sentry.init({
-  dsn: process.env.NEXT_PUBLIC_SENTRY_DSN,
-  environment: process.env.NEXT_PUBLIC_ENVIRONMENT,
+  dsn: publicRuntimeConfig.sentryDsn,
+  environment: publicRuntimeConfig.environment,
   // Note: if you want to override the automatic release value, do not set a
   // `release` value here - use the environment variable `SENTRY_RELEASE`, so
   // that it will also get attached to your source maps
