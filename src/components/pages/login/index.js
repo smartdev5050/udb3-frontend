e900b0242c70113961746ada0c31a1cdb64b2e0a
fulldev1023 <simbadevlop@gmail.com>
2021-06-09 11:57:28 +0200

    Avoid updating req parameter as workaround for const

--- src/components/pages/login/index.js
+++ src/components/pages/login/index.js
@@ -12,18 +12,17 @@ export const getServerSideProps = ({ req, resolvedUrl }) => {
 
   const url = new URL(`${origin}${resolvedUrl}`);
 
-  const referer = url.searchParams.get('referer')
+  let referer = url.searchParams.get('referer')
     ? new URL(url.searchParams.get('referer'))
     : undefined;
 
   referer?.searchParams?.delete('jwt');
 
-  req.headers.referer =
-    referer?.toString() ?? (req.headers.referer || url.toString());
+  referer = referer?.toString() ?? (req.headers.referer || url.toString());
 
   return {
     redirect: {
-      destination: `/login/${language}?referer=${req.headers.referer}`,
+      destination: `/login/${language}?referer=${referer}`,
       permanent: false,
     },
   };
