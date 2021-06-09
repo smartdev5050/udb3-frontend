14be2eb2d07bdc0a737939bbd3be05fe9de4f47c
fulldev1023 <simbadevlop@gmail.com>
2021-06-09 10:47:37 +0200

    Double encode the plus sign to avoid conversion to space

--- src/components/pages/login/index.js
+++ src/components/pages/login/index.js
@@ -13,7 +13,7 @@ export const getServerSideProps = ({ req, resolvedUrl }) => {
   const url = new URL(`${origin}${resolvedUrl}`);
 
   const referer = url.searchParams.get('referer')
-    ? new URL(url.searchParams.get('referer'))
+    ? encodeURI(url.searchParams.get('referer').replace(/\+/g, '%2B'))
     : undefined;
 
   referer?.searchParams?.delete('jwt');
