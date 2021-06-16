5f81717a1def06c154cb896703638a17c4d68e95
fulldev1023 <simbadevlop@gmail.com>
2021-06-16 11:28:40 +0200

    Delete logs

--- src/redirects.js
+++ src/redirects.js
@@ -3,23 +3,6 @@ import { tabOptions } from '@/pages/dashboard';
 import { FeatureFlags } from './hooks/useFeatureFlag';
 
 const createDashboardRedirects = (environment) => {
-  console.log('bruh', [
-    {
-      source: '/dashboard',
-      destination: '/dashboard?tabs=events&page=1',
-      permanent: environment !== 'development',
-      featureFlag: FeatureFlags.REACT_DASHBOARD,
-    },
-    tabOptions.map((tabName) => {
-      const source = `/dashboard?tabs=${tabName}`;
-      return {
-        source,
-        destination: `${source}&page=1`,
-        permanent: environment !== 'development',
-        featureFlag: FeatureFlags.REACT_DASHBOARD,
-      };
-    }),
-  ]);
   return [
     {
       source: '/dashboard',
@@ -39,43 +22,40 @@ const createDashboardRedirects = (environment) => {
   ];
 };
 
-const getRedirects = (environment) => {
-  console.log('in getRedirects');
-  return [
-    // Only make the permanent redirects really permanent in environments other
-    // than development, so we don't get permanent redirects on localhost which
-    // may conflict with other projects.
+const getRedirects = (environment) => [
+  // Only make the permanent redirects really permanent in environments other
+  // than development, so we don't get permanent redirects on localhost which
+  // may conflict with other projects.
 
-    {
-      source: '/event/:eventId/status',
-      destination: '/events/:eventId/status',
-      permanent: environment !== 'development',
-    },
-    {
-      source: '/place/:placeId/status',
-      destination: '/places/:placeId/status',
-      permanent: environment !== 'development',
-    },
-    ...createDashboardRedirects(environment),
-    {
-      source: '/events',
-      destination: '/dashboard?tabs=events&page=1',
-      permanent: environment !== 'development',
-      featureFlag: FeatureFlags.REACT_DASHBOARD,
-    },
-    {
-      source: '/organizers',
-      destination: '/dashboard?tabs=organizers&page=1',
-      permanent: environment !== 'development',
-      featureFlag: FeatureFlags.REACT_DASHBOARD,
-    },
-    {
-      source: '/places',
-      destination: '/dashboard?tabs=places&page=1',
-      permanent: environment !== 'development',
-      featureFlag: FeatureFlags.REACT_DASHBOARD,
-    },
-  ];
-};
+  {
+    source: '/event/:eventId/status',
+    destination: '/events/:eventId/status',
+    permanent: environment !== 'development',
+  },
+  {
+    source: '/place/:placeId/status',
+    destination: '/places/:placeId/status',
+    permanent: environment !== 'development',
+  },
+  ...createDashboardRedirects(environment),
+  {
+    source: '/events',
+    destination: '/dashboard?tabs=events&page=1',
+    permanent: environment !== 'development',
+    featureFlag: FeatureFlags.REACT_DASHBOARD,
+  },
+  {
+    source: '/organizers',
+    destination: '/dashboard?tabs=organizers&page=1',
+    permanent: environment !== 'development',
+    featureFlag: FeatureFlags.REACT_DASHBOARD,
+  },
+  {
+    source: '/places',
+    destination: '/dashboard?tabs=places&page=1',
+    permanent: environment !== 'development',
+    featureFlag: FeatureFlags.REACT_DASHBOARD,
+  },
+];
 
 export { getRedirects };
