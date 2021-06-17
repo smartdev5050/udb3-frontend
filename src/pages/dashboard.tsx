8bc13b820c3874efc3280dc048b2f54d23099031
fulldev1023 <simbadevlop@gmail.com>
2021-06-17 11:47:44 +0200

    Revert path changes

--- src/pages/dashboard.tsx
+++ src/pages/dashboard.tsx
@@ -1,8 +1,8 @@
-import Dashboard, {
-  getServerSideProps as dashboardGetServerSideProps,
-} from '@/components/pages/dashboard';
 import { FeatureFlags, useFeatureFlag } from '@/hooks/useFeatureFlag';
 import Fallback from '@/pages/[...params]';
+import Dashboard, {
+  getServerSideProps as dashboardGetServerSideProps,
+} from '@/pages/dashboard';
 
 const DashboardWrapper = (props) => {
   const [isReactDashboardFeatureFlagEnabled] = useFeatureFlag(
