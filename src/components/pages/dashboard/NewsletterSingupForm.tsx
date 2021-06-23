215b6fcb78b3d5f4e756f4262191d7d3742773d8
fulldev1023 <simbadevlop@gmail.com>
2021-06-23 16:17:55 +0200

    Use useAddNewsletterSubscriber

--- src/components/pages/dashboard/NewsletterSingupForm.tsx
+++ src/components/pages/dashboard/NewsletterSingupForm.tsx
@@ -1,7 +1,9 @@
 import { useRef, useState } from 'react';
 import { useTranslation } from 'react-i18next';
-import validator from 'validator';
+import * as yup from 'yup';
 
+import { QueryStatus } from '@/hooks/api/authenticated-query';
+import { useAddNewsletterSubscriber } from '@/hooks/api/newsletter';
 import { Alert, AlertVariants } from '@/ui/Alert';
 import { parseSpacing } from '@/ui/Box';
 import { Button } from '@/ui/Button';
@@ -15,6 +17,9 @@ import { Stack } from '@/ui/Stack';
 import { Text } from '@/ui/Text';
 import { Title } from '@/ui/Title';
 
+const isEmail = (value: string) =>
+  yup.string().required().email().isValidSync(value);
+
 const NewsletterSignupForm = () => {
   const formRef = useRef<HTMLFormElement>();
   const [email, setEmail] = useState('');
@@ -22,17 +27,17 @@ const NewsletterSignupForm = () => {
 
   const { i18n, t } = useTranslation();
 
+  const addNewsletterSubscriberMutation = useAddNewsletterSubscriber();
+
   const validate = () => {
-    if (validator.isEmpty(email) || !validator.isEmail(email)) {
-      setIsValid(false);
-    }
+    setIsValid(isEmail(email));
   };
 
   const handleSubmit = () => {
     validate();
 
     if (isValid) {
-      console.log('Do stuff');
+      addNewsletterSubscriberMutation.mutate({ email });
     }
   };
 
@@ -45,47 +50,59 @@ const NewsletterSignupForm = () => {
       {i18n.language === 'nl' && (
         <Panel backgroundColor="white" padding={4} spacing={4}>
           <Inline as="div" justifyContent="space-between">
-            <Stack spacing={4}>
-              <Title>{t('dashboard.newsletter.title')}</Title>
-              <Paragraph>{t('dashboard.newsletter.content')}</Paragraph>
-              <Inline
-                as="form"
-                ref={formRef}
-                onSubmit={(e) => {
-                  e.preventDefault();
-                  handleSubmit();
-                }}
-                alignItems="center"
-                spacing={4}
-              >
-                <InputWithLabel
-                  type="email"
-                  id="newletter-email"
-                  label="Email"
-                  placeholder="test@test.be"
-                  onInput={(e) => {
-                    setIsValid(true);
-                    setEmail(e.target.value);
+            {addNewsletterSubscriberMutation.status === 'success' ? (
+              <Stack spacing={4}>
+                <Title>{t('dashboard.newsletter.success.title')}</Title>
+                <Paragraph>
+                  {t('dashboard.newsletter.success.content')}
+                </Paragraph>
+              </Stack>
+            ) : (
+              <Stack spacing={4}>
+                <Title>{t('dashboard.newsletter.title')}</Title>
+                <Paragraph>{t('dashboard.newsletter.content')}</Paragraph>
+                <Inline
+                  as="form"
+                  ref={formRef}
+                  onSubmit={(e) => {
+                    e.preventDefault();
+                    handleSubmit();
                   }}
-                  value={email}
-                  flex={1}
-                  maxWidth="30rem"
-                />
-                <Button onClick={handleSubmit} maxHeight={parseSpacing(5)()}>
-                  {t('dashboard.newsletter.subscribe')}
-                </Button>
-              </Inline>
-            </Stack>
+                  alignItems="center"
+                  spacing={4}
+                >
+                  <InputWithLabel
+                    type="email"
+                    id="newletter-email"
+                    label="Email"
+                    placeholder="test@test.be"
+                    onInput={(e) => {
+                      setIsValid(true);
+                      setEmail(e.target.value);
+                    }}
+                    value={email}
+                    flex={1}
+                    maxWidth="30rem"
+                  />
+                  <Button onClick={handleSubmit} maxHeight={parseSpacing(5)()}>
+                    {t('dashboard.newsletter.subscribe')}
+                  </Button>
+                </Inline>
+                {!isValid && (
+                  <Alert variant={AlertVariants.DANGER}>
+                    {t('dashboard.newsletter.invalid')}
+                  </Alert>
+                )}
+              </Stack>
+            )}
+
             <Image
               src={`/assets/${t('dashboard.newsletter.logo')}`}
-              width={300}
+              alt={t('dashboard.newsletter.logo_alt')}
+              width={200}
+              paddingLeft={5}
             />
           </Inline>
-          {!isValid && (
-            <Alert variant={AlertVariants.DANGER}>
-              {t('dashboard.newsletter.invalid')}
-            </Alert>
-          )}
         </Panel>
       )}
     </Stack>
