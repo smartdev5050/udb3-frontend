7e28849a599a85f8ccd8d140495052e3e46e8dc9
fulldev1023 <simbadevlop@gmail.com>
2021-06-23 10:49:11 +0200

    Run format script

--- src/utils/getApplicationServerSideProps.js
+++ src/utils/getApplicationServerSideProps.js
@@ -90,8 +90,8 @@ const getApplicationServerSideProps = (callbackFn) => async ({
 
   // set token in req.headers.cookie so that the token is known when prefetching a request
   if (query.jwt && cookies.get('token') !== query.jwt) {
-    cookies.set('token', query.jwt);    
-  }  
+    cookies.set('token', query.jwt);
+  }
 
   req.headers.cookie = cookies.toString();
 
