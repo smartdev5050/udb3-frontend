d97b3d212b20128f76ad13671e00b621a2a23c89
fulldev1023 <simbadevlop@gmail.com>
2021-06-23 13:51:12 +0200

    Add basic validation logic

--- src/components/pages/dashboard/NewsletterSingupForm.tsx
+++ src/components/pages/dashboard/NewsletterSingupForm.tsx
@@ -1,3 +1,8 @@
+import type { FormEvent } from 'react';
+import { useRef, useState } from 'react';
+
+import { Button } from '@/ui/Button';
+import { Inline } from '@/ui/Inline';
 import { InputWithLabel } from '@/ui/InputWithLabel';
 import { Link } from '@/ui/Link';
 import { Stack } from '@/ui/Stack';
@@ -5,24 +10,48 @@ import { Text } from '@/ui/Text';
 import { Title } from '@/ui/Title';
 
 const NewsletterSignupForm = () => {
+  const formRef = useRef<HTMLFormElement>();
+  const [email, setEmail] = useState('');
+
+  const handleSubmit = () => {
+    if (email.length > 0 && !!formRef?.current?.checkValidity()) {
+      // do stuff
+      console.log('do stuff');
+    }
+  };
+
   return (
-    <Stack spacing={3}>
+    <Stack spacing={5}>
       <Text>
         Vragen of feedback? <Link href="#">Contacteer ons</Link>
       </Text>
-      <Stack backgroundColor="white" padding={3} spacing={3}>
+      <Stack backgroundColor="white" padding={4} spacing={4}>
         <Title>Praktische invoertips in je mailbox</Title>
         <Text>
           Schrijf je in op onze nieuwsbrief en ontvang praktische tips voor het
           invoeren van jouw activiteiten in de UiTdatabank!
         </Text>
-        <InputWithLabel
-          type="email"
-          id="newletter-email"
-          label="Email"
-          placeholder="test@test.be"
-          onInput={() => {}}
-        />
+        <Inline
+          as="form"
+          ref={formRef}
+          spacing={4}
+          onSubmit={(e) => {
+            e.preventDefault();
+            handleSubmit();
+          }}
+        >
+          <InputWithLabel
+            type="email"
+            id="newletter-email"
+            label="Email"
+            placeholder="test@test.be"
+            onInput={(e) => {
+              setEmail(e.target.value);
+            }}
+            value={email}
+          />
+          <Button onClick={handleSubmit}>Inschrijven</Button>
+        </Inline>
       </Stack>
     </Stack>
   );
