d68678f0e5cdf65195d3b6aaf1cb8de1abd60a5b
fulldev1023 <simbadevlop@gmail.com>
2021-06-23 15:25:54 +0200

    Structure all element in newsletter correctly

--- src/components/pages/dashboard/NewsletterSingupForm.tsx
+++ src/components/pages/dashboard/NewsletterSingupForm.tsx
@@ -3,11 +3,14 @@ import { useTranslation } from 'react-i18next';
 import validator from 'validator';
 
 import { Alert, AlertVariants } from '@/ui/Alert';
+import { parseSpacing } from '@/ui/Box';
 import { Button } from '@/ui/Button';
+import { Image } from '@/ui/Image';
 import { Inline } from '@/ui/Inline';
 import { InputWithLabel } from '@/ui/InputWithLabel';
 import { Link } from '@/ui/Link';
 import { Panel } from '@/ui/Panel';
+import { Paragraph } from '@/ui/Paragraph';
 import { Stack } from '@/ui/Stack';
 import { Text } from '@/ui/Text';
 import { Title } from '@/ui/Title';
@@ -41,33 +44,43 @@ const NewsletterSignupForm = () => {
       </Text>
       {i18n.language === 'nl' && (
         <Panel backgroundColor="white" padding={4} spacing={4}>
-          <Title>{t('dashboard.newsletter.title')}</Title>
-          <Text>{t('dashboard.newsletter.content')}</Text>
-          <Inline
-            as="form"
-            ref={formRef}
-            spacing={4}
-            onSubmit={(e) => {
-              e.preventDefault();
-              handleSubmit();
-            }}
-          >
-            <InputWithLabel
-              type="email"
-              id="newletter-email"
-              label="Email"
-              placeholder="test@test.be"
-              onInput={(e) => {
-                setIsValid(true);
-                setEmail(e.target.value);
-              }}
-              value={email}
+          <Inline as="div" justifyContent="space-between">
+            <Stack spacing={4}>
+              <Title>{t('dashboard.newsletter.title')}</Title>
+              <Paragraph>{t('dashboard.newsletter.content')}</Paragraph>
+              <Inline
+                as="form"
+                ref={formRef}
+                onSubmit={(e) => {
+                  e.preventDefault();
+                  handleSubmit();
+                }}
+                alignItems="center"
+                spacing={4}
+              >
+                <InputWithLabel
+                  type="email"
+                  id="newletter-email"
+                  label="Email"
+                  placeholder="test@test.be"
+                  onInput={(e) => {
+                    setIsValid(true);
+                    setEmail(e.target.value);
+                  }}
+                  value={email}
+                  flex={1}
+                  maxWidth="30rem"
+                />
+                <Button onClick={handleSubmit} maxHeight={parseSpacing(5)()}>
+                  {t('dashboard.newsletter.subscribe')}
+                </Button>
+              </Inline>
+            </Stack>
+            <Image
+              src={`/assets/${t('dashboard.newsletter.logo')}`}
+              width={300}
             />
-            <Button onClick={handleSubmit}>
-              {t('dashboard.newsletter.subscribe')}
-            </Button>
           </Inline>
-
           {!isValid && (
             <Alert variant={AlertVariants.DANGER}>
               {t('dashboard.newsletter.invalid')}
