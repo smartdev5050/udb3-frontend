06a9efc210bc394bd56b220b74b0a8600fe5c053
fulldev1023 <simbadevlop@gmail.com>
2021-07-02 11:27:52 +0200

    Add redirect page and use it for events organizers and places

---
+++ src/components/pages/redirect.tsx
@@ -0,0 +1,6 @@
+import { getApplicationServerSideProps } from '@/utils/getApplicationServerSideProps';
+
+const Redirect = () => null;
+
+export default Redirect;
+export const getServerSideProps = getApplicationServerSideProps();
