d6b9ef3c579d81b16fc01ccee4588fad9b2b428d
fulldev1023 <simbadevlop@gmail.com>
2021-08-17 13:52:37 +0200

    Add useLog back in

--- src/hooks/useLog.ts
+++ src/hooks/useLog.ts
@@ -1,12 +1,33 @@
-import { useEffect } from 'react';
+import { detailedDiff } from 'deep-object-diff';
+import pick from 'lodash/pick';
+import { useEffect, useRef } from 'react';
 
-const useLog = (deps: { [key: string]: unknown }) => {
-  useEffect(
+const diff = (oldValue, newValue) => {
+  const { added = {}, deleted = {}, updated = {} } = detailedDiff(
+    oldValue,
+    newValue,
+  ) as {
+    added: Record<string, unknown>;
+    deleted: Record<string, unknown>;
+    updated: Record<string, unknown>;
+  };
+  return pick(newValue, Object.keys({ ...deleted, ...added, ...updated }));
+};
+
+const useLog = (
+  variables,
+  { rawData = false, showOnlyDifference = false } = {},
+) => {
+  const ref = useRef();
+
+  useEffect(() => {
+    if (process.env.NODE_ENV === 'production') return;
+    const toLog = showOnlyDifference ? diff(ref.current, variables) : variables;
+    ref.current = variables;
+
     // eslint-disable-next-line no-console
-    () => console.log(deps),
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-    Object.values(deps),
-  );
+    console.log(rawData ? toLog : JSON.stringify(toLog, undefined, 2));
+  }, Object.values(variables));
 };
 
 export { useLog };
