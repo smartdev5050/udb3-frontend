08babfaa00e0d0e4d58935ad5303e589df2ddf66
fulldev1023 <simbadevlop@gmail.com>
2021-09-02 13:23:49 +0200

    Enable Step 5

--- src/components/pages/manage/movies/create.tsx
+++ src/components/pages/manage/movies/create.tsx
@@ -18,6 +18,7 @@ import { Step1 } from './Step1';
 import { Step2 } from './Step2';
 import { Step3 } from './Step3';
 import { Step4 } from './Step4';
+import { Step5 } from './Step5';
 
 const MovieEventTypes = {
   CHOOSE_THEME: 'CHOOSE_THEME',
@@ -130,7 +131,7 @@ const Create = () => {
 
   useLog({ production: movieState.context.production });
 
-  const steps = useMemo(() => [Step1, Step2, Step3, Step4], []);
+  const steps = useMemo(() => [Step1, Step2, Step3, Step4, Step5], []);
 
   return (
     <Page>
