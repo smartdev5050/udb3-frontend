26c1ade3f094c6b7d65774331a9b2b731ae3403d
fulldev1023 <simbadevlop@gmail.com>
2021-09-10 09:54:53 +0200

    Allow empty group label

--- src/ui/RadioButtonGroup.js
+++ src/ui/RadioButtonGroup.js
@@ -15,7 +15,9 @@ const RadioButtonGroup = ({
 }) => {
   return (
     <Stack className={className} as="div" spacing={3} {...getStackProps(props)}>
-      {groupLabel && <Label variant={LabelVariants.BOLD}>{groupLabel}</Label>}
+      {groupLabel !== '' && (
+        <Label variant={LabelVariants.BOLD}>{groupLabel}</Label>
+      )}
       <Stack role="radiogroup" as="ul" spacing={2}>
         {items.map((item) => (
           <RadioButtonWithLabel
