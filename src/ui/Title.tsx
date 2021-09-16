f246d96fb5d5403cc44cf42a3bf8943b1d402f7f
fulldev1023 <simbadevlop@gmail.com>
2021-09-16 09:00:55 +0200

    Add an underlined title variant

--- src/ui/Title.tsx
+++ src/ui/Title.tsx
@@ -1,6 +1,15 @@
 import type { InlineProps } from './Inline';
 import { getInlineProps, Inline } from './Inline';
 import { Text } from './Text';
+import { css } from 'styled-components';
+import { getValueFromTheme } from '@/ui/theme';
+
+const getValue = getValueFromTheme('title');
+
+const TitleVariants = {
+  DEFAULT: 'default',
+  UNDERLINED: 'underlined',
+} as const;
 
 const getFontWeight = (props) => {
   if (props.size === 1) return 300;
@@ -12,17 +21,35 @@ const getFontSize = (props) => {
   return 1.2;
 };
 
+const getBorderBottom = (props) => {
+  if (props.variant === TitleVariants.UNDERLINED) {
+    return css`
+      border-bottom: 1px solid ${getValue('borderColor')};
+    `;
+  }
+
+  return css``;
+};
+
 type TitleProps = InlineProps;
 
-const Title = ({ size, children, className, ...props }: TitleProps) => {
+const Title = ({
+  size,
+  variant,
+  children,
+  className,
+  ...props
+}: TitleProps) => {
   return (
     <Inline
       forwardedAs={`h${size}`}
       size={size}
+      variant={variant}
       className={className}
       css={`
         font-weight: ${getFontWeight};
         font-size: ${getFontSize}rem;
+        ${getBorderBottom}
       `}
       {...getInlineProps(props)}
     >
@@ -33,7 +60,8 @@ const Title = ({ size, children, className, ...props }: TitleProps) => {
 
 Title.defaultProps = {
   size: 2,
+  variant: TitleVariants.DEFAULT,
 };
 
-export { Title };
+export { Title, TitleVariants };
 export type { TitleProps };
