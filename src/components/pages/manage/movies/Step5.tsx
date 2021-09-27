89921ab82e09545804f0bc19de138c69cdc6922a
fulldev1023 <simbadevlop@gmail.com>
2021-09-27 13:47:06 +0200

    Add PictureUploadModal with basic layout

--- src/components/pages/manage/movies/Step5.tsx
+++ src/components/pages/manage/movies/Step5.tsx
@@ -1,9 +1,15 @@
-import { useState } from 'react';
+import { yupResolver } from '@hookform/resolvers/yup';
+import { useRef, useState } from 'react';
+import { useForm } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
+import * as yup from 'yup';
 
 import { Button, ButtonVariants } from '@/ui/Button';
+import { Icon, Icons } from '@/ui/Icon';
 import { Inline } from '@/ui/Inline';
-import { Modal, ModalVariants } from '@/ui/Modal';
+import { InputWithLabel } from '@/ui/InputWithLabel';
+import { Modal, ModalSizes, ModalVariants } from '@/ui/Modal';
+import { Paragraph } from '@/ui/Paragraph';
 import type { StackProps } from '@/ui/Stack';
 import { Stack } from '@/ui/Stack';
 import { Text, TextVariants } from '@/ui/Text';
@@ -17,15 +23,130 @@ const getValue = getValueFromTheme('moviesCreatePage');
 
 type Step5Props = StackProps & MachineProps;
 
+type FormData = {
+  description: string;
+  copyright: string;
+};
+
 const PictureUploadModal = ({ visible, onClose }) => {
+  const { t } = useTranslation();
+  const formComponent = useRef<HTMLFormElement>();
+
+  const schema = yup
+    .object()
+    .shape({
+      description: yup.string().required().max(250),
+      copyright: yup.string().required(),
+    })
+    .required();
+
+  const {
+    register,
+    formState: { errors },
+    handleSubmit,
+  } = useForm<FormData>({
+    resolver: yupResolver(schema),
+  });
+
   return (
     <Modal
-      title="afbeelding toevoegen"
+      title={t('movies.create.modal.title')}
       visible={visible}
-      variant={ModalVariants.CONTENT}
-      size="lg"
+      variant={ModalVariants.QUESTION}
       onClose={onClose}
-    />
+      confirmTitle="Opladen"
+      cancelTitle="Annuleren"
+      size={ModalSizes.MD}
+      onConfirm={() => {
+        formComponent.current.dispatchEvent(
+          new Event('submit', { cancelable: true, bubbles: true }),
+        );
+      }}
+      confirmButtonDisabled={Object.keys(errors).length > 0}
+    >
+      <Stack
+        as="form"
+        ref={formComponent}
+        spacing={4}
+        padding={4}
+        onSubmit={handleSubmit()}
+      >
+        <Stack
+          flex={1}
+          spacing={4}
+          height={300}
+          backgroundColor={getValue('pictureUploadBox.backgroundColor')}
+          justifyContent="center"
+          alignItems="center"
+          css={`
+            border: 1px solid ${getValue('pictureUploadBox.borderColor')};
+          `}
+          padding={4}
+        >
+          <Text fontWeight={700}>Selecteer je foto</Text>
+          <Icon
+            name={Icons.IMAGE}
+            width="4rem"
+            height="4rem"
+            color={getValue('pictureUploadBox.imageIconColor')}
+          />
+          <Stack spacing={2} alignItems="center">
+            <Text>Sleep een bestand hierheen of</Text>
+            <Button>Kies bestand</Button>
+          </Stack>
+          <Text variant={TextVariants.MUTED} textAlign="center">
+            De maximale grootte van je afbeelding is 5MB en heeft als type
+            .jpeg, .gif of .png
+          </Text>
+        </Stack>
+        <InputWithLabel
+          id="description"
+          label="Beschrijving"
+          info="Maximum 250 karakters"
+          required
+          error={
+            errors.description &&
+            t(
+              `movies.create.modal.validation_messages.description.${errors.description.type}`,
+            )
+          }
+          {...register('description')}
+        />
+
+        <InputWithLabel
+          id="copyright"
+          label="Copyright"
+          required
+          info={
+            <Stack spacing={3}>
+              <Paragraph>
+                Vermeld de naam van de rechtenhoudende fotograaf. Vul alleen de
+                naam van je eigen vereniging of organisatie in als je zelf de
+                rechten bezit (minimum 2 karakters).
+              </Paragraph>
+              <Paragraph>
+                Je staat op het punt (een) afbeelding(en) toe te voegen en
+                openbaar te verspreiden. Je dient daartoe alle geldende auteurs-
+                en portretrechten te respecteren, alsook alle andere
+                toepasselijke wetgeving. Je kan daarvoor aansprakelijk worden
+                gehouden, zoals vastgelegd in de algemene voorwaarden. Meer
+                informatie over copyright
+              </Paragraph>
+            </Stack>
+          }
+          error={
+            errors.copyright &&
+            t(
+              `movies.create.modal.validation_messages.copyright.${errors.copyright.type}`,
+            )
+          }
+          {...register('copyright')}
+        />
+        <Text>
+          <Text color="red">*</Text> verplicht veld
+        </Text>
+      </Stack>
+    </Modal>
   );
 };
 
