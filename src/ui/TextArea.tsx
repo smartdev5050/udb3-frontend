1e2c3ff7358eec52628b021074627000b9e2917c
fulldev1023 <simbadevlop@gmail.com>
2021-10-12 15:07:58 +0200

    Add onChange ref and name props

--- src/ui/TextArea.tsx
+++ src/ui/TextArea.tsx
@@ -1,39 +1,52 @@
+import type { ChangeEvent } from 'react';
+import { forwardRef } from 'react';
 import { Form } from 'react-bootstrap';
 
 import type { BoxProps } from './Box';
 import { Box, getBoxProps } from './Box';
 
-type TextAreaProps = { value: string; rows?: number };
-
-type Props = BoxProps & TextAreaProps;
-
-const BaseInput = (props: Props) => <Box as="textarea" {...props} />;
-
-const TextArea = ({
-  id,
-  className,
-  onInput,
-  value,
-  disabled,
-  rows,
-  ...props
-}: Props) => {
-  return (
-    <Form.Control
-      forwardedAs={BaseInput}
-      id={id}
-      className={className}
-      width="100%"
-      minHeight="4rem"
-      onInput={onInput}
-      value={value}
-      disabled={disabled}
-      css="border-radius: 0;"
-      rows={rows}
-      {...getBoxProps(props)}
-    />
-  );
+type TextAreaProps = {
+  name?: string;
+  value?: string;
+  rows?: number;
+  onChange: (e: ChangeEvent<HTMLTextAreaElement>) => void;
 };
+
+type Props = Omit<BoxProps, 'onChange'> & TextAreaProps;
+
+const BaseInput = forwardRef<HTMLTextAreaElement, Props>((props, ref) => (
+  <Box as="textarea" {...props} ref={ref} />
+));
+
+BaseInput.displayName = 'BaseInput';
+
+const TextArea = forwardRef<HTMLInputElement, Props>(
+  (
+    { id, className, onInput, value, disabled, rows, onChange, name, ...props },
+    ref,
+  ) => {
+    return (
+      <Form.Control
+        forwardedAs={BaseInput}
+        id={id}
+        className={className}
+        width="100%"
+        minHeight="4rem"
+        onInput={onInput}
+        onChange={onChange}
+        value={value}
+        disabled={disabled}
+        css="border-radius: 0;"
+        rows={rows}
+        ref={ref}
+        name={name}
+        {...getBoxProps(props)}
+      />
+    );
+  },
+);
+
+TextArea.displayName = 'TextArea';
 
 TextArea.defaultProps = {
   disabled: false,
--- src/ui/TextAreaWithLabel.tsx
+++ src/ui/TextAreaWithLabel.tsx
@@ -1,41 +1,68 @@
+import { forwardRef } from 'react';
+
 import { Label, LabelVariants } from './Label';
 import type { StackProps } from './Stack';
 import { getStackProps, Stack } from './Stack';
 import type { TextAreaProps } from './TextArea';
 import { TextArea } from './TextArea';
 
-type Props = StackProps & TextAreaProps & { label: string; id: string };
+type Props = Omit<StackProps, 'onChange'> &
+  TextAreaProps & { label: string; id: string };
 
-const TextAreaWithLabel = ({
-  id,
-  label,
-  className,
-  onInput,
-  value,
-  disabled,
-  rows,
-  ...props
-}: Props) => {
-  return (
-    <Stack
-      as="div"
-      spacing={2}
-      className={className}
-      flex={1}
-      {...getStackProps(props)}
-    >
-      <Label htmlFor={id} variant={LabelVariants.BOLD}>
-        {label}
-      </Label>
-      <TextArea
-        id={id}
-        rows={rows}
-        onInput={onInput}
-        value={value}
-        disabled={disabled}
-      />
-    </Stack>
-  );
-};
+const TextAreaWithLabel = forwardRef<HTMLInputElement, Props>(
+  (
+    {
+      id,
+      label,
+      className,
+      onInput,
+      disabled,
+      rows,
+      onChange,
+      value,
+      name,
+      ...props
+    }: Props,
+    ref,
+  ) => {
+    console.log({
+      id,
+      label,
+      className,
+      onInput,
+      disabled,
+      rows,
+      onChange,
+      value,
+      name,
+      ...props,
+    });
+    return (
+      <Stack
+        as="div"
+        spacing={2}
+        className={className}
+        flex={1}
+        {...getStackProps(props)}
+      >
+        <Label htmlFor={id} variant={LabelVariants.BOLD}>
+          {label}
+        </Label>
+        <TextArea
+          id={id}
+          rows={rows}
+          onInput={onInput}
+          onChange={onChange}
+          value={value}
+          disabled={disabled}
+          ref={ref}
+          name={name}
+        />
+      </Stack>
+    );
+  },
+);
+
+TextAreaWithLabel.displayName = 'TextAreaWithLabel';
 
 export { TextAreaWithLabel };
