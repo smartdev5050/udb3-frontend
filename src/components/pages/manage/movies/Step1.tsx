d5b114d6f0d7e813ae9c78f3572463787d2403fe
fulldev1023 <simbadevlop@gmail.com>
2021-10-14 10:26:58 +0200

    Add validation for Step 1

--- src/components/pages/manage/movies/Step1.tsx
+++ src/components/pages/manage/movies/Step1.tsx
@@ -1,3 +1,4 @@
+import { Controller } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
 
 import { MovieThemes } from '@/constants/MovieThemes';
@@ -10,61 +11,62 @@ import { getStackProps } from '@/ui/Stack';
 import { Text } from '@/ui/Text';
 import { getValueFromTheme } from '@/ui/theme';
 
-import type { MachineProps } from './create';
-import { MovieEventTypes } from './create';
 import { Step } from './Step';
 
 const getValue = getValueFromTheme('moviesCreatePage');
 
-type Step1Props = StackProps & MachineProps;
+type Step1Props = StackProps;
 
-const Step1 = ({
-  movieState,
-  sendMovieEvent,
-  isInvalid,
-  ...props
-}: Step1Props) => {
+const Step1 = ({ errors, control, reset, ...props }: Step1Props) => {
   const { t } = useTranslation();
 
   return (
     <Step stepNumber={1} {...getStackProps(props)}>
-      <Inline spacing={3} flexWrap="wrap" maxWidth="70rem">
-        {movieState.context.theme === null ? (
-          Object.entries(MovieThemes).map(([key, value]) => (
-            <Button
-              width="auto"
-              marginBottom={3}
-              display="inline-flex"
-              key={key}
-              variant={ButtonVariants.SECONDARY}
-              onClick={() =>
-                sendMovieEvent({ type: MovieEventTypes.CHOOSE_THEME, value })
-              }
-            >
-              {t(`themes*${value}`, { keySeparator: '*' })}
-            </Button>
-          ))
-        ) : (
-          <Inline alignItems="center" spacing={3}>
-            <Icon
-              name={Icons.CHECK_CIRCLE}
-              color={getValue('check.circleFillColor')}
-            />
-            <Text>
-              {t(`themes*${movieState.context.theme}`, { keySeparator: '*' })}
-            </Text>
-            <Button
-              variant={ButtonVariants.LINK}
-              onClick={() =>
-                sendMovieEvent({ type: MovieEventTypes.CLEAR_THEME })
-              }
-            >
-              {t('movies.create.actions.change_theme')}
-            </Button>
-          </Inline>
-        )}
-      </Inline>
-      {isInvalid ? (
+      <Controller
+        name="theme"
+        control={control}
+        render={({ field }) => {
+          if (!field.value) {
+            return (
+              <Inline spacing={3} flexWrap="wrap" maxWidth="70rem">
+                {Object.entries(MovieThemes).map(([key, value]) => (
+                  <Button
+                    width="auto"
+                    marginBottom={3}
+                    display="inline-flex"
+                    key={key}
+                    variant={ButtonVariants.SECONDARY}
+                    onClick={() => field.onChange(value)}
+                  >
+                    {t(`themes*${value}`, { keySeparator: '*' })}
+                  </Button>
+                ))}
+              </Inline>
+            );
+          }
+
+          return (
+            <Inline alignItems="center" spacing={3}>
+              <Icon
+                name={Icons.CHECK_CIRCLE}
+                color={getValue('check.circleFillColor')}
+              />
+              <Text>
+                {t(`themes*${field.value}`, {
+                  keySeparator: '*',
+                })}
+              </Text>
+              <Button
+                variant={ButtonVariants.LINK}
+                onClick={() => reset({ theme: undefined })}
+              >
+                {t('movies.create.actions.change_theme')}
+              </Button>
+            </Inline>
+          );
+        }}
+      />
+      {errors?.theme ? (
         <Alert visible variant={AlertVariants.DANGER} maxWidth="53rem">
           this is an error
         </Alert>
--- src/components/pages/manage/movies/create.tsx
+++ src/components/pages/manage/movies/create.tsx
@@ -1,151 +1,63 @@
-import { useMachine } from '@xstate/react';
+import { yupResolver } from '@hookform/resolvers/yup';
 import { useMemo } from 'react';
+import { useForm } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
-import type { State } from 'xstate';
-import { assign, createMachine } from 'xstate';
+import * as yup from 'yup';
 
-import type { MovieThemes } from '@/constants/MovieThemes';
-import { OfferCategories } from '@/constants/OfferCategories';
-import { OfferType } from '@/constants/OfferType';
-import { useLog } from '@/hooks/useLog';
-import type { Place } from '@/types/Place';
-import type { Production } from '@/types/Production';
-import type { Values } from '@/types/Values';
+import { Button } from '@/ui/Button';
 import { Page } from '@/ui/Page';
 import { getApplicationServerSideProps } from '@/utils/getApplicationServerSideProps';
 
 import { Step1 } from './Step1';
-import { Step2 } from './Step2';
-import { Step3 } from './Step3';
-import { Step4 } from './Step4';
-import { Step5 } from './Step5';
 
-const MovieEventTypes = {
-  CHOOSE_THEME: 'CHOOSE_THEME',
-  CLEAR_THEME: 'CLEAR_THEME',
-  CHANGE_TIME_TABLE: 'CHANGE_TIME_TABLE',
-  CHOOSE_CINEMA: 'CHOOSE_CINEMA',
-  CLEAR_CINEMA: 'CLEAR_CINEMA',
-  CHOOSE_PRODUCTION: 'CHOOSE_PRODUCTION',
-  CLEAR_PRODUCTION: 'CLEAR_PRODUCTION',
-} as const;
-
-type Theme = Values<typeof MovieThemes>;
-type Time = string;
-
-type MovieContext = {
-  offerType: typeof OfferType.EVENT;
-  type: typeof OfferCategories.Film;
-  theme: Theme;
-  timeTable: Time[][];
-  cinema: Place;
-  production: Production;
-};
-
-type MovieEvent =
-  | { type: typeof MovieEventTypes.CHOOSE_THEME; value: Theme }
-  | { type: typeof MovieEventTypes.CLEAR_THEME }
-  | { type: typeof MovieEventTypes.CHANGE_TIME_TABLE; value: Time[][] }
-  | { type: typeof MovieEventTypes.CHOOSE_CINEMA; value: Place }
-  | { type: typeof MovieEventTypes.CLEAR_CINEMA }
-  | { type: typeof MovieEventTypes.CHOOSE_PRODUCTION; value: Production }
-  | { type: typeof MovieEventTypes.CLEAR_PRODUCTION };
-
-type MachineProps = {
-  movieState: State<MovieContext, MovieEvent>;
-  sendMovieEvent: (event: MovieEvent) => State<MovieContext, MovieEvent>;
-};
-
-const movieMachine = createMachine<MovieContext, MovieEvent>({
-  id: 'movie',
-  initial: 'idle',
-  context: {
-    offerType: OfferType.EVENT,
-    type: OfferCategories.Film,
-    theme: null,
-    timeTable: null,
-    cinema: null,
-    production: null,
-  },
-  states: {
-    idle: {
-      on: {
-        [MovieEventTypes.CHOOSE_THEME]: {
-          actions: [
-            assign({
-              theme: (ctx, event) => event.value,
-            }),
-          ],
-        },
-        [MovieEventTypes.CLEAR_THEME]: {
-          actions: [
-            assign({
-              theme: () => null,
-            }),
-          ],
-        },
-        [MovieEventTypes.CHANGE_TIME_TABLE]: {
-          actions: [
-            assign({
-              timeTable: (ctx, event) => event.value,
-            }),
-          ],
-        },
-        [MovieEventTypes.CHOOSE_CINEMA]: {
-          actions: [
-            assign({
-              cinema: (ctx, event) => event.value,
-            }),
-          ],
-        },
-        [MovieEventTypes.CLEAR_CINEMA]: {
-          actions: [
-            assign({
-              cinema: () => null,
-            }),
-          ],
-        },
-        [MovieEventTypes.CHOOSE_PRODUCTION]: {
-          actions: [
-            assign({
-              production: (ctx, event) => event.value,
-            }),
-          ],
-        },
-        [MovieEventTypes.CLEAR_PRODUCTION]: {
-          actions: [
-            assign({
-              production: () => null,
-            }),
-          ],
-        },
-      },
-    },
-  },
-});
+const schema = yup
+  .object({
+    theme: yup.string().required(),
+    // timeTable: yup
+    //   .array()
+    //   .test('has-timeslot', (value) =>
+    //     value.some((rows) => rows.some((cell) => !!cell)),
+    //   )
+    //   .required(),
+    // cinema: yup.string().required(),
+  })
+  .required();
 
 const Create = () => {
-  const [movieState, sendMovieEvent] = useMachine(movieMachine);
+  const {
+    handleSubmit,
+    formState: { errors },
+    register,
+    control,
+    reset,
+  } = useForm({
+    resolver: yupResolver(schema),
+  });
 
   const { t } = useTranslation();
 
-  useLog({ production: movieState.context.production });
+  const requiredSteps = useMemo(() => [Step1], []);
 
-  const steps = useMemo(() => [Step1, Step2, Step3, Step4, Step5], []);
+  const handleFormValid = (values) => {
+    console.log(values);
+  };
 
   return (
     <Page>
       <Page.Title spacing={3} alignItems="center">
         {t(`movies.create.title`)}
       </Page.Title>
-      <Page.Content spacing={5} paddingBottom={6}>
-        {steps.map((Step, index) => (
+      <Page.Content spacing={5} paddingBottom={6} alignItems="flex-start">
+        {requiredSteps.map((Step, index) => (
           <Step
-            movieState={movieState}
-            sendMovieEvent={sendMovieEvent}
             key={index}
+            errors={errors}
+            control={control}
+            register={register}
+            reset={reset}
           />
         ))}
+        <Button onClick={handleSubmit(handleFormValid)}>Opslaan</Button>
       </Page.Content>
     </Page>
   );
@@ -153,6 +65,4 @@ const Create = () => {
 
 export const getServerSideProps = getApplicationServerSideProps();
 
-export type { MachineProps };
-export { MovieEventTypes };
 export default Create;
