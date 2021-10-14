fc4af31ed6ce05cf16ce5942f950c38c7396f78e
fulldev1023 <simbadevlop@gmail.com>
2021-10-14 16:07:37 +0200

    Finetune UX in Step 4

--- src/components/pages/manage/movies/Step4.tsx
+++ src/components/pages/manage/movies/Step4.tsx
@@ -5,16 +5,22 @@ import { useTranslation } from 'react-i18next';
 
 import { useGetProductions } from '@/hooks/api/productions';
 import type { Production } from '@/types/Production';
+import { Button, ButtonVariants } from '@/ui/Button';
+import { Icon, Icons } from '@/ui/Icon';
+import { getInlineProps, Inline } from '@/ui/Inline';
 import type { StackProps } from '@/ui/Stack';
 import { getStackProps } from '@/ui/Stack';
+import { Text } from '@/ui/Text';
+import { getValueFromTheme } from '@/ui/theme';
 import { TypeaheadWithLabel } from '@/ui/TypeaheadWithLabel';
 
 import { Step } from './Step';
 
 type Step4Props = StackProps;
 
-const Step4 = ({ errors, control, ...props }: Step4Props) => {
-  console.log({ errors });
+const getValue = getValueFromTheme('moviesCreatePage');
+
+const Step4 = ({ errors, control, getValues, reset, ...props }: Step4Props) => {
   const { t } = useTranslation();
   const [searchInput, setSearchInput] = useState('');
 
@@ -38,22 +44,47 @@ const Step4 = ({ errors, control, ...props }: Step4Props) => {
         control={control}
         name="production"
         render={({ field }) => {
+          const selectedProduction = field?.value?.[0];
+
+          if (!selectedProduction) {
+            return (
+              <TypeaheadWithLabel<Production>
+                newSelectionPrefix="Voeg nieuwe productie toe: "
+                allowNew
+                error={errors.production ? 'this is another error' : undefined}
+                id="step4-name-typeahead"
+                label={t('movies.create.actions.choose_name')}
+                options={productions}
+                onInputChange={throttle(setSearchInput, 275)}
+                labelKey="name"
+                maxWidth="43rem"
+                selected={field.value}
+                onChange={(value) => field.onChange(value)}
+                minLength={3}
+                {...getStackProps(props)}
+              />
+            );
+          }
+
           return (
-            <TypeaheadWithLabel<Production>
-              newSelectionPrefix="Voeg nieuwe productie toe:"
-              allowNew
-              error={errors.production ? 'this is another error' : undefined}
-              id="step4-name-typeahead"
-              label={t('movies.create.actions.choose_name')}
-              options={productions}
-              onInputChange={throttle(setSearchInput, 275)}
-              labelKey="name"
-              maxWidth="43rem"
-              selected={field.value}
-              onChange={(value) => field.onChange(value)}
-              minLength={3}
-              {...getStackProps(props)}
-            />
+            <Inline alignItems="center" spacing={3} {...getInlineProps(props)}>
+              <Icon
+                name={Icons.CHECK_CIRCLE}
+                color={getValue('check.circleFillColor')}
+              />
+              <Text>{selectedProduction.name}</Text>
+              <Button
+                variant={ButtonVariants.LINK}
+                onClick={() =>
+                  reset(
+                    { ...getValues(), production: undefined },
+                    { keepDirty: true },
+                  )
+                }
+              >
+                {t('movies.create.actions.change_name')}
+              </Button>
+            </Inline>
           );
         }}
       />
