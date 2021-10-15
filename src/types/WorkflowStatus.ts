5c3a82fc3cbbff8c91e0f43a7d275baa4d71add2
fulldev1023 <simbadevlop@gmail.com>
2021-10-15 16:36:54 +0200

    Add map for workflowStatus
    Co-Authored-By: Simon de Bruijn <66943525+simon-debruijn@users.noreply.github.com>

--- src/types/WorkflowStatus.ts
+++ src/types/WorkflowStatus.ts
@@ -1,8 +1,15 @@
-type WorkflowStatus =
-  | 'DRAFT'
-  | 'APPROVED'
-  | 'READY_FOR_VALIDATION'
-  | 'REJECTED'
-  | 'DELETED';
+import { Values } from './Values';
+
+const WorkflowStatusMap = {
+  DRAFT: 'DRAFT',
+  APPROVED: 'APPROVED',
+  READY_FOR_VALIDATION: 'READY_FOR_VALIDATION',
+  REJECTED: 'REJECTED',
+  DELETED: 'DELETED',
+} as const;
+
+type WorkflowStatus = Values<typeof WorkflowStatusMap>;
+
+export { WorkflowStatusMap };
 
 export type { WorkflowStatus };
