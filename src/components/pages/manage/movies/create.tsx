ef2d4f0b1c49942fc806a9a6dcb50f5516004da3
fulldev1023 <simbadevlop@gmail.com>
2021-10-20 10:20:10 +0200

    Remove unused FormState

--- src/components/pages/manage/movies/create.tsx
+++ src/components/pages/manage/movies/create.tsx
@@ -1,7 +1,7 @@
 import { yupResolver } from '@hookform/resolvers/yup';
 import { addDays, set as setTime } from 'date-fns';
 import { useState } from 'react';
-import type { FormState, UseFormReturn } from 'react-hook-form';
+import type { UseFormReturn } from 'react-hook-form';
 import { useForm } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
 import { useQueryClient } from 'react-query';
@@ -91,7 +91,9 @@ type FormData = {
 type StepProps = Pick<
   UseFormReturn<FormData>,
   'control' | 'getValues' | 'register' | 'reset'
-> & { errors: Partial<Record<keyof FormData, any>> };
+> & {
+  errors: Partial<Record<keyof FormData, any>>;
+};
 
 const Create = () => {
   const {
