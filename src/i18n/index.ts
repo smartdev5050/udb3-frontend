f2ae74c265c882e21ba9cfc905d37a3d6f7cec42
fulldev1023 <simbadevlop@gmail.com>
2021-10-29 09:30:44 +0200

    Add german json file

--- src/i18n/index.ts
+++ src/i18n/index.ts
@@ -2,6 +2,7 @@ import i18n from 'i18next';
 import LanguageDetector from 'i18next-browser-languagedetector';
 import { initReactI18next } from 'react-i18next';
 
+import de from './de.json';
 import fr from './fr.json';
 import nl from './nl.json';
 
