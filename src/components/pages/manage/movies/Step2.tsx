7abe52a3825e1c32ceb30bf788dfca3b3c8054f8
fulldev1023 <simbadevlop@gmail.com>
2021-11-19 16:43:38 +0100

    Add some spacing between timeTable and error message

--- src/components/pages/manage/movies/Step2.tsx
+++ src/components/pages/manage/movies/Step2.tsx
@@ -2,6 +2,7 @@ import { Controller } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
 
 import { Alert, AlertVariants } from '@/ui/Alert';
+import { Box, parseSpacing } from '@/ui/Box';
 import type { StackProps } from '@/ui/Stack';
 import { getStackProps } from '@/ui/Stack';
 import { TimeTable } from '@/ui/TimeTable';
@@ -22,24 +23,24 @@ const Step2 = ({
   const { t } = useTranslation();
 
   return (
-    <Step stepNumber={2} {...getStackProps(props)}>
-      <Controller
-        name="timeTable"
-        control={control}
-        render={({ field }) => {
-          return (
-            <TimeTable
-              id="timetable-movies"
-              value={field.value}
-              onChange={(value) => field.onChange(value)}
-              dateStart={dateStart}
-              onDateStartChange={onDateStartChange}
-              {...getStackProps(props)}
-            />
-          );
-        }}
-      />
-
+    <Step stepNumber={2} spacing={3} {...getStackProps(props)}>
+      <Box>
+        <Controller
+          name="timeTable"
+          control={control}
+          render={({ field }) => {
+            return (
+              <TimeTable
+                id="timetable-movies"
+                value={field.value}
+                onChange={(value) => field.onChange(value)}
+                dateStart={dateStart}
+                onDateStartChange={onDateStartChange}
+              />
+            );
+          }}
+        />
+      </Box>
       {errors?.timeTable ? (
         <Alert visible variant={AlertVariants.DANGER} maxWidth="53rem">
           {t(
