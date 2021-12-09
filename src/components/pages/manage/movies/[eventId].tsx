265bc7c7243be932ed537131a4b2c15de71e46aa
fulldev1023 <simbadevlop@gmail.com>
2021-12-09 11:47:34 +0100

    prefetch event on edit page

--- src/components/pages/manage/movies/[eventId].tsx
+++ src/components/pages/manage/movies/[eventId].tsx
@@ -1,7 +1,27 @@
+import { dehydrate } from 'react-query/hydration';
+
+import { useGetEventById } from '@/hooks/api/events';
 import { getApplicationServerSideProps } from '@/utils/getApplicationServerSideProps';
 
 import { MoviePage } from './MoviePage';
 
-export const getServerSideProps = getApplicationServerSideProps();
+export const getServerSideProps = getApplicationServerSideProps(
+  async ({ req, query, cookies, queryClient }) => {
+    const { eventId } = query;
+
+    await useGetEventById({
+      req,
+      queryClient,
+      id: eventId,
+    });
+
+    return {
+      props: {
+        dehydratedState: dehydrate(queryClient),
+        cookies,
+      },
+    };
+  },
+);
 
 export default MoviePage;
