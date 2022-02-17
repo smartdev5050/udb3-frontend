ece8a0a12fad3542f8d734861ce256b52cbc2e2b
fulldev1023 <simbadevlop@gmail.com>
2022-02-17 10:21:04 +0100

    Set up Step2 component

--- src/components/pages/create.tsx
+++ src/components/pages/create.tsx
@@ -1,10 +1,31 @@
+import { useTranslation } from 'react-i18next';
+
+import { Box } from '@/ui/Box';
 import { Page } from '@/ui/Page';
+import { Stack } from '@/ui/Stack';
 import { getApplicationServerSideProps } from '@/utils/getApplicationServerSideProps';
+
+import { Step } from '../Step';
+
+const Step2 = () => {
+  const { t } = useTranslation();
+
+  return (
+    <Step stepNumber={2} title={t(`create.step2.title`)}>
+      <Box>test</Box>
+    </Step>
+  );
+};
 
 const Create = () => {
   return (
     <Page>
       <Page.Title>create</Page.Title>
+      <Page.Content>
+        <Stack>
+          <Step2 />
+        </Stack>
+      </Page.Content>
     </Page>
   );
 };
