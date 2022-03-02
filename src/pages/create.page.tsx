86065543038c834c8014dabfd10370cf415f87e6
fulldev1023 <simbadevlop@gmail.com>
2022-03-02 15:42:48 +0100

    Remove dummy steps

--- src/pages/create.page.tsx
+++ src/pages/create.page.tsx
@@ -1,38 +1,11 @@
-import { useTranslation } from 'react-i18next';
-
-import { Step } from '@/pages/Step';
-import { Box } from '@/ui/Box';
 import { Page } from '@/ui/Page';
 import { getApplicationServerSideProps } from '@/utils/getApplicationServerSideProps';
-
-const Step1 = (props) => {
-  const { t } = useTranslation();
-
-  return (
-    <Step stepNumber={1} title={t(`create.step1.title`)} {...props}>
-      <Box>test</Box>
-    </Step>
-  );
-};
-
-const Step2 = (props) => {
-  const { t } = useTranslation();
-
-  return (
-    <Step stepNumber={2} title={t(`create.step2.title`)} {...props}>
-      <Box>test</Box>
-    </Step>
-  );
-};
 
 const Create = () => {
   return (
     <Page>
       <Page.Title>create</Page.Title>
-      <Page.Content spacing={5}>
-        <Step1 />
-        <Step2 />
-      </Page.Content>
+      <Page.Content spacing={5}>test</Page.Content>
     </Page>
   );
 };
