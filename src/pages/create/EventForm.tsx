13ff021f6e42b01bc710ba5b1024f51e200a9bc8
fulldev1023 <simbadevlop@gmail.com>
2022-03-03 11:58:57 +0100

    Create placeholder components for type, theme and calendar

--- src/pages/create/EventForm.tsx
+++ src/pages/create/EventForm.tsx
@@ -1,15 +1,67 @@
+import { yupResolver } from '@hookform/resolvers/yup';
+import { useMemo } from 'react';
+import { useForm } from 'react-hook-form';
+import { useTranslation } from 'react-i18next';
+import * as yup from 'yup';
+
+import type { StepsConfiguration } from '@/pages/Steps';
+import { Steps } from '@/pages/Steps';
 import { Page } from '@/ui/Page';
-import { Text } from '@/ui/Text';
+
+import { CalendarStep } from './CalendarStep';
+import { ThemeStep } from './ThemeStep';
+import { TypeStep } from './TypeStep';
+
+type FormData = {
+  type: any;
+  theme: any;
+  calendar: any;
+};
+
+const schema = yup
+  .object({
+    type: yup.string().required(),
+    theme: yup.string().required(),
+    calendar: yup.mixed().required(),
+  })
+  .required();
 
 const EventForm = () => {
+  const { t } = useTranslation();
+
+  const form = useForm<FormData>({
+    resolver: yupResolver(schema),
+  });
+
+  const configuration: StepsConfiguration<FormData> = useMemo(() => {
+    return [
+      {
+        Component: TypeStep,
+        field: 'type',
+        title: t(`event.create.type.title`),
+      },
+      {
+        Component: ThemeStep,
+        field: 'theme',
+        title: t(`event.create.theme.title`),
+      },
+      {
+        Component: CalendarStep,
+        field: 'calendar',
+        title: t(`event.create.calendar.title`),
+      },
+    ];
+  }, [t]);
+
   return (
     <Page>
       <Page.Title>create</Page.Title>
       <Page.Content spacing={5}>
-        <Text>test</Text>
+        <Steps configuration={configuration} {...form} />
       </Page.Content>
     </Page>
   );
 };
 
 export { EventForm };
+export type { FormData };
