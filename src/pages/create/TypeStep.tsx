9350f23d89284df332c7388e4200b13aeefdc001
fulldev1023 <simbadevlop@gmail.com>
2022-03-10 11:54:44 +0100

    Set-up offerType step with ToggleBox component

--- src/pages/create/TypeStep.tsx
+++ src/pages/create/TypeStep.tsx
@@ -1,15 +1,41 @@
 import { Controller } from 'react-hook-form';
+import { useTranslation } from 'react-i18next';
 
+import { OfferType } from '@/constants/OfferType';
 import type { StepProps } from '@/pages/Steps';
-import { Text } from '@/ui/Text';
+import { Icons } from '@/ui/Icon';
+import { Inline } from '@/ui/Inline';
+import { ToggleBox } from '@/ui/ToggleBox';
 
 const TypeStep = ({ control, field }: StepProps<FormData>) => {
+  const { t } = useTranslation();
+
   return (
     <Controller
       control={control}
       name={field}
       render={({ field }) => {
-        return <Text>{field.name}</Text>;
+        return (
+          <Inline
+            spacing={5}
+            alignItems="center"
+            justifyContent="space-between"
+          >
+            <ToggleBox
+              onClick={() => field.onChange(OfferType.EVENT)}
+              active={field.value === OfferType.EVENT}
+              icon={Icons.CALENDAR_ALT}
+              text={t('offerTypes.event')}
+            />
+            <span css="font-style: italic;">{t('steps.offerTypeStep.or')}</span>
+            <ToggleBox
+              onClick={() => field.onChange(OfferType.PLACE)}
+              active={field.value === OfferType.PLACE}
+              icon={Icons.BUILDING}
+              text={t('offerTypes.place')}
+            />
+          </Inline>
+        );
       }}
     />
   );
