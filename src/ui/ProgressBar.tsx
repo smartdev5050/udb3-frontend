23233ede750bd7e99a7bba16b0eb814a0325e196
fulldev1023 <simbadevlop@gmail.com>
2022-03-16 14:19:48 +0100

    III-4475 - Use box in progressbar

--- src/ui/ProgressBar.tsx
+++ src/ui/ProgressBar.tsx
@@ -1,9 +1,36 @@
 import { ProgressBar as BootstrapProgressBar } from 'react-bootstrap';
 
-const ProgressBar = ({ children, className, ...props }) => {
-  return <BootstrapProgressBar now={60} />;
+import type { Values } from '@/types/Values';
+
+import type { BoxProps } from './Box';
+import { Box, getBoxProps } from './Box';
+
+const ProgressBarVariants = {
+  INFO: 'info',
+  SUCCESS: 'success',
+  DANGER: 'danger',
+  WARNING: 'warning',
+} as const;
+
+type Props = BoxProps & {
+  progress: number;
+  variant: Values<typeof ProgressBarVariants>;
+};
+
+const ProgressBar = ({
+  children,
+  className,
+  progress,
+  variant,
+  ...props
+}: Props) => {
+  return (
+    <Box {...getBoxProps(props)}>
+      <BootstrapProgressBar now={progress} variant={variant} />
+    </Box>
+  );
 };
 
 ProgressBar.defaultProps = {};
 
-export { ProgressBar };
+export { ProgressBar, ProgressBarVariants };
