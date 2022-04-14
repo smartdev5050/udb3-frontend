d73a0c3ef84a329524d33db6e14dc3e8bb613f03
fulldev1023 <simbadevlop@gmail.com>
2022-04-14 14:57:36 +0200

    III-4483 - Add ContactInfoTypes and translations

--- src/pages/steps/ContactInfo.tsx
+++ src/pages/steps/ContactInfo.tsx
@@ -1,8 +1,16 @@
 import { useTranslation } from 'react-i18next';
 
+import { Button, ButtonVariants } from '@/ui/Button';
 import { Inline } from '@/ui/Inline';
+import { Select } from '@/ui/Select';
 import { Stack } from '@/ui/Stack';
 import { Text } from '@/ui/Text';
+
+const ContactInfoType = {
+  PHONE: 'phone',
+  EMAIL: 'email',
+  URL: 'url',
+} as const;
 
 type Props = {};
 
@@ -15,7 +23,25 @@ const ContactInfo = ({}: Props) => {
         <Text fontWeight="bold">
           {t('create.additionalInformation.contact_info.title')}
         </Text>
+        <Button
+          onClick={() => console.log('add price info')}
+          variant={ButtonVariants.SECONDARY}
+        >
+          Contactinformatie toevoegen
+        </Button>
       </Inline>
+      <Stack>
+        <Text>Price Info</Text>
+        <Select>
+          {Object.keys(ContactInfoType).map((key, index) => (
+            <option key={index} value={ContactInfoType[key]}>
+              {t(
+                `create.additionalInformation.contact_info.${ContactInfoType[key]}`,
+              )}
+            </option>
+          ))}
+        </Select>
+      </Stack>
     </Stack>
   );
 };
