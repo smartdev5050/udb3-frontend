8da68d5859be90c018a127f89d0d7fb6f916476b
fulldev1023 <simbadevlop@gmail.com>
2022-07-25 08:32:28 +0200

    III-4488 - Get terms from taxonomy API

---
+++ src/hooks/api/terms.ts
@@ -0,0 +1,33 @@
+import getConfig from 'next/config';
+import { useQuery } from 'react-query';
+
+type TermScope = 'events' | 'places';
+type TermDomain = 'eventtype' | 'theme' | 'facility';
+
+type Term = {
+  id: string;
+  domain: TermDomain;
+  name: {
+    nl: string;
+    fr: string;
+    de: string;
+    en: string;
+  };
+  scope: TermScope[];
+};
+
+type EventType = Term & {
+  otherSuggestedTerms?: Term[];
+};
+
+const getTerms = async (): Promise<{ terms: EventType[] }> => {
+  const { publicRuntimeConfig } = getConfig();
+  const res = await fetch(`${publicRuntimeConfig.taxonomyUrl}/terms`);
+  return await res.json();
+};
+
+const useGetTermsQuery = (configuration = { retry: false }) =>
+  useQuery(['terms'], getTerms, configuration);
+
+export { useGetTermsQuery };
+export type { EventType };
