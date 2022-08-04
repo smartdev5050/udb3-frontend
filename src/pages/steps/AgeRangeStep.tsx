a629c8e936bdd108a2cefd42c5cdb5fd23fc9ee6
fulldev1023 <simbadevlop@gmail.com>
2022-08-04 14:04:43 +0200

    III-4474 - Handle custom age range

--- src/pages/steps/AgeRangeStep.tsx
+++ src/pages/steps/AgeRangeStep.tsx
@@ -1,3 +1,4 @@
+import { FormEvent } from 'react';
 import { Controller } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
 import { css } from 'styled-components';
@@ -12,14 +13,14 @@ import { getValueFromTheme } from '@/ui/theme';
 const getValue = getValueFromTheme('ageRange');
 
 const AgeRanges = {
-  ALL: {},
-  TODDLERS: { min: 0, max: 2 },
-  PRESCHOOLERS: { min: 3, max: 5 },
-  KIDS: { min: 6, max: 11 },
-  TEENAGERS: { min: 12, max: 15 },
-  YOUNGSTERS: { min: 16, max: 26 },
-  ADULTS: { min: 18 },
-  SENIORS: { min: 65 },
+  ALL: { apiLabel: '-' },
+  TODDLERS: { label: '0-2', apiLabel: '0-2' },
+  PRESCHOOLERS: { label: '3-5', apiLabel: '3-5' },
+  KIDS: { label: '6-11', apiLabel: '6-11' },
+  TEENAGERS: { label: '12-15', apiLabel: '12-15' },
+  YOUNGSTERS: { label: '16-26', apiLabel: '16-26' },
+  ADULTS: { label: '18+', apiLabel: '18-' },
+  SENIORS: { label: '65+', apiLabel: '65-' },
   CUSTOM: {},
 } as const;
 
@@ -34,58 +35,39 @@ const AgeRangeStep = ({
 }) => {
   const { t } = useTranslation();
 
-  const getAgeRangeLabel = (key: string): string => {
-    if (
-      typeof AgeRanges[key].min === 'number' &&
-      typeof AgeRanges[key].max === 'number'
-    ) {
-      return `${AgeRanges[key].min}-${AgeRanges[key].max}`;
-    }
-    if (typeof AgeRanges[key].min === 'number') {
-      return `${AgeRanges[key].min}+`;
-    }
-    return '';
-  };
-
-  const ageRangeLabelToApiFormat = (label: string) => {
-    if (!label) return '-';
-    return label.replace('+', '-');
-  };
-
   const isCustomAgeRange = (typicalAgeRange: string): boolean => {
-    const [min, max] = typicalAgeRange.split('-');
-    return Object.keys(AgeRanges).some(
-      (key) => !(AgeRanges[key].min === min && AgeRanges[key].max === max),
+    return !Object.keys(AgeRanges).some(
+      (key) =>
+        AgeRanges[key].apiLabel && AgeRanges[key].apiLabel === typicalAgeRange,
     );
   };
 
-  const isSelectedAgeRange = (key: string, ageRangeLabel: string): boolean => {
+  const isSelectedAgeRange = (key: string, apiLabel: string): boolean => {
     if (!field.value?.typicalAgeRange) {
       return;
     }
 
-    if (key === 'CUSTOM' && isCustomAgeRange(field.value.typicalAgeRange)) {
-      return true;
+    if (key === 'CUSTOM') {
+      return isCustomAgeRange(field.value.typicalAgeRange);
     }
-    return (
-      field.value.typicalAgeRange === ageRangeLabelToApiFormat(ageRangeLabel)
-    );
+
+    return field.value.typicalAgeRange === apiLabel;
   };
 
   return (
-    //TODO: Handle custom ranges
     <Controller
       name="nameAndAge"
       control={control}
       render={({ field }) => {
+        const [min, max] = (field.value?.typicalAgeRange ?? '').split('-');
+
         return (
           <Stack spacing={2}>
             <Text fontWeight="bold">{t(`create.step4.age.title`)}</Text>
             <Inline spacing={3} flexWrap="wrap" maxWidth="40rem">
               {Object.keys(AgeRanges).map((key) => {
-                const [min, max] = field.value.typicalAgeRange.split('-');
-                const ageRangeLabel = getAgeRangeLabel(key);
-                const isSelected = isSelectedAgeRange(key, ageRangeLabel);
+                const apiLabel = AgeRanges[key].apiLabel;
+                const isSelected = isSelectedAgeRange(key, apiLabel);
                 return (
                   <Inline key={key}>
                     <Button
@@ -100,15 +82,11 @@ const AgeRangeStep = ({
                       onClick={() => {
                         field.onChange({
                           ...field.value,
-                          typicalAgeRange: ageRangeLabelToApiFormat(
-                            ageRangeLabel,
-                          ),
+                          typicalAgeRange: key === 'CUSTOM' ? '0-99' : apiLabel,
                         });
                         onChange({
                           ...field.value,
-                          typicalAgeRange: ageRangeLabelToApiFormat(
-                            ageRangeLabel,
-                          ),
+                          typicalAgeRange: key === 'CUSTOM' ? '0-99' : apiLabel,
                         });
                       }}
                     >
@@ -119,17 +97,39 @@ const AgeRangeStep = ({
                           font-size: 0.9rem;
                         `}
                       >
-                        &nbsp;{ageRangeLabel}
+                        &nbsp; {AgeRanges[key].label ?? ''}
                       </span>
                     </Button>
                     {isSelected && key === 'CUSTOM' && (
-                      <Inline flex="2">
+                      <Inline width="100%">
                         <Stack>
                           <Text fontWeight="bold">Van</Text>
                           <Input
                             marginRight={3}
                             value={min}
                             placeholder="Van"
+                            onChange={(event) => {
+                              field.onChange({
+                                ...field.value,
+                                typicalAgeRange: `${
+                                  (event.target as HTMLInputElement).value
+                                }-${max ?? ''}`,
+                              });
+                            }}
+                            onBlur={(event: FormEvent<HTMLInputElement>) => {
+                              field.onChange({
+                                ...field.value,
+                                typicalAgeRange: `${
+                                  (event.target as HTMLInputElement).value
+                                }-${max ?? ''}`,
+                              });
+                              onChange({
+                                ...field.value,
+                                typicalAgeRange: `${
+                                  (event.target as HTMLInputElement).value
+                                }-${max ?? ''}`,
+                              });
+                            }}
                           />
                         </Stack>
                         <Stack>
@@ -138,6 +138,28 @@ const AgeRangeStep = ({
                             marginRight={3}
                             value={max}
                             placeholder="Tot"
+                            onChange={(event) => {
+                              field.onChange({
+                                ...field.value,
+                                typicalAgeRange: `${min ?? 0}-${
+                                  (event.target as HTMLInputElement).value
+                                }`,
+                              });
+                            }}
+                            onBlur={(event: FormEvent<HTMLInputElement>) => {
+                              field.onChange({
+                                ...field.value,
+                                typicalAgeRange: `${min ?? 0}-${
+                                  (event.target as HTMLInputElement).value
+                                }`,
+                              });
+                              onChange({
+                                ...field.value,
+                                typicalAgeRange: `${min ?? 0}-${
+                                  (event.target as HTMLInputElement).value
+                                }`,
+                              });
+                            }}
                           />
                         </Stack>
                       </Inline>
