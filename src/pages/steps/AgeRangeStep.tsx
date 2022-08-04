c8c7f56114b37d05f0060a322da4e10b718da163
fulldev1023 <simbadevlop@gmail.com>
2022-08-04 10:45:33 +0200

    III-4474 - Getting started with custom age range

--- src/pages/steps/AgeRangeStep.tsx
+++ src/pages/steps/AgeRangeStep.tsx
@@ -4,6 +4,7 @@ import { css } from 'styled-components';
 
 import { Button, ButtonVariants } from '@/ui/Button';
 import { Inline } from '@/ui/Inline';
+import { Input } from '@/ui/Input';
 import { Stack } from '@/ui/Stack';
 import { Text } from '@/ui/Text';
 import { getValueFromTheme } from '@/ui/theme';
@@ -51,10 +52,23 @@ const AgeRangeStep = ({
     return label.replace('+', '-');
   };
 
+  const isCustomAgeRange = (typicalAgeRange: string): boolean => {
+    const [min, max] = typicalAgeRange.split('-');
+    return Object.keys(AgeRanges).some(
+      (key) => !(AgeRanges[key].min === min && AgeRanges[key].max === max),
+    );
+  };
+
   const isSelectedAgeRange = (key: string, ageRangeLabel: string): boolean => {
-    if (key === 'CUSTOM') return false;
+    if (!field.value?.typicalAgeRange) {
+      return;
+    }
+
+    if (key === 'CUSTOM' && isCustomAgeRange(field.value.typicalAgeRange)) {
+      return true;
+    }
     return (
-      field.value?.typicalAgeRange === ageRangeLabelToApiFormat(ageRangeLabel)
+      field.value.typicalAgeRange === ageRangeLabelToApiFormat(ageRangeLabel)
     );
   };
 
@@ -69,43 +83,66 @@ const AgeRangeStep = ({
             <Text fontWeight="bold">{t(`create.step4.age.title`)}</Text>
             <Inline spacing={3} flexWrap="wrap" maxWidth="40rem">
               {Object.keys(AgeRanges).map((key) => {
+                const [min, max] = field.value.typicalAgeRange.split('-');
                 const ageRangeLabel = getAgeRangeLabel(key);
+                const isSelected = isSelectedAgeRange(key, ageRangeLabel);
                 return (
-                  <Button
-                    width="auto"
-                    marginBottom={3}
-                    display="inline-flex"
-                    key={key}
-                    variant={
-                      isSelectedAgeRange(key, ageRangeLabel)
-                        ? ButtonVariants.SUCCESS
-                        : ButtonVariants.SECONDARY
-                    }
-                    onClick={() => {
-                      field.onChange({
-                        ...field.value,
-                        typicalAgeRange: ageRangeLabelToApiFormat(
-                          ageRangeLabel,
-                        ),
-                      });
-                      onChange({
-                        ...field.value,
-                        typicalAgeRange: ageRangeLabelToApiFormat(
-                          ageRangeLabel,
-                        ),
-                      });
-                    }}
-                  >
-                    {t(`create.step4.age.${key.toLowerCase()}`)}
-                    <span
-                      css={css`
-                        color: ${getValue('rangeTextColor')};
-                        font-size: 0.9rem;
-                      `}
+                  <Inline key={key}>
+                    <Button
+                      width="auto"
+                      marginBottom={3}
+                      display="inline-flex"
+                      variant={
+                        isSelected
+                          ? ButtonVariants.SUCCESS
+                          : ButtonVariants.SECONDARY
+                      }
+                      onClick={() => {
+                        field.onChange({
+                          ...field.value,
+                          typicalAgeRange: ageRangeLabelToApiFormat(
+                            ageRangeLabel,
+                          ),
+                        });
+                        onChange({
+                          ...field.value,
+                          typicalAgeRange: ageRangeLabelToApiFormat(
+                            ageRangeLabel,
+                          ),
+                        });
+                      }}
                     >
-                      &nbsp;{ageRangeLabel}
-                    </span>
-                  </Button>
+                      {t(`create.step4.age.${key.toLowerCase()}`)}
+                      <span
+                        css={css`
+                          color: ${getValue('rangeTextColor')};
+                          font-size: 0.9rem;
+                        `}
+                      >
+                        &nbsp;{ageRangeLabel}
+                      </span>
+                    </Button>
+                    {isSelected && key === 'CUSTOM' && (
+                      <Inline flex="2">
+                        <Stack>
+                          <Text fontWeight="bold">Van</Text>
+                          <Input
+                            marginRight={3}
+                            value={min}
+                            placeholder="Van"
+                          />
+                        </Stack>
+                        <Stack>
+                          <Text fontWeight="bold">Tot</Text>
+                          <Input
+                            marginRight={3}
+                            value={max}
+                            placeholder="Tot"
+                          />
+                        </Stack>
+                      </Inline>
+                    )}
+                  </Inline>
                 );
               })}
             </Inline>
