41a6b448009de0675be5d44f39f3e0e593bd14a9
fulldev1023 <simbadevlop@gmail.com>
2022-08-10 11:30:38 +0200

    III-4473 - Add MunicipalityAndPlaceStep with CityPicker
    Co-Authored-By: Simon de Bruijn <66943525+simon-debruijn@users.noreply.github.com>

---
+++ src/pages/steps/MunicipalityAndPlaceStep.tsx
@@ -0,0 +1,90 @@
+import { Controller } from 'react-hook-form';
+import { useTranslation } from 'react-i18next';
+
+import { Button, ButtonVariants } from '@/ui/Button';
+import { Icon, Icons } from '@/ui/Icon';
+import { Inline } from '@/ui/Inline';
+import { Stack } from '@/ui/Stack';
+import { Text } from '@/ui/Text';
+import { getValueFromTheme } from '@/ui/theme';
+
+import { CityPicker } from '../CityPicker';
+import { FormDataUnion, StepsConfiguration } from './Steps';
+
+const MunicipalityAndPlaceStep = <TFormData extends FormDataUnion>({
+  formState: { errors },
+  getValues,
+  reset,
+  control,
+  field,
+  loading,
+  onChange,
+  terms,
+  ...props
+}) => {
+  const { t, i18n } = useTranslation();
+
+  const getValue = getValueFromTheme('createPage');
+
+  return (
+    <Controller<TFormData>
+      control={control}
+      name={field}
+      render={({ field }) => {
+        const selectedMunicipality = field?.value?.municipality as any;
+        return (
+          <Stack>
+            {!selectedMunicipality && (
+              <CityPicker
+                {...field}
+                value={field.value?.municipality}
+                onChange={(value) =>
+                  field.onChange({ ...field.value, municipality: value })
+                }
+              />
+            )}
+            {selectedMunicipality && (
+              <Inline alignItems="center" spacing={3}>
+                <Icon
+                  name={Icons.CHECK_CIRCLE}
+                  color={getValue('check.circleFillColor')}
+                />
+                <Text>{selectedMunicipality.name}</Text>
+                <Button
+                  variant={ButtonVariants.LINK}
+                  onClick={() =>
+                    reset(
+                      {
+                        ...getValues(),
+                        municipalityAndPlace: {
+                          ...field.value,
+                          municipality: undefined,
+                        },
+                      },
+                      {
+                        keepDirty: true,
+                      },
+                    )
+                  }
+                >
+                  {t('create.municipality_and_place.municipality.change')}
+                </Button>
+              </Inline>
+            )}
+          </Stack>
+        );
+      }}
+    />
+  );
+};
+
+const municipalityAndPlaceStepConfiguration: StepsConfiguration<FormDataUnion> = {
+  Component: MunicipalityAndPlaceStep,
+  field: 'municipalityAndPlace',
+  shouldShowStep: () => true,
+  title: (t) => 'Waar vindt dit evenement of deze activiteit plaats?',
+};
+
+MunicipalityAndPlaceStep.defaultProps = {};
+
+export { municipalityAndPlaceStepConfiguration };
