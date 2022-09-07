645ed25fa934530cb0d6b14546f0e84194be8be0
fulldev1023 <simbadevlop@gmail.com>
2022-09-07 14:48:50 +0200

    Add online toggle to switch between layouts

--- src/pages/steps/MunicipalityAndPlaceStep.tsx
+++ src/pages/steps/MunicipalityAndPlaceStep.tsx
@@ -5,13 +5,19 @@ import { useTranslation } from 'react-i18next';
 import { EventTypes } from '@/constants/EventTypes';
 import { City } from '@/hooks/api/cities';
 import { useChangeLocationMutation } from '@/hooks/api/events';
+import { FormData as EventFormData } from '@/pages/create/EventForm';
 import { Place } from '@/types/Place';
 import { Values } from '@/types/Values';
+import { Alert } from '@/ui/Alert';
 import { Button, ButtonVariants } from '@/ui/Button';
+import { FormElement } from '@/ui/FormElement';
 import { Icon, Icons } from '@/ui/Icon';
 import { Inline } from '@/ui/Inline';
+import { Input } from '@/ui/Input';
+import { RadioButtonTypes } from '@/ui/RadioButton';
+import { RadioButtonWithLabel } from '@/ui/RadioButtonWithLabel';
 import { getStackProps, Stack, StackProps } from '@/ui/Stack';
-import { Text } from '@/ui/Text';
+import { Text, TextVariants } from '@/ui/Text';
 import { getValueFromTheme } from '@/ui/theme';
 import { parseOfferId } from '@/utils/parseOfferId';
 
@@ -27,13 +33,13 @@ const useEditMunicipalityAndPlace = <TFormData extends FormDataUnion>({
 }) => {
   const changeLocationMutation = useChangeLocationMutation();
 
-  return async ({ municipalityAndPlace }: TFormData) => {
-    if (!municipalityAndPlace.municipality) return;
-    if (!municipalityAndPlace.place) return;
+  return async ({ location }: TFormData) => {
+    if (!location.municipality) return;
+    if (!location.place) return;
 
     changeLocationMutation.mutate({
       id: eventId,
-      locationId: parseOfferId(municipalityAndPlace.place['@id']),
+      locationId: parseOfferId(location.place['@id']),
     });
   };
 };
@@ -69,12 +75,72 @@ const MunicipalityAndPlaceStep = <TFormData extends FormDataUnion>({
         control={control}
         name={name}
         render={({ field }) => {
-          const selectedMunicipality = field?.value?.municipality as City;
+          const {
+            isOnline,
+            onlineUrl,
+            municipality,
+          } = field?.value as EventFormData['location'];
 
-          return (
-            <Stack maxWidth="25rem">
-              {!selectedMunicipality && (
+          const OnlineToggle = (
+            <RadioButtonWithLabel
+              id="online-toggle"
+              type={RadioButtonTypes.SWITCH}
+              label={'Online'}
+              checked={isOnline}
+              onChange={(e) => {
+                const updatedValue = {
+                  ...field.value,
+                  isOnline: e.target.checked,
+                };
+                field.onChange(updatedValue);
+                onChange(updatedValue);
+              }}
+            />
+          );
+
+          if (isOnline) {
+            return (
+              <Stack>
+                {OnlineToggle}
+                <FormElement
+                  Component={
+                    <Input
+                      maxWidth="28rem"
+                      value={onlineUrl}
+                      onChange={(e) => {
+                        field.onChange({
+                          ...field?.value,
+                          onlineUrl: e.target.value,
+                        });
+                      }}
+                    />
+                  }
+                  id="online-url"
+                  label={'Url voor online deelname'}
+                  error={
+                    formState.errors.location &&
+                    t('organizer.add_modal.validation_messages.name')
+                  }
+                  info={
+                    <Alert maxWidth="53rem">
+                      Voeg een link toe zodat mensen weten hoe ze aan je
+                      evenement kunnen deelnemen. Wil je de deelnamelink niet
+                      publiek delen? Dan kan je deze stap overslaan en
+                      deelname-instructies toevoegen aan je beschrijving in stap
+                      5.
+                    </Alert>
+                  }
+                />
+              </Stack>
+            );
+          }
+
+          if (!municipality) {
+            return (
+              <Stack>
+                {OnlineToggle}
                 <CityPicker
+                  maxWidth="28rem"
                   {...field}
                   value={field.value?.municipality}
                   onChange={(value) =>
@@ -85,57 +151,60 @@ const MunicipalityAndPlaceStep = <TFormData extends FormDataUnion>({
                     })
                   }
                 />
-              )}
-              {selectedMunicipality && (
-                <Stack spacing={4}>
-                  <Inline alignItems="center" spacing={3}>
-                    <Icon
-                      name={Icons.CHECK_CIRCLE}
-                      color={getValue('check.circleFillColor')}
-                    />
-                    <Text>{selectedMunicipality.name}</Text>
-                    <Button
-                      variant={ButtonVariants.LINK}
-                      onClick={() =>
-                        field.onChange({
-                          ...field.value,
-                          municipality: undefined,
-                        })
-                      }
-                    >
-                      {t('create.municipality_and_place.municipality.change')}
-                    </Button>
-                  </Inline>
-                  <PlaceStep
-                    name={'municipalityAndPlace.place' as Path<TFormData>}
-                    zip={selectedMunicipality.zip}
-                    chooseLabel={chooseLabel}
-                    placeholderLabel={placeholderLabel}
-                    parentFieldValue={field.value}
-                    parentFieldOnChange={(val: Place | undefined) => {
-                      field.onChange({ ...field.value, place: val });
-                    }}
-                    parentOnChange={(val: Place | undefined) => {
-                      onChange({
-                        ...field.value,
-                        place: val,
-                      });
-                    }}
-                    {...{
-                      formState,
-                      getValues,
-                      reset,
-                      control,
-                      loading,
-                      terms,
-                      field,
-                      onChange,
-                      watch,
-                    }}
-                    {...props}
+              </Stack>
+            );
+          }
+
+          return (
+            <Stack>
+              <Stack spacing={4}>
+                <Inline alignItems="center" spacing={3}>
+                  <Icon
+                    name={Icons.CHECK_CIRCLE}
+                    color={getValue('check.circleFillColor')}
                   />
-                </Stack>
-              )}
+                  <Text>{municipality.name}</Text>
+                  <Button
+                    variant={ButtonVariants.LINK}
+                    onClick={() =>
+                      field.onChange({
+                        ...field.value,
+                        municipality: undefined,
+                      })
+                    }
+                  >
+                    {t('create.municipality_and_place.municipality.change')}
+                  </Button>
+                </Inline>
+                <PlaceStep
+                  name={'location.place' as Path<TFormData>}
+                  zip={municipality.zip}
+                  chooseLabel={chooseLabel}
+                  placeholderLabel={placeholderLabel}
+                  parentFieldValue={field.value}
+                  parentFieldOnChange={(val: Place | undefined) => {
+                    field.onChange({ ...field.value, place: val });
+                  }}
+                  parentOnChange={(val: Place | undefined) => {
+                    onChange({
+                      ...field.value,
+                      place: val,
+                    });
+                  }}
+                  {...{
+                    formState,
+                    getValues,
+                    reset,
+                    control,
+                    loading,
+                    terms,
+                    field,
+                    onChange,
+                    watch,
+                  }}
+                  {...props}
+                />
+              </Stack>
             </Stack>
           );
         }}
@@ -146,13 +215,16 @@ const MunicipalityAndPlaceStep = <TFormData extends FormDataUnion>({
 
 const municipalityAndPlaceStepConfiguration: StepsConfiguration<FormDataUnion> = {
   Component: MunicipalityAndPlaceStep,
-  name: 'municipalityAndPlace',
+  name: 'location',
   shouldShowStep: ({ watch }) => !!watch('typeAndTheme')?.type?.id,
   title: (t) => t('create.municipality_and_place.title'),
   stepProps: {
     chooseLabel: (t) => t('create.municipality_and_place.place.choose_label'),
     placeholderLabel: (t) =>
       t('create.municipality_and_place.place.placeholder'),
+  },
+  defaultValue: {
+    isOnline: false,
   },
 };
 
