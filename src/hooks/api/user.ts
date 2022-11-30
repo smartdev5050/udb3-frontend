60068fd536f8ab25a8785106f0b448d8fb361024
fulldev1023 <simbadevlop@gmail.com>
2022-11-30 16:41:31 +0100

    Revert api user

--- src/hooks/api/user.ts
+++ src/hooks/api/user.ts
@@ -1,41 +1,23 @@
-import {
-  ErrorObject,
-  FetchError,
-  fetchFromApi,
-  isErrorObject,
-} from '@/utils/fetchFromApi';
+import { User } from '@/types/User';
+import { fetchFromApi, isErrorObject } from '@/utils/fetchFromApi';
 
 import {
   ServerSideQueryOptions,
   useAuthenticatedQuery,
 } from './authenticated-query';
 
-const tryToFetch = async (promise: () => Promise<ErrorObject | Response>) => {
-  let res: ErrorObject | Response;
-
-  try {
-    res = await promise();
-    if (isErrorObject(res)) {
-      // eslint-disable-next-line no-console
-      return console.error(res);
-    }
-    return await res.json();
-  } catch (error) {
-    const status = error?.message === 'Failed to fetch' ? 401 : res?.status;
-    throw new FetchError(status, error?.message ?? 'Unknown error');
-  }
-};
-
 const getUser = async ({ headers }) => {
-  return tryToFetch(
-    async () =>
-      await fetchFromApi({
-        path: '/user',
-        options: {
-          headers,
-        },
-      }),
-  );
+  const res = await fetchFromApi({
+    path: '/user',
+    options: {
+      headers,
+    },
+  });
+  if (isErrorObject(res)) {
+    // eslint-disable-next-line no-console
+    return console.error(res);
+  }
+  return await res.json();
 };
 
 const useGetUserQuery = (
@@ -52,15 +34,17 @@ const useGetUserQuery = (
 };
 
 const getPermissions = async ({ headers }) => {
-  return tryToFetch(
-    async () =>
-      await fetchFromApi({
-        path: '/user/permissions/',
-        options: {
-          headers,
-        },
-      }),
-  );
+  const res = await fetchFromApi({
+    path: '/user/permissions/',
+    options: {
+      headers,
+    },
+  });
+  if (isErrorObject(res)) {
+    // eslint-disable-next-line no-console
+    return console.error(res);
+  }
+  return await res.json();
 };
 
 const useGetPermissionsQuery = (configuration = {}) =>
@@ -71,15 +55,17 @@ const useGetPermissionsQuery = (configuration = {}) =>
   });
 
 const getRoles = async ({ headers }) => {
-  return tryToFetch(
-    async () =>
-      await fetchFromApi({
-        path: '/user/roles/',
-        options: {
-          headers,
-        },
-      }),
-  );
+  const res = await fetchFromApi({
+    path: '/user/roles/',
+    options: {
+      headers,
+    },
+  });
+  if (isErrorObject(res)) {
+    // eslint-disable-next-line no-console
+    return console.error(res);
+  }
+  return await res.json();
 };
 
 const useGetRolesQuery = (configuration = {}) =>
