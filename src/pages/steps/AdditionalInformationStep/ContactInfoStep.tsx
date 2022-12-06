b54d88ac7432996a96a0aba1b9dcc8bb64dfd84a
fulldev1023 <simbadevlop@gmail.com>
2022-12-06 16:25:25 +0100

    Fix infinite loop issues

--- src/pages/steps/AdditionalInformationStep/ContactInfoStep.tsx
+++ src/pages/steps/AdditionalInformationStep/ContactInfoStep.tsx
@@ -105,7 +105,7 @@ const ContactInfoStep = ({
     });
 
     setContactInfoState(contactInfoArray);
-  }, [contactInfo, setContactInfoState, onChangeCompleted]);
+  }, [contactInfo, handleChangeCompleted]);
 
   const addContactPointMutation = useAddContactPointMutation({
     onSuccess: onSuccessfulChange,
