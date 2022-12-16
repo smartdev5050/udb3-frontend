d21c6e7a3038f9ddbfda1bedbc3d227489601256
fulldev1023 <simbadevlop@gmail.com>
2022-12-16 15:14:12 +0100

    remove import

--- src/pages/steps/ScopeStep.tsx
+++ src/pages/steps/ScopeStep.tsx
@@ -1,5 +1,4 @@
 import { useRouter } from 'next/router';
-import { useEffect } from 'react';
 import { Controller, ControllerRenderProps, Path } from 'react-hook-form';
 import { useTranslation } from 'react-i18next';
 
