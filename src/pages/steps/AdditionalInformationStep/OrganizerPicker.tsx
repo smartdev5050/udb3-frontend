34691973c2d3cc013b47e9cf40578d79e0159058
fulldev1023 <simbadevlop@gmail.com>
2023-01-11 13:38:47 +0100

    III-5200 - Fix issue of overflow typeahead of organizers

--- src/pages/steps/AdditionalInformationStep/OrganizerPicker.tsx
+++ src/pages/steps/AdditionalInformationStep/OrganizerPicker.tsx
@@ -55,7 +55,7 @@ const RecentUsedOrganizers = ({
   }
 
   return (
-    <Stack spacing={4} {...getStackProps(props)} maxWidth="50rem">
+    <Stack spacing={4} {...getStackProps(props)} maxWidth="45rem">
       <Inline>
         <Text fontWeight="bold">
           {t(
@@ -247,50 +247,53 @@ const OrganizerPicker = ({
               </Inline>
             </Stack>
           ) : (
-            <Stack spacing={4} alignItems="flex-start">
+            <Inline>
               <RecentUsedOrganizers
                 organizers={recentUsedOrganizers}
                 onChange={handleSelectRecentOrganizer}
               />
-              <Text fontWeight="bold">
-                {t(
-                  'create.additionalInformation.organizer.or_choose_other_organizer',
-                )}
-              </Text>
-              {!addButtonHasBeenPressed && (
-                <Button
-                  variant={ButtonVariants.SECONDARY}
-                  onClick={() => setAddButtonHasBeenPressed(true)}
-                >
-                  {t('create.additionalInformation.organizer.add_new_button')}
-                </Button>
-              )}
-              {addButtonHasBeenPressed && (
-                <Typeahead<Organizer>
-                  options={organizers}
-                  labelKey={(org) => getOrganizerName(org, i18n.language)}
-                  selected={valueToArray(organizer)}
-                  onInputChange={debounce(setOrganizerSearchInput, 275)}
-                  onChange={(organizers) => {
-                    const organizer = organizers[0];
-
-                    if (isNewEntry(organizer)) {
-                      onAddNewOrganizer(organizer);
-                      queryClient.invalidateQueries('organizers');
-                      return;
-                    }
-
-                    onChange(parseOfferId(organizer['@id']));
-                  }}
-                  minLength={3}
-                  width="20rem"
-                  newSelectionPrefix={t(
-                    'create.additionalInformation.organizer.add_new_label',
+              <Stack>
+                <Text fontWeight="bold" marginBottom={4}>
+                  {t(
+                    'create.additionalInformation.organizer.or_choose_other_organizer',
                   )}
-                  allowNew
-                />
-              )}
-            </Stack>
+                </Text>
+                {!addButtonHasBeenPressed && (
+                  <Button
+                    alignSelf="flex-start"
+                    variant={ButtonVariants.SECONDARY}
+                    onClick={() => setAddButtonHasBeenPressed(true)}
+                  >
+                    {t('create.additionalInformation.organizer.add_new_button')}
+                  </Button>
+                )}
+                {addButtonHasBeenPressed && (
+                  <Typeahead<Organizer>
+                    options={organizers}
+                    labelKey={(org) => getOrganizerName(org, i18n.language)}
+                    selected={valueToArray(organizer)}
+                    onInputChange={debounce(setOrganizerSearchInput, 275)}
+                    onChange={(organizers) => {
+                      const organizer = organizers[0];
+
+                      if (isNewEntry(organizer)) {
+                        onAddNewOrganizer(organizer);
+                        queryClient.invalidateQueries('organizers');
+                        return;
+                      }
+
+                      onChange(parseOfferId(organizer['@id']));
+                    }}
+                    minLength={3}
+                    width="20rem"
+                    newSelectionPrefix={t(
+                      'create.additionalInformation.organizer.add_new_label',
+                    )}
+                    allowNew
+                  />
+                )}
+              </Stack>
+            </Inline>
           )
         }
       />
