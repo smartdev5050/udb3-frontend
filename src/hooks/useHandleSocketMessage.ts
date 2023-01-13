77784062f08bbed4fead9b13d7de9cfdbffd0490
fulldev1023 <simbadevlop@gmail.com>
2023-01-13 14:52:43 +0100

    Convert to ts and explicitly use transports websocket

--- src/hooks/useHandleSocketMessage.ts
+++ src/hooks/useHandleSocketMessage.ts
@@ -1,6 +1,6 @@
 import getConfig from 'next/config';
 import { useEffect } from 'react';
-import socketIOClient from 'socket.io-client';
+import { io } from 'socket.io-client';
 
 const SocketMessageTypes = {
   JOB_STARTED: 'job_started',
@@ -9,15 +9,23 @@ const SocketMessageTypes = {
   JOB_FAILED: 'job_failed',
 };
 
+type Handler = (...args: any[]) => void;
+
 const useHandleSocketMessage = (eventsMap = {}) => {
   const { publicRuntimeConfig } = getConfig();
 
   useEffect(() => {
-    const socket = socketIOClient(publicRuntimeConfig.socketUrl);
-    Object.entries(eventsMap).forEach(([event, handler]) => {
+    const socket = io(publicRuntimeConfig.socketUrl, {
+      transports: ['websocket'],
+    });
+
+    Object.entries<Handler>(eventsMap).forEach(([event, handler]) => {
       socket.on(event, handler);
     });
-    return () => socket.close();
+
+    return () => {
+      socket.close();
+    };
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 };
