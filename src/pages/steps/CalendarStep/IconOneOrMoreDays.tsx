403ca83ebb239706c1d39ec6d87eb8a213f203e5
fulldev1023 <simbadevlop@gmail.com>
2023-01-25 15:16:26 +0100

    Optimize icons via https://jakearchibald.github.io/svgomg/

--- src/pages/steps/CalendarStep/IconOneOrMoreDays.tsx
+++ src/pages/steps/CalendarStep/IconOneOrMoreDays.tsx
@@ -1,35 +1,25 @@
 const IconOneOrMoreDays = () => {
   return (
-    <svg viewBox="0 0 64 64" width="50px" xmlns="http://www.w3.org/2000/svg">
-      <g id="Calendar">
-        <path
-          d="m57 9h-6v3a3 3 0 0 1 -6 0v-3h-10v3a3 3 0 0 1 -6 0v-3h-10v3a3 3 0 0 1 -6 0v-3h-6a4 4 0 0 0 -4 4v8h58v-8a4 4 0 0 0 -4-4z"
-          fill="#f0f0f0"
-        />
-        <rect fill="#f0f0f0" height="5" rx="1" width="6" x="23" y="28" />
-        <rect fill="#f0f0f0" height="5" rx="1" width="6" x="11" y="28" />
-        <rect fill="#f0f0f0" height="5" rx="1" width="6" x="47" y="28" />
-        <rect fill="#f0f0f0" height="5" rx="1" width="6" x="23" y="37" />
-        <rect fill="#f0f0f0" height="5" rx="1" width="6" x="11" y="37" />
-        <rect fill="#f0f0f0" height="5" rx="1" width="6" x="23" y="46" />
-        <rect fill="#f0f0f0" height="5" rx="1" width="6" x="35" y="28" />
-        <rect fill="#f0f0f0" height="5" rx="1" width="6" x="35" y="37" />
-        <rect fill="#f0f0f0" height="5" rx="1" width="6" x="11" y="46" />
-        <circle cx="49" cy="49" fill="#f0f0f0" r="12" />
-        <g fill="#999999">
-          <path d="m57 8h-5v-2a4 4 0 0 0 -8 0v2h-8v-2a4 4 0 0 0 -8 0v2h-8v-2a4 4 0 0 0 -8 0v2h-5a5 5 0 0 0 -5 5v40a5 5 0 0 0 5 5h28a1 1 0 0 0 0-2h-28a3.009 3.009 0 0 1 -3-3v-31h56v17a1 1 0 0 0 2 0v-26a5 5 0 0 0 -5-5zm-11-2a2 2 0 0 1 4 0v6a2 2 0 0 1 -4 0zm-16 0a2 2 0 0 1 4 0v6a2 2 0 0 1 -4 0zm-16 0a2 2 0 0 1 4 0v6a2 2 0 0 1 -4 0zm46 14h-56v-7a3.009 3.009 0 0 1 3-3h5v2a4 4 0 0 0 8 0v-2h8v2a4 4 0 0 0 8 0v-2h8v2a4 4 0 0 0 8 0v-2h5a3.009 3.009 0 0 1 3 3z" />
-          <path d="m30 29a2 2 0 0 0 -2-2h-4a2 2 0 0 0 -2 2v3a2 2 0 0 0 2 2h4a2 2 0 0 0 2-2zm-6 3v-3h4v3z" />
-          <path d="m18 29a2 2 0 0 0 -2-2h-4a2 2 0 0 0 -2 2v3a2 2 0 0 0 2 2h4a2 2 0 0 0 2-2zm-6 3v-3h4v3z" />
-          <path d="m52 34a2 2 0 0 0 2-2v-3a2 2 0 0 0 -2-2h-4a2 2 0 0 0 -2 2v3a2 2 0 0 0 2 2zm-4-5h4v3h-4z" />
-          <path d="m30 38a2 2 0 0 0 -2-2h-4a2 2 0 0 0 -2 2v3a2 2 0 0 0 2 2h4a2 2 0 0 0 2-2zm-6 3v-3h4v3z" />
-          <path d="m18 38a2 2 0 0 0 -2-2h-4a2 2 0 0 0 -2 2v3a2 2 0 0 0 2 2h4a2 2 0 0 0 2-2zm-6 3v-3h4v3z" />
-          <path d="m28 45h-4a2 2 0 0 0 -2 2v3a2 2 0 0 0 2 2h4a2 2 0 0 0 2-2v-3a2 2 0 0 0 -2-2zm-4 5v-3h4v3z" />
-          <path d="m36 34h4a2 2 0 0 0 2-2v-3a2 2 0 0 0 -2-2h-4a2 2 0 0 0 -2 2v3a2 2 0 0 0 2 2zm0-5h4v3h-4z" />
-          <path d="m34 41a2 2 0 0 0 2 2 1 1 0 0 0 0-2v-3h4a1 1 0 0 0 0-2h-4a2 2 0 0 0 -2 2z" />
-          <path d="m16 45h-4a2 2 0 0 0 -2 2v3a2 2 0 0 0 2 2h4a2 2 0 0 0 2-2v-3a2 2 0 0 0 -2-2zm-4 5v-3h4v3z" />
-          <path d="m49 36a13 13 0 1 0 13 13 13.015 13.015 0 0 0 -13-13zm0 24a11 11 0 1 1 11-11 11.013 11.013 0 0 1 -11 11z" />
-          <path d="m54.778 44.808-7.778 7.778-3.535-3.536a1 1 0 0 0 -1.414 1.414l4.242 4.243a1 1 0 0 0 1.414 0l8.485-8.485a1 1 0 0 0 -1.414-1.414z" />
-        </g>
+    <svg xmlns="http://www.w3.org/2000/svg" width="50" viewBox="0 0 64 64">
+      <path
+        fill="#f0f0f0"
+        d="M57 9h-6v3a3 3 0 0 1-6 0V9H35v3a3 3 0 0 1-6 0V9H19v3a3 3 0 0 1-6 0V9H7a4 4 0 0 0-4 4v8h58v-8a4 4 0 0 0-4-4z"
+      />
+      <rect width="6" height="5" x="23" y="28" fill="#f0f0f0" rx="1" />
+      <rect width="6" height="5" x="11" y="28" fill="#f0f0f0" rx="1" />
+      <rect width="6" height="5" x="47" y="28" fill="#f0f0f0" rx="1" />
+      <rect width="6" height="5" x="23" y="37" fill="#f0f0f0" rx="1" />
+      <rect width="6" height="5" x="11" y="37" fill="#f0f0f0" rx="1" />
+      <rect width="6" height="5" x="23" y="46" fill="#f0f0f0" rx="1" />
+      <rect width="6" height="5" x="35" y="28" fill="#f0f0f0" rx="1" />
+      <rect width="6" height="5" x="35" y="37" fill="#f0f0f0" rx="1" />
+      <rect width="6" height="5" x="11" y="46" fill="#f0f0f0" rx="1" />
+      <circle cx="49" cy="49" r="12" fill="#f0f0f0" />
+      <g fill="#999">
+        <path d="M57 8h-5V6a4 4 0 0 0-8 0v2h-8V6a4 4 0 0 0-8 0v2h-8V6a4 4 0 0 0-8 0v2H7a5 5 0 0 0-5 5v40a5 5 0 0 0 5 5h28a1 1 0 0 0 0-2H7a3.009 3.009 0 0 1-3-3V22h56v17a1 1 0 0 0 2 0V13a5 5 0 0 0-5-5zM46 6a2 2 0 0 1 4 0v6a2 2 0 0 1-4 0zM30 6a2 2 0 0 1 4 0v6a2 2 0 0 1-4 0zM14 6a2 2 0 0 1 4 0v6a2 2 0 0 1-4 0zm46 14H4v-7a3.009 3.009 0 0 1 3-3h5v2a4 4 0 0 0 8 0v-2h8v2a4 4 0 0 0 8 0v-2h8v2a4 4 0 0 0 8 0v-2h5a3.009 3.009 0 0 1 3 3z" />
+        <path d="M30 29a2 2 0 0 0-2-2h-4a2 2 0 0 0-2 2v3a2 2 0 0 0 2 2h4a2 2 0 0 0 2-2zm-6 3v-3h4v3zM18 29a2 2 0 0 0-2-2h-4a2 2 0 0 0-2 2v3a2 2 0 0 0 2 2h4a2 2 0 0 0 2-2zm-6 3v-3h4v3zM52 34a2 2 0 0 0 2-2v-3a2 2 0 0 0-2-2h-4a2 2 0 0 0-2 2v3a2 2 0 0 0 2 2zm-4-5h4v3h-4zM30 38a2 2 0 0 0-2-2h-4a2 2 0 0 0-2 2v3a2 2 0 0 0 2 2h4a2 2 0 0 0 2-2zm-6 3v-3h4v3zM18 38a2 2 0 0 0-2-2h-4a2 2 0 0 0-2 2v3a2 2 0 0 0 2 2h4a2 2 0 0 0 2-2zm-6 3v-3h4v3zM28 45h-4a2 2 0 0 0-2 2v3a2 2 0 0 0 2 2h4a2 2 0 0 0 2-2v-3a2 2 0 0 0-2-2zm-4 5v-3h4v3zM36 34h4a2 2 0 0 0 2-2v-3a2 2 0 0 0-2-2h-4a2 2 0 0 0-2 2v3a2 2 0 0 0 2 2zm0-5h4v3h-4zM34 41a2 2 0 0 0 2 2 1 1 0 0 0 0-2v-3h4a1 1 0 0 0 0-2h-4a2 2 0 0 0-2 2zM16 45h-4a2 2 0 0 0-2 2v3a2 2 0 0 0 2 2h4a2 2 0 0 0 2-2v-3a2 2 0 0 0-2-2zm-4 5v-3h4v3z" />
+        <path d="M49 36a13 13 0 1 0 13 13 13.015 13.015 0 0 0-13-13zm0 24a11 11 0 1 1 11-11 11.013 11.013 0 0 1-11 11z" />
+        <path d="M54.778 44.808 47 52.586l-3.535-3.536a1 1 0 0 0-1.414 1.414l4.242 4.243a1 1 0 0 0 1.414 0l8.485-8.485a1 1 0 0 0-1.414-1.414z" />
       </g>
     </svg>
   );
