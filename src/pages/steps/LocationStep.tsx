5d57785ecaf5ee42119001dd913da89ccdec6945
fulldev1023 <simbadevlop@gmail.com>
2023-01-27 12:43:25 +0100

    Remove stray console.log

--- src/pages/steps/LocationStep.tsx
+++ src/pages/steps/LocationStep.tsx
@@ -147,8 +147,6 @@ const LocationStep = ({
     }
   }, [locationStreetAndNumber, locationOnlineUrl]);
 
-  console.log('errors', formState.errors);
-
   return (
     <Stack {...getStackProps(props)}>
       <Controller
