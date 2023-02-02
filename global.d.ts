a177821d3cbb50e51629b40c253112be249deff6
fulldev1023 <simbadevlop@gmail.com>
2023-02-02 16:33:16 +0100

    Update Yup types with new method

--- global.d.ts
+++ global.d.ts
@@ -1,10 +1,20 @@
 import type { CSSProp } from 'styled-components';
 
 import type { Theme } from '@/ui/theme';
+import { AnySchema } from 'yup/lib/schema';
+import Lazy from 'yup/lib/Lazy';
+import { AnyObject, Maybe } from 'yup/lib/types';
+import { TypeOf } from 'yup/lib/util/types';
 
 declare module 'react' {
   interface Attributes {
     css?: CSSProp<Theme>;
+  }
+}
+
+declare module 'yup' {
+  interface ArraySchema<T> {
+    uniqueName(): this;
   }
 }
 
