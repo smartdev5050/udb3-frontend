139f81952b0730f7ab4f8bfec3ae97486889802d
fulldev1023 <simbadevlop@gmail.com>
2023-02-07 11:10:27 +0100

    III-5239 - Make font bold in keyboard selected date

--- src/styles/GlobalStyle.js
+++ src/styles/GlobalStyle.js
@@ -159,6 +159,7 @@ const GlobalStyle = createGlobalStyle`
   .react-datepicker__day--keyboard-selected {
     background-color: ${globalColors.grey1};
     color: #000;
+    font-weight: bold;
   }
 `;
 
