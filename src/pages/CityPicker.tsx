470d6e97f7e18d05052231a4bc1b3d9ea6105688
fulldev1023 <simbadevlop@gmail.com>
2023-02-22 15:19:55 +0100

    III-5301 - Scroll to CityPicker when focussed in field (only in create mode)

--- src/pages/CityPicker.tsx
+++ src/pages/CityPicker.tsx
@@ -1,5 +1,5 @@
 import debounce from 'lodash/debounce';
-import { forwardRef, useState } from 'react';
+import { forwardRef, useRef, useState } from 'react';
 import { useTranslation } from 'react-i18next';
 
 import { City, useGetCitiesByQuery } from '@/hooks/api/cities';
@@ -21,8 +21,12 @@ type Props = Omit<StackProps, 'onChange'> & {
 };
 
 const CityPicker = forwardRef<HTMLInputElement, Props>(
-  ({ country, name, value, onChange, onBlur, error, ...props }, ref) => {
+  (
+    { offerId, country, name, value, onChange, onBlur, error, ...props },
+    ref,
+  ) => {
     const { t, i18n } = useTranslation();
+    const cityPickerField = useRef(null);
 
     const [citySearchInput, setCitySearchInput] = useState('');
 
@@ -38,8 +42,21 @@ const CityPicker = forwardRef<HTMLInputElement, Props>(
         ? t('city_picker.no_cities_netherlands')
         : t('city_picker.no_cities');
 
+    const handleScroll = () => {
+      if (offerId) return;
+
+      if (!cityPickerField.current) {
+        return;
+      }
+
+      cityPickerField.current.scrollIntoView({
+        behavior: 'smooth',
+        block: 'center',
+      });
+    };
+
     return (
-      <Stack {...getStackProps(props)}>
+      <Stack ref={cityPickerField} {...getStackProps(props)}>
         <FormElement
           id="city_picker"
           label={t(`city_picker.label_${country?.toLowerCase()}`)}
@@ -55,6 +72,7 @@ const CityPicker = forwardRef<HTMLInputElement, Props>(
               onInputChange={debounce(setCitySearchInput, 275)}
               onChange={([value]: [City]) => onChange(value)}
               onBlur={onBlur}
+              onFocus={handleScroll}
               minLength={3}
               emptyLabel={emptyLabel}
             />
