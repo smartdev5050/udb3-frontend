f696032295837b9f00aed0b44767ae851876ef44
fulldev1023 <simbadevlop@gmail.com>
2023-02-22 16:04:08 +0100

    Add middleware to redirect to conversion page

---
+++ src/middleware.api.ts
@@ -0,0 +1,18 @@
+// middleware.ts
+import type { NextRequest } from 'next/server';
+import { NextResponse } from 'next/server';
+
+export const middleware = (request: NextRequest) => {
+  const hasSeenConversionPage =
+    request.cookies.get('has_seen_conversion_page') === 'true';
+
+  if (!hasSeenConversionPage) {
+    const url = new URL('/beta-version', request.url);
+    return NextResponse.redirect(url);
+  }
+};
+
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: '/event',
+};
