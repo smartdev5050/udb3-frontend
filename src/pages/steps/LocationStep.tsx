8873dd33f18ddcec6a72b9290aefca6c19a0ded3
fulldev1023 <simbadevlop@gmail.com>
2023-02-23 15:23:30 +0100

    Move handler onChange

--- src/pages/steps/LocationStep.tsx
+++ src/pages/steps/LocationStep.tsx
@@ -175,6 +175,7 @@ const LocationStep = ({
   onChange,
   chooseLabel,
   placeholderLabel,
+  setValue,
   watch,
   ...props
 }: PlaceStepProps) => {
@@ -404,6 +405,23 @@ const LocationStep = ({
             );
           }
 
+          const handleChangeStreetAndNumber = (
+            e: ChangeEvent<HTMLInputElement>,
+          ) => {
+            const shouldShowNextStepInCreate =
+              !offerId &&
+              !formState.touchedFields.location?.streetAndNumber &&
+              e.target.value.trim().length >= 2;
+
+            if (shouldShowNextStepInCreate) {
+              setValue('location.streetAndNumber', undefined, {
+                shouldTouch: true,
+              });
+            }
+
+            setStreetAndNumber(e.target.value);
+          };
+
           return (
             <Stack>
               <Stack spacing={4}>
@@ -460,9 +478,7 @@ const LocationStep = ({
                           field.onChange(updatedValue);
                           onChange(updatedValue);
                         }}
-                        onChange={(e) => {
-                          setStreetAndNumber(e.target.value);
-                        }}
+                        onChange={handleChangeStreetAndNumber}
                       />
                     }
                     id="location-streetAndNumber"
