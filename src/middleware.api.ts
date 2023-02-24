fb9e3c6e1f36aea97bf437891f2bf4d29d8275f6
fulldev1023 <simbadevlop@gmail.com>
2023-02-24 15:30:12 +0100

    Rename variable

--- src/middleware.api.ts
+++ src/middleware.api.ts
@@ -2,13 +2,13 @@ import type { NextRequest } from 'next/server';
 import { NextResponse } from 'next/server';
 
 export const middleware = (request: NextRequest) => {
-  const shouldNotShowBetaVersion =
+  const shouldHideBetaVersion =
     process.env.NEXT_PUBLIC_SHOULD_SHOW_BETA_VERSION !== 'true';
 
   const hasSeenConversionPage =
     request.cookies.get('has_seen_beta_conversion_page') === 'true';
 
-  if (shouldNotShowBetaVersion || hasSeenConversionPage) return;
+  if (shouldHideBetaVersion || hasSeenConversionPage) return;
 
   const url = new URL('/beta-version', request.url);
   return NextResponse.redirect(url);
