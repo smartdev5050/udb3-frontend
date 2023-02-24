7d5a4099358b3ee04902444960054a55038837c5
fulldev1023 <simbadevlop@gmail.com>
2023-02-24 13:51:45 +0100

    Check env variable if it should redirect to beta-version page

--- src/middleware.api.ts
+++ src/middleware.api.ts
@@ -2,13 +2,16 @@ import type { NextRequest } from 'next/server';
 import { NextResponse } from 'next/server';
 
 export const middleware = (request: NextRequest) => {
+  const shouldNotShowBetaVersion =
+    process.env.NEXT_PUBLIC_SHOULD_SHOW_BETA_VERSION !== 'true';
+
   const hasSeenConversionPage =
     request.cookies.get('has_seen_beta_conversion_page') === 'true';
 
-  if (!hasSeenConversionPage) {
-    const url = new URL('/beta-version', request.url);
-    return NextResponse.redirect(url);
-  }
+  if (shouldNotShowBetaVersion || hasSeenConversionPage) return;
+
+  const url = new URL('/beta-version', request.url);
+  return NextResponse.redirect(url);
 };
 
 export const config = {
