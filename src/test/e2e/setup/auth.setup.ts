0d4c6598ec54a8a2c17139552476e2e9a18cecd3
fulldev1023 <simbadevlop@gmail.com>
2023-03-07 14:17:08 +0100

    Change test slightly

--- src/test/e2e/setup/auth.setup.ts
+++ src/test/e2e/setup/auth.setup.ts
@@ -8,14 +8,21 @@ setup('authenticate', async ({ baseURL, page }) => {
 
   await page.getByRole('button', { name: 'Start hier' }).click();
 
-  await page.waitForURL(new RegExp(/publiq-acc.eu.auth0.com\/*/));
+  await page.waitForURL(/publiq-acc.eu.auth0.com\/*/);
 
   await page.getByLabel('Je e-mailadres').fill(process.env.E2E_TEST_EMAIL);
   await page.getByLabel('Je wachtwoord').fill(process.env.E2E_TEST_PASSWORD);
 
   await page.getByRole('button', { name: 'Meld je aan', exact: true }).click();
 
-  await page.waitForURL(new RegExp(`${baseURL}/dashboard*`));
+  // Now we're back to our own app
+  // Wait that the main page has loaded
+  await page.waitForURL(new RegExp(`${baseURL}/dashboard*`), {
+    timeout: 10000,
+  });
+
+  // Wait for network to be idle, if we save storage too early, needed storage values might not yet be available
+  await page.waitForLoadState('networkidle');
 
   await page.getByText('Welkom, e2e.udb3.frontend').waitFor();
 
