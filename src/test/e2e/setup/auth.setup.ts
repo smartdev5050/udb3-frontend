0db62550b672a9fc03f88ae041d52c5e667a5815
fulldev1023 <simbadevlop@gmail.com>
2023-03-07 12:16:53 +0100

    Try different setup code page

--- src/test/e2e/setup/auth.setup.ts
+++ src/test/e2e/setup/auth.setup.ts
@@ -4,22 +4,31 @@ import { test as setup } from '@playwright/test';
 const authFile = 'playwright/.auth/user.json';
 
 setup('authenticate', async ({ baseURL, browser }) => {
-  const context = await browser.newContext();
-  const page = await context.newPage();
+  try {
+    const context = await browser.newContext();
+    const page = await context.newPage();
 
-  await page.goto(
-    `${baseURL}/login/nl?referer=http%3A%2F%2Flocalhost%3A3000%2Fdashboard`,
-    {
-      timeout: 60000, // 60 seconds
-      waitUntil: 'domcontentloaded',
-    },
-  );
+    await page.goto(
+      `${baseURL}/login/nl?referer=http%3A%2F%2Flocalhost%3A3000%2Fdashboard`,
+    );
 
-  await page.getByRole('button', { name: 'Start hier' }).click();
-  await page.getByLabel('Je e-mailadres').fill(process.env.E2E_TEST_EMAIL);
-  await page.getByLabel('Je wachtwoord').fill(process.env.E2E_TEST_PASSWORD);
-  await page.getByRole('button', { name: 'Meld je aan', exact: true }).click();
-  await page.getByText('Welkom, e2e.udb3.frontend').waitFor();
+    await page.getByRole('button', { name: 'Start hier' }).click();
+    await page.getByLabel('Je e-mailadres').fill(process.env.E2E_TEST_EMAIL);
+    await page.getByLabel('Je wachtwoord').fill(process.env.E2E_TEST_PASSWORD);
+    await page
+      .getByRole('button', { name: 'Meld je aan', exact: true })
+      .click();
+    await page.getByText('Welkom, e2e.udb3.frontend').waitFor();
 
-  await page.context().storageState({ path: authFile });
+    await page.context().storageState({ path: authFile });
+
+    // Gracefully close up everything
+    await context.close();
+    await browser.close();
+  } catch (err) {
+    // eslint-disable-next-line no-console
+    console.error(err);
+  } finally {
+    browser.close();
+  }
 });
