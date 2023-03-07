b59bbfeb7cf5a0852152fa9e1e2a3ff442342f77
fulldev1023 <simbadevlop@gmail.com>
2023-03-07 13:12:32 +0100

    Add extra checks

--- src/test/e2e/setup/auth.setup.ts
+++ src/test/e2e/setup/auth.setup.ts
@@ -1,5 +1,6 @@
 // auth.setup.ts
 import { test as setup } from '@playwright/test';
+import { startsWith } from 'lodash';
 
 const authFile = 'playwright/.auth/user.json';
 
@@ -9,15 +10,17 @@ setup('authenticate', async ({ baseURL, browser }) => {
     const page = await context.newPage();
 
     await page.goto(
-      `${baseURL}/login/nl?referer=http%3A%2F%2Flocalhost%3A3000%2Fdashboard`,
+      '/login/nl?referer=http%3A%2F%2Flocalhost%3A3000%2Fdashboard',
     );
 
     await page.getByRole('button', { name: 'Start hier' }).click();
+    await page.waitForURL(new RegExp(/publiq-acc.eu.auth0.com\/*/));
     await page.getByLabel('Je e-mailadres').fill(process.env.E2E_TEST_EMAIL);
     await page.getByLabel('Je wachtwoord').fill(process.env.E2E_TEST_PASSWORD);
     await page
       .getByRole('button', { name: 'Meld je aan', exact: true })
       .click();
+    await page.waitForURL((url) => url.toString().startsWith(baseURL));
     await page.getByText('Welkom, e2e.udb3.frontend').waitFor();
 
     await page.context().storageState({ path: authFile });
