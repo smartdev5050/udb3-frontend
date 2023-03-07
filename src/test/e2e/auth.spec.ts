01da97ee011f2c3312627c554506754d4bd2835a
fulldev1023 <simbadevlop@gmail.com>
2023-03-07 10:20:37 +0100

    Update ci steps

--- src/test/e2e/auth.spec.ts
+++ src/test/e2e/auth.spec.ts
@@ -3,5 +3,5 @@ import { expect, test } from '@playwright/test';
 test('can authenticate', async ({ page, baseURL }) => {
   await page.goto(`${baseURL}/dashboard?tab=events&page=1&sort=created_desc`);
 
-  await expect(page.getByText('Welkom, e2e.udb3.frontend')).toBeVisible()
+  await expect(page.getByText('Welkom, e2e.udb3.frontend')).toBeVisible();
 });
