cec09631bf2493b6c588e55daa4e1d29a95ac35b
fulldev1023 <simbadevlop@gmail.com>
2023-03-09 17:03:03 +0100

    III-5081 - Fetch event server side

--- src/pages/events/[eventId]/duplicate/index.page.tsx
+++ src/pages/events/[eventId]/duplicate/index.page.tsx
@@ -3,9 +3,18 @@ import { dehydrate } from 'react-query/hydration';
 import { getApplicationServerSideProps } from '@/utils/getApplicationServerSideProps';
 
 import { OfferForm } from '../../../create/OfferForm';
+import { useGetEventByIdQuery } from '@/hooks/api/events';
 
 export const getServerSideProps = getApplicationServerSideProps(
-  async ({ queryClient, cookies }) => {
+  async ({ req, query, queryClient, cookies }) => {
+    const { eventId } = query;
+
+    const event = (await useGetEventByIdQuery({
+      id: eventId,
+      req,
+      queryClient,
+    })) as Event;
+
     return {
       props: {
         dehydratedState: dehydrate(queryClient),
