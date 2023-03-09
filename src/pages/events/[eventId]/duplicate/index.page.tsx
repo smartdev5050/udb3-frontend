558eda07500c9e0566999bafe9ed9a6994733f47
fulldev1023 <simbadevlop@gmail.com>
2023-03-09 17:19:18 +0100

    III-5081 - Fix linting

--- src/pages/events/[eventId]/duplicate/index.page.tsx
+++ src/pages/events/[eventId]/duplicate/index.page.tsx
@@ -1,9 +1,9 @@
 import { dehydrate } from 'react-query/hydration';
 
+import { useGetEventByIdQuery } from '@/hooks/api/events';
 import { getApplicationServerSideProps } from '@/utils/getApplicationServerSideProps';
 
 import { OfferForm } from '../../../create/OfferForm';
-import { useGetEventByIdQuery } from '@/hooks/api/events';
 
 export const getServerSideProps = getApplicationServerSideProps(
   async ({ req, query, queryClient, cookies }) => {
