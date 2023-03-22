5b2588940ebafc5af4167abff6dbe1ec90976ed0
fulldev1023 <simbadevlop@gmail.com>
2023-03-22 11:26:32 +0100

    Make email regex allow TLD of more than 3 characters

--- src/pages/steps/AdditionalInformationStep/ContactInfoStep.tsx
+++ src/pages/steps/AdditionalInformationStep/ContactInfoStep.tsx
@@ -40,7 +40,7 @@ type NewContactInfo = {
   value: string;
 };
 
-const EMAIL_REGEX: RegExp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+const EMAIL_REGEX: RegExp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,})+$/;
 const URL_REGEX: RegExp =
   /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)$/;
 const PHONE_REGEX: RegExp = /^[0-9\/_.+ ]*$/;
