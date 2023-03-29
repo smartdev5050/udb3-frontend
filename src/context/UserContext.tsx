6b4841190f65bbb98f75fba2014ab795212e9e95
fulldev1023 <simbadevlop@gmail.com>
2023-03-29 16:19:17 +0200

    III-5285 - Add UserContext

--- src/context/UserContext.tsx
+++ src/context/UserContext.tsx
@@ -1,5 +1,4 @@
 import { UserProvider } from '@auth0/nextjs-auth0/client';
-import { Children } from 'react';
 
 const UserContext = ({ children }) => {
   return <UserProvider>{children}</UserProvider>;
