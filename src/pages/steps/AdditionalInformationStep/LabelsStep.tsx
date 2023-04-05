301eb755a1800d19491bff96c68b67fb9e84346d
fulldev1023 <simbadevlop@gmail.com>
2023-04-05 14:03:48 +0200

    Prefill field with existing offer labels

--- src/pages/steps/AdditionalInformationStep/LabelsStep.tsx
+++ src/pages/steps/AdditionalInformationStep/LabelsStep.tsx
@@ -4,15 +4,22 @@ import { Text, TextVariants } from '@/ui/Text';
 import { Typeahead } from '@/ui/Typeahead';
 import { useGetLabelsByQuery } from '@/hooks/api/labels';
 import React, { useRef, useState } from 'react';
-import { Label } from '@/types/Offer';
+import { Label, Offer } from '@/types/Offer';
 import { Badge, BadgeVariants } from '@/ui/Badge';
 import { Inline } from '@/ui/Inline';
+import { Icon, Icons } from '@/ui/Icon';
+import { uniqBy } from 'lodash';
+import { useGetOfferByIdQuery } from '@/hooks/api/offers';
+import { getGlobalBorderRadius } from '@/ui/theme';
 
-function LabelsStep(props) {
+function LabelsStep({ offerId, scope, ...props }) {
+  const getOfferByIdQuery = useGetOfferByIdQuery({ id: offerId, scope });
+  const offer: Offer | undefined = getOfferByIdQuery.data;
+
   const ref = useRef(null);
   const labelsQuery = useGetLabelsByQuery({ query: '' });
   const options: Label[] = labelsQuery.data?.member ?? [];
-  const [labels, setLabels] = useState<Label[]>([]);
+  const [labels, setLabels] = useState<string[]>(offer.labels);
 
   return (
     <Stack {...getStackProps(props)}>
@@ -28,7 +35,12 @@ function LabelsStep(props) {
             labelKey={'name'}
             onSearch={(query) => labelsQuery.refetch({ query })}
             onChange={(newLabels: Label[]) => {
-              setLabels([...labels, ...newLabels]);
+              setLabels(
+                uniqBy([...labels, ...newLabels], 'uuid').map(
+                  (label) => label.name,
+                ),
+              );
+
               ref.current.clear();
             }}
             customFilter={() => true}
@@ -43,8 +55,25 @@ function LabelsStep(props) {
       />
       <Inline spacing={2}>
         {labels.map((label) => (
-          <Badge key={label.uuid} variant={BadgeVariants.SECONDARY}>
-            {label.name}
+          <Badge
+            key={label}
+            variant={BadgeVariants.SECONDARY}
+            borderRadius={getGlobalBorderRadius}
+            cursor={'pointer'}
+            display={'flex'}
+          >
+            {label}
+            <Icon
+              name={Icons.TIMES}
+              width={'12px'}
+              height={'12px'}
+              marginLeft={1}
+              onClick={() =>
+                setLabels(
+                  labels.filter((existingLabel) => label !== existingLabel),
+                )
+              }
+            />
           </Badge>
         ))}
       </Inline>
