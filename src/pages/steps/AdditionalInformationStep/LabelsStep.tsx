13245a0f94f141a5aaa82f1194acdbd03488fdfb
fulldev1023 <simbadevlop@gmail.com>
2023-04-05 12:19:31 +0200

    Add FormElement

--- src/pages/steps/AdditionalInformationStep/LabelsStep.tsx
+++ src/pages/steps/AdditionalInformationStep/LabelsStep.tsx
@@ -1,5 +1,27 @@
+import { FormElement } from '@/ui/FormElement';
+import { getStackProps, Stack } from '@/ui/Stack';
+import { Text, TextVariants } from '@/ui/Text';
+import { Typeahead } from '@/ui/Typeahead';
+import { useTranslation } from 'react-i18next';
+
 function LabelsStep(props) {
-  return <div></div>;
+  const { t, i18n } = useTranslation();
+  return (
+    <Stack>
+      <FormElement
+        id={'labels'}
+        label={'Verfijn met labels'}
+        Component={<Typeahead options={[]} labelKey={'name'} />}
+        maxWidth={'50%'}
+        info={
+          <Text variant={TextVariants.MUTED}>
+            Met labels voeg je korte, specifieke trefwoorden toe
+          </Text>
+        }
+        {...getStackProps(props)}
+      />
+    </Stack>
+  );
 }
 
 export { LabelsStep };
