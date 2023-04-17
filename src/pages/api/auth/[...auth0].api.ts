c32d07157294921e924cc96ac1dcd5618e5675fb
fulldev1023 <simbadevlop@gmail.com>
2023-04-17 15:07:19 +0200

    III-5285 - Added extra login params for UiTiD

--- src/pages/api/auth/[...auth0].api.ts
+++ src/pages/api/auth/[...auth0].api.ts
@@ -1,22 +1,30 @@
 import { handleAuth, handleLogin, handleLogout } from '@auth0/nextjs-auth0';
+import { NextApiRequest } from 'next';
+import getConfig from 'next/config';
 
 export default handleAuth({
   async login(req, res) {
+    const language = req.cookies['udb-language'] ?? 'nl';
     try {
       await handleLogin(req, res, {
         authorizationParams: {
           audience: 'https://api.publiq.be',
           scope: 'openid profile email',
+          locale: language,
+          referrer: 'udb',
+          skip_verify_legacy: 'true',
+          product_display_name: 'UiTdatabank',
         },
       });
     } catch (error) {
       res.status(error.status || 400).end(error.message);
     }
   },
-  async logout(req, res) {
+  async logout(req: NextApiRequest, res) {
+    const { publicRuntimeConfig } = getConfig();
     try {
       await handleLogout(req, res, {
-        returnTo: 'http://localhost:3000',
+        returnTo: publicRuntimeConfig.baseUrl,
       });
     } catch (err) {
       res.status(err.status || 400).end();
