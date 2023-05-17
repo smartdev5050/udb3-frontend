6607ec0b6e2b7ea16bccc761b357b3546a11c4a7
fulldev1023 <simbadevlop@gmail.com>
2023-05-17 14:22:29 +0200

    III-5582 - Use regex instead of if statements

--- src/utils/url.ts
+++ src/utils/url.ts
@@ -1,12 +1,5 @@
 const prefixUrlWithHttps = (url: string) => {
-  if (url.startsWith('https://')) return url;
-
-  if (url.startsWith('http://')) {
-    const httpsUrl = url.replace('http://', 'https://');
-    return httpsUrl;
-  }
-
-  return `https://${url}`;
+  return url.replace(/(https?:\/\/)?(.+)/, 'https://$2');
 };
 
 export { prefixUrlWithHttps };
