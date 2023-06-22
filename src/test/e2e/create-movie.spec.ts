5fcf4f7a22d373b057a536df4bcf797e487be992
fulldev1023 <simbadevlop@gmail.com>
2023-06-22 14:52:46 +0200

    Add initial spec draft

---
+++ src/test/e2e/create-movie.spec.ts
@@ -0,0 +1,43 @@
+import { expect, test } from '@playwright/test';
+
+const dummyMovie = {};
+
+test('create a movie', async ({ baseURL, page }) => {
+  await page.goto(`${baseURL}`);
+  await page.getByRole('link', { name: 'Films' }).click();
+  await page.getByRole('button', { name: 'Cinefiel' }).click();
+  await page.getByLabel('Einde').click();
+  await page.getByText('juni', { exact: true }).click();
+  await page.getByText('juli').click();
+  await page
+    .getByRole('option', { name: 'Choose zaterdag 1 juli 2023' })
+    .click();
+  await page.locator('[id="\\32 8\\/06\\/2023-0"]').click();
+  await page.locator('[id="\\32 8\\/06\\/2023-0"]').fill('10');
+  await page.locator('[id="\\32 8\\/06\\/2023-1"]').click();
+  await page.locator('[id="\\32 8\\/06\\/2023-1"]').fill('12');
+  await page.locator('[id="\\32 8\\/06\\/2023-2"]').click();
+  await page.locator('[id="\\32 8\\/06\\/2023-2"]').fill('14');
+  await page.locator('[id="\\32 8\\/06\\/2023-3"]').click();
+  await page.locator('[id="\\32 8\\/06\\/2023-3"]').fill('16');
+  await page.getByPlaceholder('Naam van bioscoop').click();
+  await page.getByPlaceholder('Naam van bioscoop').fill('str');
+  await page
+    .locator('#place-step-item-0', { hasText: 'E2E test location' })
+    .click();
+  await page.getByLabel('Kies een naam').click();
+  await page.getByLabel('Kies een naam').fill('My Name');
+  await page.getByRole('option', { name: 'My Name' }).click();
+  await page.getByRole('button', { name: 'Opslaan' }).click();
+  await page.getByRole('textbox', { name: 'rdw-editor' }).click();
+  await page
+    .getByRole('textbox', { name: 'rdw-editor' })
+    .fill('My description');
+  await page.getByRole('tab', { name: 'Prijzen' }).click();
+  await page.getByPlaceholder('Prijs').click();
+  await page.getByPlaceholder('Prijs').fill('10');
+  await page.getByRole('button', { name: 'Publiceren', exact: true }).click();
+  await page.waitForURL('**/preview');
+  await page.getByText('My Name').isVisible();
+  await page.getByText('My Description').isVisible();
+});
