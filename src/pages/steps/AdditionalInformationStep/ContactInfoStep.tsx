9c7a780d863fb5874fb0a8f98e3ed2b35a1f1a62
fulldev1023 <simbadevlop@gmail.com>
2023-06-27 14:48:20 +0200

    III-5697 - Also allow ! in url's

--- src/pages/steps/AdditionalInformationStep/ContactInfoStep.tsx
+++ src/pages/steps/AdditionalInformationStep/ContactInfoStep.tsx
@@ -36,7 +36,7 @@ type NewContactInfo = {
 
 const EMAIL_REGEX: RegExp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,})+$/;
 const URL_REGEX: RegExp =
-  /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?&\/=]*)$/;
+  /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?!&\/=]*)$/;
 const PHONE_REGEX: RegExp = /^[0-9\/\-_.+ ]{0,15}$/;
 
 const isValidEmail = (email: string) => {
