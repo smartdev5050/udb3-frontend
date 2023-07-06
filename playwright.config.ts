61ed196c1d96f6539424a340eca39157c251df10
fulldev1023 <simbadevlop@gmail.com>
2023-07-06 15:29:46 +0200

    III-5726 - Increase timeout to fix ci on github

--- playwright.config.ts
+++ playwright.config.ts
@@ -8,7 +8,7 @@ import { defineConfig, devices } from '@playwright/test';
 export default defineConfig({
   testDir: './src/test/e2e',
   /* Maximum time one test can run for. */
-  timeout: 30 * 1000,
+  timeout: 60 * 1000,
   expect: {
     /**
      * Maximum time expect() should wait for the condition to be met.
