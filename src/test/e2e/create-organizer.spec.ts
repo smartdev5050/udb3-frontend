ed0f9b56354bceeeb9b41083482fb55609d884a6
fulldev1023 <simbadevlop@gmail.com>
2023-07-26 15:01:17 +0200

    Add some blur() calls to trigger the right events

--- src/test/e2e/create-organizer.spec.ts
+++ src/test/e2e/create-organizer.spec.ts
@@ -27,6 +27,7 @@ test('create an organizer', async ({ baseURL, page }) => {
   await page.getByRole('button', { name: 'Contactgegevens toevoegen' }).click();
   await page.locator('#contact-info-value').click();
   await page.locator('#contact-info-value').fill(dummyOrganizer.email);
+  await page.locator('#contact-info-value').blur();
 
   await page.getByRole('tab', { name: 'Locatie' }).click();
   await page.getByLabel('Gemeente').click();
@@ -34,11 +35,13 @@ test('create an organizer', async ({ baseURL, page }) => {
   await page.getByLabel('9000 Gent').click();
   await page.getByLabel('Straat en nummer').click();
   await page.getByLabel('Straat en nummer').fill('rab');
+  await page.getByLabel('Straat en nummer').blur();
 
   await page.getByRole('tab', { name: 'Labels' }).click();
   await page.getByLabel('Verfijn met labels').click();
   await page.getByLabel('Verfijn met labels').fill('foo');
   await page.getByLabel('american football').click();
+  await page.getByLabel('Verfijn met labels').blur();
 
-  await page.getByRole('tab', { name: 'Beschrijving' }).click();
+  await page.getByLabel('Bewaren').click();
 });
