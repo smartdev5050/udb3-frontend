fcd008bf2a23c414fa62503a859ce8819edcf8ba
fulldev1023 <simbadevlop@gmail.com>
2023-07-27 13:10:00 +0200

    Move all dummy data to object

--- src/test/e2e/create-organizer.spec.ts
+++ src/test/e2e/create-organizer.spec.ts
@@ -6,6 +6,15 @@ const dummyOrganizer = {
   website: faker.internet.url(),
   description: faker.lorem.paragraphs(3),
   email: faker.internet.email(),
+  label: 'american football',
+  location: {
+    municipality: '9000 Gent',
+    address: faker.location.streetAddress(),
+  },
+  image: {
+    name: faker.lorem.word(),
+    copyright: faker.lorem.word(),
+  },
 };
 
 test('create an organizer', async ({ baseURL, page }) => {
@@ -31,9 +40,9 @@ test('create an organizer', async ({ baseURL, page }) => {
 
   await page.getByRole('tab', { name: 'Labels' }).click();
   await page.getByLabel('Verfijn met labels').click();
-  await page.getByLabel('Verfijn met labels').fill('foo');
-  await page.getByLabel('american football').click();
-  await page.getByText('american football').click();
+  await page.getByLabel('Verfijn met labels').fill(dummyOrganizer.label);
+  await page.getByLabel(dummyOrganizer.label).click();
+  await page.getByText(dummyOrganizer.label).click();
 
   await page.getByRole('tab', { name: 'Afbeeldingen' }).click();
   await page.getByRole('button', { name: 'Afbeelding toevoegen' }).click();
@@ -42,18 +51,20 @@ test('create an organizer', async ({ baseURL, page }) => {
     .locator('input[type=file]')
     .setInputFiles('./src/test/data/image.png');
   await page.getByLabel('Beschrijving').click();
-  await page.getByLabel('Beschrijving').fill('foo');
+  await page.getByLabel('Beschrijving').fill(dummyOrganizer.image.name);
   await page.getByLabel('Copyright').click();
-  await page.getByLabel('Copyright').fill('bar');
+  await page.getByLabel('Copyright').fill(dummyOrganizer.image.copyright);
   await page.getByRole('button', { name: 'Uploaden' }).click();
-  await page.getByText('© bar').click();
+  await page.getByText(`© ${dummyOrganizer.image.copyright}`).click();
 
   await page.getByRole('tab', { name: 'Locatie' }).click();
   await page.getByLabel('Gemeente').click();
-  await page.getByLabel('Gemeente').fill('gent');
-  await page.getByLabel('9000 Gent').click();
+  await page.getByLabel('Gemeente').fill(dummyOrganizer.location.municipality);
+  await page.getByLabel(dummyOrganizer.location.municipality).click();
   await page.getByLabel('Straat en nummer').click();
-  await page.getByLabel('Straat en nummer').fill('rab');
+  await page
+    .getByLabel('Straat en nummer')
+    .fill(dummyOrganizer.location.address);
   await page.getByLabel('Straat en nummer').blur();
 
   await page.getByText('100/100').click();
