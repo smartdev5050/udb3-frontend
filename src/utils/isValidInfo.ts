073b4561baa371de3573c95bad8037cc6c406718
fulldev1023 <simbadevlop@gmail.com>
2023-07-27 15:57:02 +0200

    Goodbye regex

--- src/utils/isValidInfo.ts
+++ src/utils/isValidInfo.ts
@@ -1,6 +1,4 @@
 const EMAIL_REGEX: RegExp = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,})+$/;
-const URL_REGEX: RegExp =
-  /^https?:\/\/(?:www\.)?[-a-zA-Z0-9@:%._\+~#=]{1,256}\.[a-zA-Z0-9()]{1,6}\b(?:[-a-zA-Z0-9()@:%_\+.~#?!&\/=]*)$/;
 const PHONE_REGEX: RegExp = /^[0-9\/\-_.+ ]{0,15}$/;
 
 const isValidEmail = (email: string) => {
