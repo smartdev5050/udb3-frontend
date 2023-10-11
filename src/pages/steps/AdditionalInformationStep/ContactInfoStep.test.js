7b82aa91fa440715447e75f4cb4e9617ff0db21e
fulldev1023 <simbadevlop@gmail.com>
2023-10-11 15:34:58 +0200

    III-5847 - Add test case in isValidUrl test

--- src/pages/steps/AdditionalInformationStep/ContactInfoStep.test.js
+++ src/pages/steps/AdditionalInformationStep/ContactInfoStep.test.js
@@ -4,6 +4,7 @@ describe('isValidUrl', () => {
   const tests = {
     goobar: false,
     'goobar.com': false,
+    'http://goobar': false,
     'http://goobar.com': true,
     'https://speeltuin.vlaanderen/speeltuinen': true,
     'https://speeltuin.vlaanderen/speeltuinen?foo[]=bar&baz=50': true,
