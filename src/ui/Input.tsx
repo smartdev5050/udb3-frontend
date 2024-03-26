b2f20595371bf1952eee0c2c8dcbf9d558a8c9f4
fulldev1023 <simbadevlop@gmail.com>
2024-03-26 15:37:56 +0100

    Allow some more props to be passed down from Input to BaseInput

--- src/ui/Input.tsx
+++ src/ui/Input.tsx
@@ -2,9 +2,10 @@ import type { ChangeEvent, HTMLProps } from 'react';
 import { forwardRef } from 'react';
 import { Form } from 'react-bootstrap';
 
-import type { BoxProps } from './Box';
+import { BoxProps, boxPropTypes } from './Box';
 import { Box, getBoxProps } from './Box';
 import { getGlobalBorderRadius, getGlobalFormInputHeight } from './theme';
+import { pickBy } from 'lodash';
 
 const BaseInput = forwardRef<HTMLInputElement, any>((props, ref) => (
   <Box as="input" {...props} ref={ref} />
@@ -37,6 +38,27 @@ type InputType =
   | 'url'
   | 'week';
 
+const getInputProps = (props) =>
+  pickBy(props, (_value, key) =>
+    [
+      'accept',
+      'className',
+      'data-testid',
+      'disabled',
+      'id',
+      'isInvalid',
+      'maxLength',
+      'name',
+      'onBlur',
+      'onFocus',
+      'onKeyDown',
+      'onPaste',
+      'placeholder',
+      'type',
+      'value',
+    ].includes(key),
+  );
+
 type InputProps = HTMLProps<HTMLInputElement> & {
   value?: string;
   onChange?: (event: ChangeEvent<HTMLInputElement>) => void;
@@ -45,47 +67,15 @@ type InputProps = HTMLProps<HTMLInputElement> & {
 type Props = Omit<BoxProps, 'onChange' | 'onBlur'> & InputProps;
 
 const Input = forwardRef(
-  (
-    {
-      type,
-      id,
-      placeholder,
-      onChange,
-      onBlur,
-      onPaste,
-      onFocus,
-      onKeyDown,
-      className,
-      value,
-      name,
-      isInvalid,
-      disabled,
-      accept,
-      ...props
-    }: Props,
-    ref,
-  ) => (
+  ({ id, onChange, className, ...props }: Props, ref) => (
     <Form.Control
       ref={ref}
       as={BaseInput}
-      id={id}
-      type={type}
-      placeholder={placeholder}
-      className={className}
       maxWidth="43rem"
       height={getGlobalFormInputHeight}
       borderRadius={getGlobalBorderRadius}
       onInput={onChange}
-      onBlur={onBlur}
-      onPaste={onPaste}
-      value={value}
-      name={name}
-      isInvalid={isInvalid}
-      accept={accept}
-      disabled={disabled}
-      onFocus={onFocus}
-      onKeyDown={onKeyDown}
-      data-testid={props['data-testid']}
+      {...getInputProps(props)}
       {...getBoxProps(props)}
     />
   ),
