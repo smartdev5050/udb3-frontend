de723ad2a66fe111ad11f6d2e744f9d0058bbb16
fulldev1023 <simbadevlop@gmail.com>
2024-04-18 09:40:46 +0200

    Fix usual query type issue

--- src/pages/steps/AdditionalInformationStep/PriceInformation.tsx
+++ src/pages/steps/AdditionalInformationStep/PriceInformation.tsx
@@ -357,6 +357,7 @@ const PriceInformation = ({
                                 shouldValidate: false,
                               });
                               onSubmit();
+                              // @ts-expect-error
                               getOfferByIdQuery.remove();
                             }}
                           >
